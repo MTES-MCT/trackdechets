d8dd32a624fb552862cb9a970ea2925b65cc930b
Riron <orioncharlier@gmail.com>
2018-12-13 18:52:47 +0100

    Insee - token renewal

---
+++ insee/cron/renew-token.sh
@@ -0,0 +1,3 @@
+curl -k -d "grant_type=client_credentials" \
+  -H "Authorization: Basic VzRJSm4yTHQ2anVzTktzaDVXT1hOZUFVNFZJYToyVmpYRlB5U2FoSHlFSF81R2FhM0FBdEtnOVVh" \
+  https://api.insee.fr/token > key.json
