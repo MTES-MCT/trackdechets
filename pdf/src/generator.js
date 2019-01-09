1618374c92a4699d5ebe10868b84db1e34f36068
Riron <orioncharlier@gmail.com>
2019-01-09 12:41:14 +0100

    PDF - add templating

--- pdf/src/generator.js
+++ pdf/src/generator.js
@@ -1,22 +1,16 @@
 const puppeteer = require("puppeteer");
+const doT = require("dot");
 
 const html = require("fs").readFileSync("./models/bsd.html", "utf8");
-
-const t = '<html>${params.a}</html>'
-
-function assemble(literal, params) {
-  return new Function(params, "return `"+literal+"`;"); // TODO: Proper escaping
-}
-var template = assemble("<html>${params.a}</html>", "params");
-template({a: 1});
+const tempFn = doT.template(html);
 
 async function write(params) {
-  console.log(params)
+  const pageContent = tempFn(params);
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.setContent(html);
-  
+  await page.setContent(pageContent);
+
   const buffer = await page.pdf({ format: "A4" });
 
   await browser.close();
