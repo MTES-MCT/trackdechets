5ff13fdceca5efb86aaae59a9041c571325bfdac
Riron <orioncharlier@gmail.com>
2019-01-10 14:20:48 +0100

    Front - embed typeform

--- front/src/search/Search.tsx
+++ front/src/search/Search.tsx
@@ -1,78 +1,21 @@
-import React, { useState } from "react";
-import Header from "../Header";
+import React, { useRef, useEffect } from "react";
 import "./Search.scss";
-import { Link } from "react-router-dom";
+import * as typeformEmbed from "@typeform/embed";
 
 export default function Search() {
-  const [show, setShow] = useState(false);
-
-  const [company, setCompany] = useState("");
-  const [wasteCode, setWasteCode] = useState("");
+  const typeFormRef = useRef(null);
+  useEffect(() => {
+    typeformEmbed.makeWidget(
+      typeFormRef.current,
+      "https://xpedition.typeform.com/to/ERgvv5",
+      {}
+    );
+  }, []);
 
   return (
     <div className="Search section section-grey">
       <div className="container">
-        <div className="panel">
-          <div className="panel__header">
-            <h3>Vérification d'un prestataire</h3>
-            <small className="panel__header-extra">
-              2018 entreprises référencées
-            </small>
-          </div>
-
-          <p>
-            Vous avez la possibilité de vérifier si un prestataire est autorisé
-            à prendre en charge un code déchet en particulier.
-          </p>
-
-          <div className="search-box">
-            <div className="search__input">
-              <input
-                type="text"
-                placeholder="Nom ou SIRET de l'entreprise"
-                value={company}
-                onChange={e => setCompany(e.target.value)}
-              />
-              <input
-                type="text"
-                placeholder="Code déchet"
-                value={wasteCode}
-                onChange={e => setWasteCode(e.target.value)}
-              />
-            </div>
-            <button className="button" onClick={() => setShow(!show)}>
-              Vérifier
-            </button>
-          </div>
-        </div>
-
-        {show && (
-          <div className="panel panel__approved">
-            <div className="approved">
-              <h3>
-                L'entreprise <strong>{company}</strong> est
-              </h3>
-              <ul>
-                <li>
-                  autorisée au titre des{" "}
-                  <a href="http://www.installationsclassees.developpement-durable.gouv.fr/rechercheICForm.php">
-                    ICPE
-                  </a>{" "}
-                  à collecter et/ou traiter des déchets dangereux
-                </li>
-                <li>
-                  capable de prendre en charge le code déchet{" "}
-                  <strong>{wasteCode}</strong>
-                </li>
-              </ul>
-              <small>
-                Pour en savoir plus sur cette entreprise, accédez{" "}
-                <Link to="#">à sa fiche.</Link>
-              </small>
-            </div>
-            <div className="stamp">Valide</div>
-          </div>
-        )}
+        <div ref={typeFormRef} className="typeform" />
       </div>
     </div>
   );
---
+++ front/src/search/typeform.d.ts
@@ -0,0 +1,4 @@
+declare module "@typeform/embed" {
+  const typeformEmbed: any;
+  export = typeformEmbed;
+}
