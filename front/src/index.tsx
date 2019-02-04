504d5ffac0df4bebad973484ea1083005829ede7
Riron <orioncharlier@gmail.com>
2019-02-04 09:44:32 +0100

    Front - ie11 polyfills

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,9 +1,14 @@
+// Polyfills
+import 'unfetch/polyfill'
+import 'react-app-polyfill/ie11';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'template.data.gouv.fr/dist/style/main.css';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
