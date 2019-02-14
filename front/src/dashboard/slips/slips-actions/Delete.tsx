4fc88dfe00c55e6d62bf920bcd1eef43c72da50a
Riron <orioncharlier@gmail.com>
2019-02-14 10:44:18 +0100

    Front - confirmation box on darft delete

--- front/src/dashboard/slips/slips-actions/Delete.tsx
+++ front/src/dashboard/slips/slips-actions/Delete.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaTrash } from "react-icons/fa";
 import { Mutation } from "react-apollo";
 import mutations from "./slip-actions.mutations";
@@ -8,30 +8,58 @@ import { Form } from "../../../form/model";
 type Props = { formId: string };
 
 export default function Delete({ formId }: Props) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <Mutation mutation={mutations.DELETE_FORM}>
       {(deleteForm, { error }) => (
-        <a
-          className="icon"
-          title="Supprimer définitivement"
-          onClick={() =>
-            deleteForm({
-              variables: { id: formId },
-              update: (store, { data: { deleteForm } }) => {
-                const data = store.readQuery<{ forms: Form[] }>({
-                  query: GET_SLIPS
-                });
-                if (!data || !data.forms) {
-                  return;
+        <React.Fragment>
+          <a
+            className="icon"
+            title="Supprimer définitivement"
+            onClick={() => setIsOpen(true)}
+          >
+            <FaTrash />
+          </a>
+          <div
+            className="modal__backdrop"
+            id="modal"
+            style={{ display: isOpen ? "flex" : "none" }}
+          >
+            <div className="modal">
+              <h3>Confirmer la suppression ?</h3>
+              <p>Cette action supprimera le brouillon et est irréversible.</p>
+              <button
+                className="button warning"
+                onClick={() => setIsOpen(false)}
+              >
+                Annuler
+              </button>
+              <button
+                className="button"
+                onClick={() =>
+                  deleteForm({
+                    variables: { id: formId },
+                    update: (store, { data: { deleteForm } }) => {
+                      const data = store.readQuery<{ forms: Form[] }>({
+                        query: GET_SLIPS
+                      });
+                      if (!data || !data.forms) {
+                        return;
+                      }
+                      data.forms = data.forms.filter(
+                        f => f.id !== deleteForm.id
+                      );
+                      store.writeQuery({ query: GET_SLIPS, data });
+                    }
+                  })
                 }
-                data.forms = data.forms.filter(f => f.id !== deleteForm.id);
-                store.writeQuery({ query: GET_SLIPS, data });
-              }
-            })
-          }
-        >
-          <FaTrash />
-        </a>
+              >
+                Supprimer
+              </button>
+            </div>
+          </div>
+        </React.Fragment>
       )}
     </Mutation>
   );
