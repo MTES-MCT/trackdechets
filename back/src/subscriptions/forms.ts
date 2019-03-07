9b727326fa1d292fd02575372f612eab6084bff5
Riron <orioncharlier@gmail.com>
2019-03-07 17:06:50 +0100

    Back - mail when recipient company is not signed up

--- back/src/subscriptions/forms.ts
+++ back/src/subscriptions/forms.ts
@@ -1,5 +1,31 @@
-import { FormSubscriptionPayload } from "../generated/prisma-client";
+import { FormSubscriptionPayload, prisma } from "../generated/prisma-client";
+import { sendMail } from "../common/mails.helper";
+import { userMails } from "../users/mails";
 
 export function formsSubscriptionCallback(payload: FormSubscriptionPayload) {
+  mailToInexistantRecipient(payload);
+}
 
+async function mailToInexistantRecipient(payload: FormSubscriptionPayload) {
+  const previousRecipientSiret = payload.previousValues.recipientCompanySiret;
+  const recipientSiret = payload.node.recipientCompanySiret;
+  const recipientMail = payload.node.recipientCompanyMail;
+  const recipientName =
+    payload.node.recipientCompanyName || "Monsieur / Madame";
+
+  if (
+    !recipientSiret ||
+    !recipientMail ||
+    previousRecipientSiret === recipientSiret
+  ) {
+    return;
+  }
+
+  const companyExists = await prisma.$exists.company({ siret: recipientSiret });
+
+  if (companyExists) {
+    return;
+  }
+
+  return sendMail(userMails.contentAwaitsGuest(recipientMail, recipientName));
 }
--- back/src/users/mails.ts
+++ back/src/users/mails.ts
@@ -13,5 +13,21 @@ export const userMails = {
     Pour rappel, Trackdéchets est un site en béta conçu par la Fabrique Numérique du Ministère de l'Ecologie et des Territoires.
     <br>
     Si vous avez la moindre interrogation, n’hésitez pas à nous contacter à l'email <a href="mailto:emmanuel.flahaut@developpement-durable.gouv.fr">emmanuel.flahaut@developpement-durable.gouv.fr</a>.`
+  }),
+  contentAwaitsGuest: (toEmail, toName) => ({
+    toEmail,
+    toName,
+    subject: "",
+    title: "Un bordereau vous attend sur Trackdéchets",
+    body: `Bonjour ${toName},
+    <br>
+    Vous avez reçu un BSD dématérialisé disponible sur <a href="https://trackdechets.beta.gouv.fr/">https://trackdechets.beta.gouv.fr</a><br>
+    Ce message vous est adressé car l'entreprise qui vous a transmis ce bordereau dispose d'un compte sur Trackdéchets et son bordereau est en attente d'une action de votre part (confirmation de la réception et/ou du traitement du déchet).
+    <br>
+    Trackdéchets est un produit de la Fabrique Numérique du Ministère de la Transition Écologique et Solidaire.<br>
+    Il permet entre autres, de dématérialiser la procédure liée aux bordereaux de suivi de déchets et de tracer le déchet jusqu'à son traitement final.
+    <br>
+    Vous pouvez créer votre compte en cliquant <a href="https://trackdechets.beta.gouv.fr/signup">sur ce lien</a> et en suivant la procédure d'inscription. Vous pourrez alors commencer à utiliser Trackdéchets.
+  `
   })
 };
