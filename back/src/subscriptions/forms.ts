0a81c0348ae67b3310f2ed98140b64317779fd2f
Riron <orioncharlier@gmail.com>
2019-03-26 18:26:53 +0100

    back - log subscribtion

--- back/src/subscriptions/forms.ts
+++ back/src/subscriptions/forms.ts
@@ -9,6 +9,7 @@ export function formsSubscriptionCallback(payload: FormSubscriptionPayload) {
 }
 
 async function mailToInexistantRecipient(payload: FormSubscriptionPayload) {
+  console.info('SUB FORM "mailToInexistantRecipient" triggered');
   if (payload.updatedFields && payload.updatedFields.includes("isDeleted")) {
     return;
   }
