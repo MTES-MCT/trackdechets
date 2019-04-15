983f1342517557e21b265a32810abf3a646bd864
Riron <orioncharlier@gmail.com>
2019-04-15 15:23:53 +0200

    back - fix contacts

--- back/src/subscriptions/users.ts
+++ back/src/subscriptions/users.ts
@@ -4,9 +4,11 @@ import { addContact } from "../common/mails.helper";
 export async function usersSubscriptionCallback(
   payload: UserSubscriptionPayload
 ) {
-  addNewUserAsContact(payload).catch(err =>
-    console.error("Error on addNewUserAsContact subscription", err)
-  );
+  // As soon as you send an email though mailjet, he is added to the contacts
+  // As we send welcome emails, no need to add him ourself
+  // addNewUserAsContact(payload).catch(err =>
+  //   console.error("Error on addNewUserAsContact subscription", err)
+  // );
 }
 
 function addNewUserAsContact(payload: UserSubscriptionPayload) {
