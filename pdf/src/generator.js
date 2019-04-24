2bd99cbf8b0c8b843d8d3b61c97c89ba82dd50f7
Riron <orioncharlier@gmail.com>
2019-04-24 16:50:30 +0200

    pdf - enhancements
    - BIG speed up
    - better "signed" stamps

--- pdf/src/generator.js
+++ pdf/src/generator.js
@@ -12,20 +12,23 @@ const toDate = string => {
   return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 };
 
+const browserLaunch = puppeteer.launch({
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    // This will write shared memory files into /tmp instead of /dev/shm,
+    // because Docker’s default for /dev/shm is 64MB
+    "--disable-dev-shm-usage"
+  ]
+});
+
 async function write(params) {
+  const browser = await browserLaunch.catch(err =>
+    console.error("Cannot launch puppeteer", err)
+  );
+
   const pageContent = tempFn({ toDate, ...params });
 
-  const browser = await puppeteer
-    .launch({
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        // This will write shared memory files into /tmp instead of /dev/shm,
-        // because Docker’s default for /dev/shm is 64MB
-        "--disable-dev-shm-usage"
-      ]
-    })
-    .catch(err => console.error("Cannot launch puppeteer", err));
   const page = await browser
     .newPage()
     .catch(err => console.error("Error while starting new page", err));
@@ -35,7 +38,7 @@ async function write(params) {
     .pdf({ format: "A4" })
     .catch(err => console.error("Error while generating PDF", err));
 
-  await browser
+  await page
     .close()
     .catch(err => console.error("Error while closing buffer", err));
   return buffer;
