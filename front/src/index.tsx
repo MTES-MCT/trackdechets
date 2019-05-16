4698e91e6e1a7db230008f7efc819101124f1b85
Riron <orioncharlier@gmail.com>
2019-05-16 10:47:02 +0200

    front - pre-render home page and hydrate

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,16 +1,21 @@
 // Polyfills
-import 'unfetch/polyfill'
-import 'react-app-polyfill/ie11';
+import "unfetch/polyfill";
+import "react-app-polyfill/ie11";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'template.data.gouv.fr/dist/style/main.css';
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "template.data.gouv.fr/dist/style/main.css";
+import "./index.scss";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
+const rootElement = document.getElementById("root");
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (rootElement && rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<App />, rootElement);
+} else {
+  ReactDOM.render(<App />, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
