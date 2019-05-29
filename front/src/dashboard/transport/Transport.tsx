6df417083bae23d7a6cfc10cca57c504f2c672f2
Riron <orioncharlier@gmail.com>
2019-05-29 18:55:57 +0200

    front - load transport forms

--- front/src/dashboard/transport/Transport.tsx
+++ front/src/dashboard/transport/Transport.tsx
@@ -1,11 +1,35 @@
 import React from "react";
 import { Me } from "../../login/model";
 import "./Transport.scss";
+import { Query, QueryResult } from "react-apollo";
+import gql from "graphql-tag";
+import { Form } from "../../form/model";
 
 type Props = {
   me: Me;
   siret: string;
 };
+
+export const GET_TRANSPORT_SLIPS = gql`
+  query GetSlips($siret: String, $type: FormType) {
+    forms(siret: $siret, type: $type) {
+      id
+      readableId
+      createdAt
+      emitter {
+        company {
+          name
+          siret
+        }
+      }
+      wasteDetails {
+        code
+        name
+        quantity
+      }
+    }
+  }
+`;
 
 export default function Transport({ me, siret }: Props) {
   return (
@@ -15,16 +39,28 @@ export default function Transport({ me, siret }: Props) {
       </div>
 
       <div>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Colonne 1</th>
-              <th>Colonne 2</th>
-              <th>Colonne 3</th>
-              <th>Colonne 4</th>
-            </tr>
-          </thead>
-        </table>
+        <Query
+          query={GET_TRANSPORT_SLIPS}
+          variables={{ siret, type: "TRANSPORTER" }}
+        >
+          {({ loading, error, data }: QueryResult<{ forms: Form[] }>) => {
+            if (loading) return "Chargement...";
+            if (error || !data) return "Erreur...";
+
+            return (
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Colonne 1</th>
+                    <th>Colonne 2</th>
+                    <th>Colonne 3</th>
+                    <th>Colonne 4</th>
+                  </tr>
+                </thead>
+              </table>
+            );
+          }}
+        </Query>
       </div>
     </>
   );
