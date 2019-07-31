504dbe8af628dd36e9e71d83fd7d1e2f6ca9c767
Riron <orioncharlier@gmail.com>
2019-07-31 13:48:42 +0200

    back - fix readable id glitch on new form

--- back/src/forms/readable-id.ts
+++ back/src/forms/readable-id.ts
@@ -1,16 +1,21 @@
 import { Context } from "../types";
+import { string } from "yup";
 
 export async function getReadableId(context: Context) {
   const beginningOfYear = new Date(new Date().getFullYear(), 0, 1);
 
-  const forms = await context.prisma.forms({
-    where: { createdAt_gte: beginningOfYear }
+  const mostRecentForms = await context.prisma.forms({
+    orderBy: "readableId_DESC",
+    first: 10
   });
+
+  const latestFormReadableId = mostRecentForms[0].readableId;
+  const latestReadableIdAsNumber = decodeNumber(latestFormReadableId.slice(-8));
 
   return `TD-${beginningOfYear
     .getFullYear()
     .toString()
-    .slice(-2)}-${encodeNumber(forms.length + 1)}`;
+    .slice(-2)}-${encodeNumber(latestReadableIdAsNumber + 1)}`;
 }
 
 // AAA12345
@@ -29,4 +34,19 @@ function encodeNumber(n): string {
   }
 
   return letters.reverse().join("");
+}
+
+function decodeNumber(n: string): number {
+  const letters = n.slice(0, 3);
+  const numbers = parseInt(n.slice(-5), 10);
+
+  const multiplier = letters
+    .split("")
+    .reduce(
+      (prev, cur, idx) =>
+        prev + Math.pow(26, 2 - idx) * (cur.charCodeAt(0) - 65),
+      0
+    );
+
+  return numbers + 99999 * multiplier;
 }
