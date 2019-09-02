081e2231ea7ced64f5fa84ab971cb9fa90f9b743
benoitguigal <benoit.guigal@gmail.com>
2019-09-02 13:13:00 +0200

    back - debug update script set-company-name-gerepid-naf

--- back/prisma/scripts/set-company-name-gerepid-naf.ts
+++ back/prisma/scripts/set-company-name-gerepid-naf.ts
@@ -8,41 +8,44 @@ import { prisma } from "../../src/generated/prisma-client";
   `Populate fields name, gerepId and codeNaf on company records for analytics purpose`
 )
 export class SetCampanyNameGerepNafUpdater implements Updater {
-  run() {
+  async run() {
     console.info("Starting script to populate name, gerepId and codeNaf on companies");
 
     try {
+      const companies = await prisma.companies();
 
-      return prisma.companies().then(async companies => {
+      for (const company of companies) {
 
-        for (const company of companies) {
+        console.log(`Populating fields for company ${company.siret}`);
 
-          try {
-            const response = await axios.get(`http://td-insee:81/siret/${company.siret}`);
+        try {
+          const response = await axios.get(`http://td-insee:81/siret/${company.siret}`);
 
-            if (response.status == 200) {
-              const companyInfo = response.data;
+          if (response.status == 200) {
 
-              await prisma.updateCompany({
-                data: {
-                  name: companyInfo.name,
-                  gerepId: companyInfo.codeS3ic,
-                  codeNaf: companyInfo.naf
-                },
-                where: {
-                  id: company.id
-                }
-              });
-            } else {
-              console.log(
-                `Received status code ${response.status}`,
-                response)
-            }
-          } catch (error) {
-            console.log(error)
+            const companyInfo = response.data;
+
+            console.log(companyInfo);
+
+            await prisma.updateCompany({
+              data: {
+                name: companyInfo.name,
+                gerepId: companyInfo.codeS3ic,
+                codeNaf: companyInfo.naf
+              },
+              where: {
+                id: company.id
+              }
+            });
+          } else {
+            console.log(
+              `Received status code ${response.status}`,
+              response)
           }
+        } catch (error) {
+          console.log(error)
         }
-      });
+      }
 
     } catch (err) {
       console.error("☠ Something went wrong during the update", err);
