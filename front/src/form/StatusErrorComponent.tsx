eb4837b5248c6e7c9f1648d68224c54b3dd49e18
Laurent Paoletti <lp@providenz.fr>
2019-09-16 09:25:13 +0200

    Display error message when siret is already in db
    Error message was returned from api and passed to formik status top
    level object.
    Wizard component made it hard to retrieve status from wizard children
    component without messing wizard genericity.
    StatusErrorComponent now leverages Formik connect util to retrieve status and display
    it to user.

---
+++ front/src/form/StatusErrorComponent.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { connect } from "formik";
+
+/**
+ * Retrieve status property from Formik context and display it as an error message if not empty.
+ * Used in child components (eg. Wizard Pages) when accessing parent context is not trivial.
+ *
+ */
+const StatusErrorMessage = (props: any) => {
+  return props.formik.status ? (
+    <div className="input-error-message">{props.formik.status}</div>
+  ) : null;
+};
+
+export default connect(StatusErrorMessage);
