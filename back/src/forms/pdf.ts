3abf90a0df3d13cf513119ee6d4d600922d964d5
Laurent Paoletti <lp@providenz.fr>
2019-09-19 15:35:10 +0200

    Convert a pdf form to base64 iot be mailed

--- back/src/forms/pdf.ts
+++ back/src/forms/pdf.ts
@@ -24,3 +24,36 @@ export const pdfHandler = async (req, res) => {
       res.status(500).send("Erreur lors de la génération du PDF");
     });
 };
+
+/**
+ * Build a pdf converted to base64 string to be sent via Mailjet API
+ * @param {string} id - form id
+ * @return {Promise}
+ */
+export const pdfAttachment = async id => {
+  // retrieve form
+  const form = await prisma.form({ id });
+
+  if (form.status === "DRAFT") {
+    console.log("Impossible de générer un PDF pour un brouillon.");
+    return;
+  }
+
+  const appendix2Forms = await prisma.form({ id }).appendix2Forms();
+  // Request pdf to td-pdf service, then build a base64 string and return an object containing
+  // file and its name (readableID + pdf extension)
+  return axios
+    .post(
+      "http://td-pdf:3201/pdf",
+      { ...form, appendix2Forms },
+      { responseType: "arraybuffer" }
+    )
+    .then(response => ({
+      file: Buffer.from(response.data, "binary").toString("base64"),
+      name: `${form.readableId}.pdf`
+    }))
+
+    .catch(err => {
+      console.log("Erreur lors de la génération du PDF", err);
+    });
+};
