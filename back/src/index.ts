8e60e6d1b87b78bbd38f2de0f3ae8c177aaf773d
Laurent Paoletti <lp@providenz.fr>
2019-09-26 16:22:17 +0200

    Sentry setup

--- back/src/index.ts
+++ back/src/index.ts
@@ -7,9 +7,12 @@ import { userActivationHandler } from "./users/activation";
 import { pdfHandler } from "./forms/pdf";
 import { initSubsriptions } from "./subscriptions";
 import { csvExportHandler } from "./forms/exports/handler";
+import { sentry } from "graphql-middleware-sentry";
+import { CaptureConsole } from "@sentry/integrations";
 
 const port = process.env.port || 80;
 const isProd = process.env.NODE_ENV === "production";
+const sentryDsn = process.env.SENTRY_DSN;
 
 const typesArray = fileLoader(`${__dirname}/**/*.graphql`, { recursive: true });
 const typeDefs = mergeTypes(typesArray, { all: true });
@@ -22,15 +25,45 @@ const resolvers = mergeResolvers(resolversArray);
 const rulesArray = fileLoader(`${__dirname}/**/rules.ts`, { recursive: true });
 const permissions = shield(rulesArray.reduce((prev, cur) => merge(prev, cur)));
 
+/**
+ * Sentry configuration
+ * Capture console.error statements
+ */
+const sentryMiddleware = () =>
+  sentry({
+    config: {
+      dsn: sentryDsn,
+      environment: process.env.NODE_ENV,
+      integrations: [new CaptureConsole({ levels: ["error"] })]
+    },
+    withScope: (scope, error, context: any) => {
+      scope.setExtra("body", context.request.body);
+      scope.setExtra("origin", context.request.headers.origin);
+      scope.setExtra("user-agent", context.request.headers["user-agent"]);
+    }
+  });
+
+/**
+ * Build an array of yoga-compatible middlewares
+ * Only sentry for now
+ */
+const getMiddlewares = () => {
+  let middlewares = [];
+  if (!!sentryDsn) { // not set in dev
+    middlewares.push(sentryMiddleware());
+  }
+  return middlewares;
+};
+
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
-  //middlewares: [permissions],
+  middlewares: getMiddlewares(),
   context: request => ({
     ...request,
     prisma
   })
-});
+} as any);
 
 server.express.get("/ping", (_, res) => res.send("Pong!"));
 server.express.get("/userActivation", userActivationHandler);
