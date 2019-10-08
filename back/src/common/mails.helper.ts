8835885032cf125e5a49d99b6904fc986d9477c2
Laurent Paoletti <lp@providenz.fr>
2019-10-08 08:48:00 +0200

    Add a few formatting helpers
    Node Intl formatting is broken due to Node image missing relevant icus,
    we now use a small util to properly format FR dates.

--- back/src/common/mails.helper.ts
+++ back/src/common/mails.helper.ts
@@ -28,3 +28,38 @@ export function addContact({ email, name }: { email: string; name: string }) {
       console.error("Error while pushing new contact to mail service", err)
     );
 }
+
+const unwantedChars = /\*|\//g;
+/**
+ * Remove * and / special chars appearing on some individual companies
+ * @param name string
+ */
+export const cleanupSpecialChars = (name: string): string => {
+  if (!name) {
+    return "";
+  }
+  return name.replace(unwantedChars, " ").trim();
+};
+
+const frMonth = [
+  "janvier",
+  "février",
+  "mars",
+  "avril",
+  "mai",
+  "juin",
+  "juillet",
+  "août",
+  "septembre",
+  "octobre",
+  "novembre",
+  "décembre"
+];
+/**
+ * Format a date as fr verbose format
+ * @param someDate Date
+ */
+export const toFrFormat = (someDate: Date): string =>
+  `${someDate.getDate()} ${
+    frMonth[someDate.getMonth()]
+  } ${someDate.getFullYear()}`;
