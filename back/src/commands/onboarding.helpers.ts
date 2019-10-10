7bc6777e6ae7993b6fce8358d9894330d00fd860
Laurent Paoletti <lp@providenz.fr>
2019-10-10 10:05:58 +0200

    Add missing returns

--- back/src/commands/onboarding.helpers.ts
+++ back/src/commands/onboarding.helpers.ts
@@ -50,13 +50,11 @@ export const sendOnboardingEmails = async (daysAgo: number, emailFunction) => {
 /**
  * Send first step onboarding email to active users who suscribed yesterday
  */
-export const sendOnboardingFirstStepMails = () => {
+export const sendOnboardingFirstStepMails = () =>
   sendOnboardingEmails(1, userMails.onboardingFirstStep);
-};
 
 /**
  * Send second step onboarding email to active users who suscribed 3 days ago
  */
-export const sendOnboardingSecondStepMails = () => {
+export const sendOnboardingSecondStepMails = () =>
   sendOnboardingEmails(3, userMails.onboardingSecondStep);
-};
