b9f59a72a256cffe57324ea4ba22366bd8378457
Laurent Paoletti <lp@providenz.fr>
2019-11-19 11:41:01 +0100

    Fix refused forms display

--- front/src/dashboard/slips/slips-actions/next-step.ts
+++ front/src/dashboard/slips/slips-actions/next-step.ts
@@ -8,17 +8,27 @@ export enum SlipTabs {
   HISTORY
 }
 
+enum FormStatus {
+  DRAFT = "DRAFT",
+  SEALED = "SEALED",
+  SENT = "SENT",
+  NO_TRACEABILITY = "NO_TRACEABILITY",
+  PROCESSED = "PROCESSED",
+  RECEIVED = "RECEIVED",
+  REFUSED = "REFUSED"
+}
+
 export function getTabForms(tab: SlipTabs, forms: Form[], currentUser: Me) {
   switch (tab) {
     case SlipTabs.DRAFTS:
       return forms
-        .filter((f: Form) => f.status === "DRAFT")
+        .filter((f: Form) => f.status === FormStatus.DRAFT)
         .sort((a: any, b: any) => a.createdAt - b.createdAt);
     case SlipTabs.TO_SIGN:
       return forms
         .filter(
           (f: Form) =>
-            f.status !== "DRAFT" && getNextStep(f, currentUser) != null
+            f.status !== FormStatus.DRAFT && getNextStep(f, currentUser) != null
         )
         .sort((a: any, b: any) =>
           a.status > b.status
@@ -28,12 +38,15 @@ export function getTabForms(tab: SlipTabs, forms: Form[], currentUser: Me) {
             : -1
         );
     case SlipTabs.STATUS:
+      // filter from whose status is not DRAFT|PROCESSED|REFUSED and from which there is no next step for current user
       return forms
         .filter(
           (f: Form) =>
-            f.status !== "DRAFT" &&
-            f.status !== "PROCESSED" &&
-            getNextStep(f, currentUser) == null
+            !(<any>[
+              FormStatus.DRAFT,
+              FormStatus.PROCESSED,
+              FormStatus.REFUSED
+            ]).includes(f.status) && getNextStep(f, currentUser) == null
         )
         .sort((a: any, b: any) =>
           a.status > b.status
@@ -43,11 +56,18 @@ export function getTabForms(tab: SlipTabs, forms: Form[], currentUser: Me) {
             : -1
         );
     case SlipTabs.HISTORY:
-      return forms
-        .filter(
-          (f: Form) => ["PROCESSED", "NO_TRACEABILITY"].indexOf(f.status) !== -1
-        )
-        .sort((a: any, b: any) => a.createdAt - b.createdAt);
+      return (
+        forms
+          // filter from whose status is   PROCESSED|NO_TRACEABILITY|REFUSED
+          .filter((f: Form) =>
+            (<any>[
+              FormStatus.PROCESSED,
+              FormStatus.NO_TRACEABILITY,
+              FormStatus.REFUSED
+            ]).includes(f.status)
+          )
+          .sort((a: any, b: any) => a.createdAt - b.createdAt)
+      );
   }
 }
 
@@ -61,14 +81,14 @@ export function getNextStep(form: Form, currentUser: Me) {
       .map(c => c.siret)
       .indexOf(form.recipient.company.siret) > -1;
 
-  if (form.status === "DRAFT") return "SEALED";
+  if (form.status === FormStatus.DRAFT) return FormStatus.SEALED;
 
   if (currentUserIsEmitter) {
-    if (form.status === "SEALED") return "SENT";
+    if (form.status === FormStatus.SEALED) return FormStatus.SENT;
     if (!currentUserIsRecipient) return null;
   }
 
-  if (form.status === "SENT") return "RECEIVED";
-  if (form.status === "RECEIVED") return "PROCESSED";
+  if (form.status === FormStatus.SENT) return FormStatus.RECEIVED;
+  if (form.status === FormStatus.RECEIVED) return FormStatus.PROCESSED;
   return null;
 }
