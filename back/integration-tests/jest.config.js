a2f608f3d3acd05000a9bd271905d9ef58632373
Riron <orioncharlier@gmail.com>
2019-12-11 12:31:31 +0100

    `integration` suffix for integrations tests

--- back/integration-tests/jest.config.js
+++ back/integration-tests/jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node"
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/**/?(*.)+(integration).[jt]s?(x)"],
+  rootDir: '../src'
 };
