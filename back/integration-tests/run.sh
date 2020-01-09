339d2ca0e4bc4747ed34edb9baecfb6f40e4c658
Laurent Paoletti <lp@providenz.fr>
2020-01-09 23:38:30 +0100

    Update tests launcher

--- back/integration-tests/run.sh
+++ back/integration-tests/run.sh
@@ -1,24 +1,32 @@
 #!/bin/sh
 
 export NODE_ENV=test
-export PRISMA_ENDPOINT=http://localhost:4467/default/staging
+export PRISMA_ENDPOINT=http://prisma:4467/default/staging
 export PRISMA_SECRET=any_secret
 export JWT_SECRET=any_secret
 export BACK_PORT=8383
 
+echo ">> Running integration test 🚀..."
 echo ">> Starting containers..."
 docker-compose up --build -d
 
+# We curl prisma on exposed port 4466
+if [ "$(curl -sL -w '%{http_code}' http://prisma:4466 -o /dev/null)" = "200" ]; then
+    echo ">> Prisma is up 👍"
+else
+    echo ">> Waiting for Prisma to wake up ⌚..."
+    sleep 5
+fi
+
 echo ">> Deploy to prisma..."
-cd ../prisma
-npx prisma deploy
-npx prisma seed -r
 
+docker exec -it $(docker ps -qf "name=td-api") npx prisma deploy
+docker exec -it $(docker ps -qf "name=td-api") npx prisma seed -r
 echo ">> Run tests..."
-cd ..
-npx jest --config ./integration-tests/jest.config.js  -i --forceExit --detectOpenHandles
 
-echo ">> Stoping containers..."
-cd ./integration-tests
+docker exec -it $(docker ps -qf "name=td-api") npx jest --config integration.jest.config.js  -i --forceExit --detectOpenHandles
+
+echo ">> Stopping containers 🛏️ ..."
+
 docker-compose stop
 
