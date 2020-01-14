a87bfb1b12ea5284823c3edc2c125c34c2c43e67
Laurent Paoletti <lp@providenz.fr>
2020-01-14 08:59:27 +0100

    Use prisma reset instead of seed

--- back/integration-tests/run.sh
+++ back/integration-tests/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export NODE_ENV=test
+export API_HOST=api-td.local
 export PRISMA_ENDPOINT=http://prisma:4467/default/staging
 export PRISMA_SECRET=any_secret
 export COMPOSE_PROJECT_NAME=integration
@@ -20,7 +21,7 @@ fi
 echo ">> Deploy to prisma..."
 
 docker exec -it $(docker ps -qf "name=td-api") npx prisma deploy
-docker exec -it $(docker ps -qf "name=td-api") npx prisma seed -r
+docker exec -it $(docker ps -qf "name=td-api") npx prisma reset --force
 echo ">> Run tests..."
 
 docker exec -it $(docker ps -qf "name=td-api") npx jest --config integration.jest.config.js  -i --forceExit --detectOpenHandles
