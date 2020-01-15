f9c4699d8a4a98ac5ecc6a3fcf795cc16fadc50b
Riron <orioncharlier@gmail.com>
2020-01-16 00:21:34 +0100

    Fix mutation schemas

--- back/src/forms/mutations/mark-as.ts
+++ back/src/forms/mutations/mark-as.ts
@@ -13,8 +13,12 @@ export async function markAsSealed(_, { id }, context: Context) {
 export const markAsSent = {
   getValidationSchema: () =>
     object().shape({
-      sentAt: date().required("Vous devez saisir une date d'envoi."),
-      sentBy: string().required("Vous devez saisir un responsable de l'envoi.")
+      sentInfo: object({
+        sentAt: date().required("Vous devez saisir une date d'envoi."),
+        sentBy: string().required(
+          "Vous devez saisir un responsable de l'envoi."
+        )
+      })
     }),
   resolve: (_, { id, sentInfo }, context: Context) =>
     transitionForm(
@@ -27,16 +31,18 @@ export const markAsSent = {
 export const markAsReceived = {
   getValidationSchema: () =>
     object().shape({
-      isAccepted: boolean().required(
-        "Vous devez préciser si vous acceptez ou non le déchet."
-      ),
-      receivedBy: string().required(
-        "Vous devez saisir un responsable de la réception."
-      ),
-      receivedAt: date().required("Vous devez saisir une date de réception."),
-      quantityReceived: number().positive(
-        "Vous devez saisir une quantité reçue supérieure à 0."
-      )
+      receivedInfo: object({
+        isAccepted: boolean().required(
+          "Vous devez préciser si vous acceptez ou non le déchet."
+        ),
+        receivedBy: string().required(
+          "Vous devez saisir un responsable de la réception."
+        ),
+        receivedAt: date().required("Vous devez saisir une date de réception."),
+        quantityReceived: number().positive(
+          "Vous devez saisir une quantité reçue supérieure à 0."
+        )
+      })
     }),
   resolve: (_, { id, receivedInfo }, context: Context) =>
     transitionForm(
@@ -49,20 +55,24 @@ export const markAsReceived = {
 export const markAsProcessed = {
   getValidationSchema: () =>
     object().shape({
-      processingOperationDone: string().matches(
-        /(R|D)\s\d{1,2}/,
-        "Cette opération de traitement n'existe pas."
-      ),
-      processingOperationDescription: string().required(
-        "Vous devez renseigner la description de l'opération."
-      ),
-      processedBy: string().required(
-        "Vous devez saisir un responsable de traitement."
-      ),
-      processedAt: date().required("Vous devez saisir la date de traitement."),
-      nextDestinationProcessingOperation: string().nullable(true),
-      nextDestinationDetails: string().nullable(true),
-      noTraceability: boolean().nullable(true)
+      processedInfo: object({
+        processingOperationDone: string().matches(
+          /(R|D)\s\d{1,2}/,
+          "Cette opération de traitement n'existe pas."
+        ),
+        processingOperationDescription: string().required(
+          "Vous devez renseigner la description de l'opération."
+        ),
+        processedBy: string().required(
+          "Vous devez saisir un responsable de traitement."
+        ),
+        processedAt: date().required(
+          "Vous devez saisir la date de traitement."
+        ),
+        nextDestinationProcessingOperation: string().nullable(true),
+        nextDestinationDetails: string().nullable(true),
+        noTraceability: boolean().nullable(true)
+      })
     }),
   resolve: (_, { id, processedInfo }, context: Context) =>
     transitionForm(
@@ -75,21 +85,27 @@ export const markAsProcessed = {
 export const signedByTransporter = {
   getValidationSchema: () =>
     object().shape({
-      sentAt: date().required("Vous devez saisir une date d'envoi."),
-      signedByTransporter: boolean().required(
-        "Voud devez indiquer si le transporteur a signé."
-      ),
-      securityCode: number().nullable(true),
-      sentBy: string().required("Vous devez saisir un responsable de l'envoi."),
-      signedByProducer: boolean().required(
-        "Voud devez indiquer si le producteur a signé."
-      ),
+      signingInfo: object({
+        sentAt: date().required("Vous devez saisir une date d'envoi."),
+        signedByTransporter: boolean().required(
+          "Voud devez indiquer si le transporteur a signé."
+        ),
+        securityCode: number().nullable(true),
+        sentBy: string().required(
+          "Vous devez saisir un responsable de l'envoi."
+        ),
+        signedByProducer: boolean().required(
+          "Voud devez indiquer si le producteur a signé."
+        ),
 
-      packagings: array().of(string().matches(/(FUT|GRV|CITERNE|BENNE|AUTRE)/)),
-      quantity: number().positive(
-        "Vous devez saisir une quantité envoyée supérieure à 0."
-      ),
-      onuCode: string().required("Vous devez saisir un code ONU.")
+        packagings: array().of(
+          string().matches(/(FUT|GRV|CITERNE|BENNE|AUTRE)/)
+        ),
+        quantity: number().positive(
+          "Vous devez saisir une quantité envoyée supérieure à 0."
+        ),
+        onuCode: string().required("Vous devez saisir un code ONU.")
+      })
     }),
   resolve: (_, { id, signingInfo }, context: Context) => {
     const input = {
