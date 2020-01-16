654aef72ded111af492978a329dcc9396d5e0d8a
Laurent Paoletti <lp@providenz.fr>
2020-01-16 08:22:27 +0000

    Update integrtaion test runner
    - Close exposed ports
    - Query prisma endpoint from container

--- back/integration-tests/run.sh
+++ back/integration-tests/run.sh
@@ -10,20 +10,13 @@ echo ">> Running integration test 🚀..."
 echo ">> Starting containers..."
 docker-compose up --build -d
 
-# We curl prisma on exposed port 4466
-if [ "$(curl -sL -w '%{http_code}' http://prisma:4466 -o /dev/null)" = "200" ]; then
-    echo ">> Prisma is up 👍"
-else
-    echo ">> Waiting for Prisma to wake up ⌚..."
-    sleep 5
-fi
-
 echo ">> Deploy to prisma..."
 
+docker exec -it $(docker ps -qf "name=td-api") bash /usr/src/app/integration-tests/wait-for-prisma.sh
 docker exec -it $(docker ps -qf "name=td-api") npx prisma deploy
 docker exec -it $(docker ps -qf "name=td-api") npx prisma reset --force
-echo ">> Run tests..."
 
+echo ">> Run tests..."
 docker exec -it $(docker ps -qf "name=td-api") npx jest --config integration.jest.config.js  -i --forceExit --detectOpenHandles
 
 echo ">> Stopping containers 🛏️ ..."
