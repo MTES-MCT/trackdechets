dff93f8edc9dc558bc93d69908d311857a48b28f
Laurent Paoletti <lp@providenz.fr>
2020-01-20 21:50:34 +0000

    Add new form fields to handle partial refusal

---
+++ back/prisma/scripts/set-wasteacceptationstatus.ts
@@ -0,0 +1,38 @@
+import { Updater, registerUpdater } from ".";
+import {
+  prisma,
+  WasteAcceptationStatus
+} from "../../src/generated/prisma-client";
+
+@registerUpdater(
+  "Set wasteAcceptationStatus",
+  `The wasteAcceptationStatus properties was added to replace isAccepted`,
+  true
+)
+export class SetFormUpdater implements Updater {
+  async run() {
+    console.info("Starting script to fill empty wasteAcceptationStatuses...");
+
+    try {
+      const forms = await prisma.forms({
+        where: { isAccepted_not: null, wasteAcceptationStatus: null }
+      });
+
+      for (let frm of forms) {
+        let wasteAcceptationStatus =
+          frm.isAccepted === true ? "ACCEPTED" : "REFUSED";
+        await prisma.updateForm({
+          where: { id: frm.id },
+          data: {
+            wasteAcceptationStatus: wasteAcceptationStatus as WasteAcceptationStatus
+          }
+        });
+      }
+      console.info(`${forms.length} forms updated`);
+      console.info(`⚡ Update done.`);
+    } catch (err) {
+      console.error("☠ Something went wrong during the update", err);
+      throw new Error();
+    }
+  }
+}
