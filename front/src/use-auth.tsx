bca3d6420aff10695fa271c4fe72766ed1887494
benoitguigal <benoit.guigal@gmail.com>
2020-01-30 09:28:18 +0100

    front - use fetch instead of axios

--- front/src/use-auth.tsx
+++ front/src/use-auth.tsx
@@ -1,20 +1,20 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import "whatwg-fetch";
 
 export function useAuth() {
   const [loading, setIsLoading] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
   useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_API_ENDPOINT}/isAuthenticated`, {
-        withCredentials: true
-      })
-      .then(response => {
-        setIsAuthenticated(response.data.isAuthenticated);
+    const GETisAuthenticated = `${process.env.REACT_APP_API_ENDPOINT}/isAuthenticated`;
+    fetch(GETisAuthenticated, { credentials: "include" })
+      .then(res => res.json())
+      .then(data => {
+        const { isAuthenticated } = data;
+        setIsAuthenticated(isAuthenticated);
         setIsLoading(false);
       })
-      .catch(_ => {
+      .catch(err => {
         setIsAuthenticated(false);
         setIsLoading(false);
       });
