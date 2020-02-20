c330d2bbc2dda9237dc73ccb5f699089cf1e8407
benoitguigal <benoit.guigal@gmail.com>
2020-02-20 18:56:50 +0100

    back - add a script to output graphql schema documentation in Markdown

---
+++ back/src/scripts/graphql-markdown.ts
@@ -0,0 +1,21 @@
+import { renderSchema, schemaToJSON } from "graphql-markdown";
+import { schema } from "../server";
+import * as graphql from "graphql";
+import * as fs from "fs";
+
+// Render GraphQL schema to markdown
+// and save as a page in the documentation folder
+
+const content = [];
+
+const options = {
+  printer: (txt: string) => content.push(txt)
+};
+
+const filePath = "documentation/api-reference.md";
+
+schemaToJSON(schema, { graphql }).then(s => {
+  renderSchema(s, options);
+  fs.writeFileSync(filePath, content.join("\n"));
+  process.exit(0);
+});
