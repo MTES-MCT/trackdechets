98e34ec41f5cacefad564ff99ac4bda0d9cd95d1
benoitguigal <benoit.guigal@gmail.com>
2020-03-18 13:33:44 +0100

    doc - modify splash container background color

--- doc/website/pages/en/index.js
+++ doc/website/pages/en/index.js
@@ -21,7 +21,7 @@ class HomeSplash extends React.Component {
     const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
 
     const SplashContainer = props => (
-      <div className="homeContainer">
+      <div className="homeContainer lightBackground">
         <div className="homeSplashFade">
           <div className="wrapper homeWrapper">{props.children}</div>
         </div>
