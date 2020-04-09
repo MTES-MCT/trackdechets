d2122a911b04b5b66b5690d5e1cc646b6ac69784
benoitguigal <benoit.guigal@gmail.com>
2020-04-09 10:25:29 +0200

    back - add comments to graphql-markdown-plugin.js

--- back/graphql-markdown-plugin.js
+++ back/graphql-markdown-plugin.js
@@ -1,6 +1,14 @@
 const { schemaToJSON, renderSchema } = require("graphql-markdown");
 const graphql = require("graphql");
 
+/**
+ * GraphQL codegen plugin used to generate the API reference
+ * for the documentation in markdown. This script is referenced
+ * from the config file codegen.yml.
+ * See https://graphql-code-generator.com/docs/custom-codegen/write-your-plugin
+ * for more information about graphql codegen plugins
+ */
+
 // Docusaurus use this annotation to generate markdown pages
 const prologue = `---
 id: api-reference
