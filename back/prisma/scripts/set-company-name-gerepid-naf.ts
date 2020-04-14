6b89d96df384bed5177e8a8b7b3fe508722dcc1c
benoitguigal <benoit.guigal@gmail.com>
2020-04-14 12:02:21 +0200

    back - fix scripts using td-insee

--- back/prisma/scripts/set-company-name-gerepid-naf.ts
+++ back/prisma/scripts/set-company-name-gerepid-naf.ts
@@ -1,14 +1,14 @@
-import axios from "axios";
 import { Updater, registerUpdater } from ".";
 import { prisma } from "../../src/generated/prisma-client";
+import { searchCompany } from "../../src/companies/sirene";
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 @registerUpdater(
-  "Set company name, gerepid and code naf",
-  `Populate fields name, gerepId and codeNaf on company records for analytics purpose`,
+  "Set company name and code naf",
+  `Populate fields name and codeNaf on company records for analytics purpose`,
   false
 )
 export class SetCampanyNameGerepNafUpdater implements Updater {
@@ -24,28 +24,17 @@ export class SetCampanyNameGerepNafUpdater implements Updater {
         console.log(`Populating fields for company ${company.siret}`);
 
         try {
-          const response = await axios.get(
-            `http://td-insee:81/siret/${company.siret}`
-          );
+          const companyInfo = await searchCompany(company.siret);
 
-          if (response.status === 200) {
-            const companyInfo = response.data;
-
-            console.log(companyInfo);
-
-            await prisma.updateCompany({
-              data: {
-                name: companyInfo.name,
-                gerepId: companyInfo.codeS3ic,
-                codeNaf: companyInfo.naf
-              },
-              where: {
-                id: company.id
-              }
-            });
-          } else {
-            console.log(`Received status code ${response.status}`, response);
-          }
+          await prisma.updateCompany({
+            data: {
+              name: companyInfo.name,
+              codeNaf: companyInfo.naf
+            },
+            where: {
+              id: company.id
+            }
+          });
         } catch (error) {
           console.log(error);
         }
