e73466f93575945cb54a9356ee667a0b78309bd5
Riron <orioncharlier@gmail.com>
2020-05-22 10:19:00 +0200

    tslint removal
    It's deprecated

---
+++ back/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "no-console": 0,
+  },
+};
