9e552ca0d6dbee41f284f20fcd393d44273378ce
gabinaureche <hello@gabinaureche.com>
2020-06-25 17:23:35 +0200

    Revert changes to context.user

--- back/src/types.ts
+++ back/src/types.ts
@@ -1,5 +1,5 @@
 import { ExpressContext } from "apollo-server-express/dist/ApolloServer";
 
 export type GraphQLContext = ExpressContext & {
-  user: Express.User | undefined;
+  user: Express.User;
 };
