9425bd8451991b71c71c85f0b8ecac1cf6d17f2e
benoitguigal <benoit.guigal@gmail.com>
2020-07-01 16:18:47 +0200

    remove info box for registre

--- front/src/dashboard/exports/Exports.tsx
+++ front/src/dashboard/exports/Exports.tsx
@@ -21,12 +21,6 @@ export default function Exports({ companies }: IProps) {
   return (
     <div className="tw-p-6">
       <h2>Exporter un registre</h2>
-      <p className="notification success">
-        Vous avez la possibilité de télécharger un registre des déchets entrants
-        et sortants de votre entreprise. Cet export est un document CSV au
-        format UTF-8. Assurez vous que vous l'ouvrez dans le bon format pour
-        éviter les problèmes d'accents.
-      </p>
       <ExportsForm
         companies={filter(ExportsForm.fragments.company, companies)}
       />
