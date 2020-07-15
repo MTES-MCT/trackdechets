c1c070af010eeb198f128191e155502315921bbd
benoitguigal <benoit.guigal@gmail.com>
2020-07-15 08:47:33 +0200

    customize exports filename

--- back/src/forms/exports/handler.ts
+++ back/src/forms/exports/handler.ts
@@ -5,6 +5,7 @@ import { QueryFormsRegisterArgs } from "../../generated/graphql/types";
 import { formsReader, formsTransformer } from "./streams";
 import { formsWhereInput } from "./where-inputs";
 import { formFragment } from "./fragments";
+import { getExportsFileName } from "./filename";
 
 /**
  * Download handler for forms register
@@ -29,9 +30,15 @@ export async function downloadFormsRegister(
   const reader = formsReader({ whereInput, fragment });
   const transformer = formsTransformer();
 
+  const filename = getExportsFileName(
+    args.exportType,
+    args.sirets,
+    args.wasteCode
+  );
+
   switch (args.exportFormat) {
     case "CSV": {
-      res.set("Content-disposition", "attachment; filename=export.csv");
+      res.set("Content-disposition", `attachment; filename=${filename}.csv`);
       res.set("Content-Type", "text/csv");
       res.set("Transfer-Encoding", "chunked");
       const csvStream = format({ headers: true, delimiter: ";" });
@@ -39,7 +46,7 @@ export async function downloadFormsRegister(
       break;
     }
     case "XLSX": {
-      res.set("Content-Disposition", "attachment; filename=export.xlsx");
+      res.set("Content-Disposition", `attachment; filename=${filename}.xlsx`);
       const contenType =
         "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
       res.set("Content-Type", contenType);
