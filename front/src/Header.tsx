6fb5babce4d937ddee65bf4e959dba20bf3711df
Laurent Paoletti <lp@providenz.fr>
2020-09-02 14:55:52 +0000

    Redesign responsive header

--- front/src/Header.tsx
+++ front/src/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   NavLink,
   Link,
@@ -9,119 +9,170 @@ import { trackEvent } from "./tracker";
 import "./Header.scss";
 import { localAuthService } from "./login/auth.service";
 import { currentSiretService } from "./dashboard/CompanySelector";
-
+import { LogoutIcon, LoginIcon } from "./common/Icons";
 type Props = {
   isAuthenticated: boolean;
 };
 
-export default withRouter(function Header({
+export default withRouter(function Header2({
   isAuthenticated,
 }: RouteComponentProps & Props) {
   const { REACT_APP_API_ENDPOINT, REACT_APP_DEVELOPERS_ENDPOINT } = process.env;
+  const [menuOpened, toggleMenu] = useState(false);
+  const menuClass = menuOpened ? "" : "header__nav-items--hidden";
 
   return (
-    <header className="navbar" role="navigation">
-      <div className="navbar__container">
-        <div className="navbar__branding">
+    <nav id="header" className="header">
+      <div className="header__toggle">
+        <button onClick={() => toggleMenu(!menuOpened)}>
+          <svg
+            className="fill-current"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+          <span className="header__toggle-text">Menu</span>
+        </button>
+      </div>
+      <div className="header__content">
+        <div className="header__branding">
           <img
-            className="navbar__marianne"
             src="/marianne.svg"
-            alt="Ministère de la Transition écologique et solidaire"
+            alt=""
+            style={{ height: "80px", width: "80px" }}
           />
-          <Link className="navbar__home" to="/">
+          <Link to="/">
             <img
-              className="navbar__logo"
               src="/trackdechets.png"
               alt="trackdechets.data.gouv.fr"
+              style={{ height: "80px", width: "80px" }}
             />
           </Link>
         </div>
-        <nav>
-          <ul className="nav__links">
-            <li className="nav__item">
-              <a href={REACT_APP_DEVELOPERS_ENDPOINT}>Développeurs</a>
+
+        <div className="header__nav">
+          <ul className={`header__nav-items  ${menuClass}`}>
+            <li className=" header__nav-item">
               <a
+                className="header__nav-link"
                 href="https://faq.trackdechets.fr/"
                 onClick={() => trackEvent("navbar", "faq")}
               >
-                FAQ
+                Foire aux questions
               </a>
             </li>
-            <li className="nav__item">
+
+            <li className="header__nav-item">
               <a
+                className="header__nav-link"
+                href={REACT_APP_DEVELOPERS_ENDPOINT}
+              >
+                Développeurs
+              </a>
+            </li>
+
+            <li className="header__nav-item">
+              <a
+                className="header__nav-link"
                 href="https://trackdechets.beta.gouv.fr/partners"
                 onClick={() => trackEvent("navbar", "partners")}
               >
                 Partenaires
               </a>
             </li>
-            <li className="nav__item" role="separator">
-              |
-            </li>
             {isAuthenticated ? (
               <>
-                <li className="nav__item">
+                <li className="header__nav-item">
                   <NavLink
                     to={`/dashboard/${currentSiretService.getSiret()}`}
-                    activeClassName="active"
-                    onClick={() => trackEvent("navbar", "mon-espace")}
+                    activeClassName="header__nav-link--active"
+                    className="header__nav-link"
+                    onClick={() => {
+                      toggleMenu(!menuOpened);
+                      return trackEvent("navbar", "mon-espace");
+                    }}
                   >
                     Mon espace
                   </NavLink>
+                  <div className="header__nav-active"></div>
                 </li>
-                <li className="nav__item">
+                <li className="header__nav-item">
                   <NavLink
-                    activeClassName="active"
                     to="/account/info"
-                    onClick={() => trackEvent("navbar", "mon-compte")}
+                    activeClassName="header__nav-link--active"
+                    className="header__nav-link"
+                    onClick={() => {
+                      toggleMenu(!menuOpened);
+                      return trackEvent("navbar", "mon-compte");
+                    }}
                   >
                     Mon compte
                   </NavLink>
+                  <div className="header__nav-active"></div>
                 </li>
-                <li className="nav__item logout">
+                <li className="header__nav-item">
                   <form
                     name="logout"
                     action={`${REACT_APP_API_ENDPOINT}/logout`}
                     method="post"
                   >
                     <button
-                      className="link"
+                      className="header__connexion btn btn--primary"
                       onClick={() => {
                         localAuthService.locallySignOut();
                         document.forms["logout"].submit();
                         return false;
                       }}
                     >
-                      Se déconnecter
+                      <span>Se déconnecter</span>
+                      <LogoutIcon />
                     </button>
                   </form>
                 </li>
               </>
             ) : (
               <>
-                <li className="nav__item">
+                <li className="header__nav-item">
                   <NavLink
                     to="/signup"
-                    activeClassName="active"
+                    className="header__nav-link"
+                    activeClassName="header__nav-link--active"
                     onClick={() => trackEvent("navbar", "signup")}
                   >
                     S'inscrire
                   </NavLink>
+                  <div className="header__nav-active"></div>
                 </li>
-                <li className="nav__item">
+
+                <li className="header__nav-item">
                   <NavLink
                     to="/login"
-                    activeClassName="active"
+                    className="header__connexion btn btn--primary"
                     onClick={() => trackEvent("navbar", "login")}
                   >
-                    Se connecter
+                    <span>Se connecter</span>
+
+                    <LoginIcon />
                   </NavLink>
                 </li>
               </>
             )}
           </ul>
-        </nav>
+        </div>
       </div>
-    </header>
+
+      <button
+        className="header__logout-mobile "
+        onClick={() => {
+          localAuthService.locallySignOut();
+          document.forms["logout"].submit();
+          return false;
+        }}
+      >
+        <LogoutIcon />
+      </button>
+    </nav>
   );
 });
