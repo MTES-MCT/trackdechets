6534e9ac462468644457f51846af60304f5576c7
Laurent Paoletti <lp@providenz.fr>
2020-10-05 17:25:45 +0000

    Extract transport table

---
+++ front/src/dashboard/transport/TransportTable.tsx
@@ -0,0 +1,158 @@
+import React from "react";
+import SortControl from "src/dashboard/slips/SortableTableHeader";
+
+import MarkSegmentAsReadyToTakeOver from "./actions/MarkSegmentAsReadyToTakeOver";
+import PrepareSegment from "./actions/PrepareSegment";
+import TakeOverSegment from "./actions/TakeOverSegment";
+import EditSegment from "./actions/EditSegment";
+
+import DownloadPdf from "src/dashboard/slips/slips-actions/DownloadPdf";
+import { useFormsTable } from "src/dashboard/slips/use-forms-table";
+
+import TransporterInfoEdit from "./actions/TransporterInfoEdit";
+import TransportSignature from "./actions/TransportSignature";
+import styles from "./TransportTable.module.scss";
+import { Segments } from "./Segments";
+import Quicklook from "src/dashboard/slips/slips-actions/Quicklook";
+export const TransportTable = ({ forms, userSiret, refetchQuery }) => {
+  const [sortedForms, sortParams, sortBy, filter] = useFormsTable(forms);
+
+  return (
+    <table className="td-table transport-table">
+      <thead>
+        <tr className="td-table__head-tr">
+          <SortControl
+            sortFunc={sortBy}
+            fieldName="readableId"
+            caption="Numéro"
+            sortParams={sortParams}
+          />
+          <SortControl
+            sortFunc={sortBy}
+            fieldName="emitter.company.name"
+            caption="Emetteur"
+            sortParams={sortParams}
+          />
+          <SortControl
+            sortFunc={sortBy}
+            fieldName="stateSummary.recipient.name"
+            caption="Destinataire"
+            sortParams={sortParams}
+          />
+
+          <th>Déchet</th>
+          <th className={styles.hideOnMobile}>Quantité estimée</th>
+          <th colSpan={2}>Champ libre</th>
+          <th colSpan={2}>Plaque d'immatriculation</th>
+          <th>Multimodal</th>
+          <th>Action</th>
+          <th></th>
+        </tr>
+        <tr className="td-table__head-tr td-table__tr--bordered">
+          <th>
+            <input
+              type="text"
+              className="td-input"
+              onChange={e => filter("readableId", e.target.value)}
+              placeholder="Filtrer..."
+            />
+          </th>
+          <th>
+            <input
+              type="text"
+              className="td-input"
+              onChange={e => filter("emitter.company.name", e.target.value)}
+              placeholder="Filtrer..."
+            />
+          </th>
+          <th className={styles.hideOnMobile}>
+            <input
+              type="text"
+              className="td-input"
+              onChange={e =>
+                filter("stateSummary.recipient.name", e.target.value)
+              }
+              placeholder="Filtrer..."
+            />
+          </th>
+          <th>
+            <input
+              type="text"
+              className="td-input"
+              onChange={e => filter("wasteDetails.name", e.target.value)}
+              placeholder="Filtrer..."
+            />
+          </th>
+          <th className={styles.hideOnMobile}></th>
+
+          <th colSpan={7}></th>
+        </tr>
+      </thead>
+      <tbody>
+        {sortedForms.map(form => (
+          <tr key={form.id} className="td-table__tr--bordered">
+            <td>
+              <div className={styles.readableId}>
+                <DownloadPdf formId={form.id} />
+                {form.readableId}
+              </div>
+            </td>
+            <td>{form.stateSummary?.emitter?.name}</td>
+            <td className={styles.hideOnMobile}>
+              {form.stateSummary?.recipient?.name}
+            </td>
+            <td>
+              <div>{form.wasteDetails?.name}</div>
+            </td>
+            <td className={styles.hideOnMobile}>
+              {form.stateSummary?.quantity} tonnes
+            </td>
+
+            <td style={{ paddingRight: 0 }} className="tw-text-right">
+              <TransporterInfoEdit
+                form={form}
+                fieldName="customInfo"
+                verboseFieldName={"champ libre"}
+                refetchQuery={refetchQuery}
+              />
+            </td>
+            <td style={{ paddingLeft: 0 }}>
+              {form.stateSummary?.transporterCustomInfo}
+            </td>
+            <td style={{ paddingRight: 0 }} className="tw-text-right">
+              <TransporterInfoEdit
+                form={form}
+                fieldName="numberPlate"
+                verboseFieldName={"plaque d'immatriculation"}
+                refetchQuery={refetchQuery}
+              />
+            </td>
+            <td style={{ paddingLeft: 0 }}>
+              {form.stateSummary?.transporterNumberPlate}
+            </td>
+
+            <td>
+              <Segments form={form} userSiret={userSiret} />
+            </td>
+            <td className="">
+              <TransportSignature form={form} userSiret={userSiret} />
+
+              <EditSegment form={form} userSiret={userSiret} />
+
+              <PrepareSegment form={form} userSiret={userSiret} />
+              <MarkSegmentAsReadyToTakeOver form={form} userSiret={userSiret} />
+
+              <TakeOverSegment form={form} userSiret={userSiret} />
+            </td>
+            <td>
+              <Quicklook
+                formId={form.id}
+                buttonClass={`btn--no-style slips-actions__button ${styles.quicklook}`}
+              />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
