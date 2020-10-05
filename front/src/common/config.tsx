fe254b74265c98867c14dd9f8e25f337ad25bea9
Laurent Paoletti <lp@providenz.fr>
2020-10-05 16:33:29 +0000

    Common constants

---
+++ front/src/common/config.tsx
@@ -0,0 +1,11 @@
+export const MEDIA_QUERIES = {
+  mobile: 480,
+  handHeld: 900,
+};
+
+export const COLORS = {
+  white: "#ffffff",
+  blueLight: "#006be6",
+};
+
+export const tdContactEmail = process.env.REACT_TD_CONTACT_EMAIL;
