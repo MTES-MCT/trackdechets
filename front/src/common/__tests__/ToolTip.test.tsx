fa6d63bb8cd8817efcdd6b808843f3dc79fb9268
Laurent Paoletti <lp@providenz.fr>
2020-10-07 07:08:26 +0000

    Fix tooltip test

--- front/src/common/__tests__/ToolTip.test.tsx
+++ front/src/common/__tests__/ToolTip.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tooltip from "../Tooltip";
+import Tooltip from "src/common/components/Tooltip";
 import "@testing-library/jest-dom/extend-expect";
 import { render, fireEvent } from "@testing-library/react";
 
