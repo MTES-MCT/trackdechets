2dcdf6d6f1033f1ef77d8dd162dbff34ee0e3bd0
gabinaureche <hello@gabinaureche.com>
2020-11-06 14:00:34 +0100

    Clean

--- back/src/forms/resolvers/mutations/__tests__/duplicateForm.integration.ts
+++ back/src/forms/resolvers/mutations/__tests__/duplicateForm.integration.ts
@@ -3,7 +3,7 @@ import {
   userWithCompanyFactory
 } from "../../../../__tests__/factories";
 import makeClient from "../../../../__tests__/testClient";
-import { prisma, Form } from "../../../../generated/prisma-client";
+import { prisma } from "../../../../generated/prisma-client";
 import { cleanUpNotDuplicatableFieldsInForm } from "../../../form-converter";
 import { resetDatabase } from "../../../../../integration-tests/helper";
 
@@ -32,22 +32,37 @@ describe("Mutation.duplicateForm", () => {
       }
     });
 
-    const duplicateForm = (await prisma.form({
+    const duplicateForm = await prisma.form({
       id: data.duplicateForm.id
-    })) as Form;
+    });
 
     expect(cleanUpNotDuplicatableFieldsInForm(form)).toEqual(
       cleanUpNotDuplicatableFieldsInForm(duplicateForm)
     );
+  });
 
-    // check relevant statusLog is created
+  it("should create a status log", async () => {
+    const { user, company } = await userWithCompanyFactory("MEMBER");
+    const form = await formFactory({
+      ownerId: user.id,
+      opt: { emitterCompanySiret: company.siret }
+    });
+
+    const { mutate } = makeClient(user);
+    const { data } = await mutate(DUPLICATE_FORM, {
+      variables: {
+        id: form.id
+      }
+    });
+
     const statusLogs = await prisma.statusLogs({
       where: {
-        form: { id: duplicateForm.id },
+        form: { id: data.duplicateForm.id },
         user: { id: user.id },
         status: "DRAFT"
       }
     });
+
     expect(statusLogs.length).toEqual(1);
     expect(statusLogs[0].loggedAt).toBeTruthy();
   });
