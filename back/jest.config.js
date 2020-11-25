93a0e1a14c8a873192c78726e19f80266fe9209c
Riron <orioncharlier@gmail.com>
2020-11-25 12:41:23 +0100

    Unit tests

--- back/jest.config.js
+++ back/jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["<rootDir>/dist/"],
-  testMatch: ["**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)"]
+  testMatch: ["**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)"],
+  moduleNameMapper: {
+    "^src/(.*)$": "<rootDir>/src/$1",
+    "^integration-tests/(.*)$": "<rootDir>/integration-tests/$1"
+  }
 };
