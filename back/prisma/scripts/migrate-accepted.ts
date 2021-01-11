2bdcde9714c7d7ada930cc4cd797d8775b8d67c9
benoitguigal <benoit.guigal@gmail.com>
2021-01-11 17:23:40 +0100

    de-activate old data migration scripts

--- back/prisma/scripts/migrate-accepted.ts
+++ back/prisma/scripts/migrate-accepted.ts
@@ -2,24 +2,24 @@ import { Updater, registerUpdater } from "./helper/helper";
 import { prisma } from "../../src/generated/prisma-client";
 
 @registerUpdater(
-    "Migrate the forms to received forms to the new accepted status",
-    "Replace RECEIVED with ACCEPTED, and TEMP_STORED to TEMP_STORER_ACCEPTED",
-    true
+  "Migrate the forms to received forms to the new accepted status",
+  "Replace RECEIVED with ACCEPTED, and TEMP_STORED to TEMP_STORER_ACCEPTED",
+  false
 )
 export class MigrateAcceptedForms implements Updater {
-    async run() {
-        await prisma.updateManyForms({
-            where: {
-                status: "RECEIVED"
-            },
-            data: { status: "ACCEPTED" }
-        })
+  async run() {
+    await prisma.updateManyForms({
+      where: {
+        status: "RECEIVED"
+      },
+      data: { status: "ACCEPTED" }
+    });
 
-        await prisma.updateManyForms({
-            where: {
-                status: "TEMP_STORED"
-            },
-            data: { status: "TEMP_STORER_ACCEPTED" }
-        })
-    }
+    await prisma.updateManyForms({
+      where: {
+        status: "TEMP_STORED"
+      },
+      data: { status: "TEMP_STORER_ACCEPTED" }
+    });
+  }
 }
