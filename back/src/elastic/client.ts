348af8c7f38da61d405da67f30dfb7faf8fdd49e
gabinaureche <hello@gabinaureche.com>
2021-01-20 14:25:34 +0100

    Update config for clevercloud

--- back/src/elastic/client.ts
+++ back/src/elastic/client.ts
@@ -1,9 +1,9 @@
 import { Client } from "@elastic/elasticsearch";
 
 export const client = new Client({
-  node: process.env.ELASTIC_SEARCH_NODE,
+  node: process.env.ELASTIC_SEARCH_URL,
   auth: {
-    username: process.env.ELASTIC_SEARCH_USERNAME,
+    username: process.env.ELASTIC_SEARCH_USER,
     password: process.env.ELASTIC_SEARCH_PASSWORD
   }
 });
