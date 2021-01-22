6b62c71e42d99b3ac9ec34146a079c95d97014bd
benoitguigal <benoit.guigal@gmail.com>
2021-01-22 14:19:05 +0100

    fix type error with prisma client 2.15.0

--- back/src/forms/resolvers/mutations/multiModal.ts
+++ back/src/forms/resolvers/mutations/multiModal.ts
@@ -226,8 +226,7 @@ export async function prepareSegment(
 
   const segment = await prisma.transportSegment.create({
     data: {
-      form: { connect: { id } },
-
+      formId: id,
       ...nextSegmentPayload,
       previousTransporterCompanySiret: siret,
       segmentNumber: transportSegments.length + 1 // additional segments begin at index 1
