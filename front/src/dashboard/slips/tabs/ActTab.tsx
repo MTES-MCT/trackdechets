60a77375d0dec72462834c90b83e30231b0ed9de
gabinaureche <hello@gabinaureche.com>
2021-02-11 16:04:26 +0100

    Add ActTab and HistoryTab

--- front/src/dashboard/slips/tabs/ActTab.tsx
+++ front/src/dashboard/slips/tabs/ActTab.tsx
@@ -1,64 +1,40 @@
-import { useQuery, NetworkStatus } from "@apollo/client";
 import React from "react";
-import { InlineError } from "common/components/Error";
-import Loader from "common/components/Loaders";
-import { Query, QueryFormsArgs } from "generated/graphql/types";
-import { ACT_TAB_FORMS } from "./queries";
-import Slips, { SlipsColumn } from "../Slips";
-import TabContent from "./TabContent";
-import EmptyTab from "./EmptyTab";
 import { useParams } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
+import { Query } from "generated/graphql/types";
+import { formSearchResultFragment } from "common/fragments";
+import { FormSearchResultTable } from "../../FormSearchResultTable";
+
+// FIXME: missing hasNextStep
+export const SEARCH_ACTS = gql`
+  query SearchFollows($siret: String!) {
+    searchForms(
+      siret: $siret
+      status: [
+        "SENT"
+        "RECEIVED"
+        "ACCEPTED"
+        "TEMP_STORED"
+        "TEMP_STORER_ACCEPTED"
+        "RESENT"
+      ]
+    ) {
+      ...FormSearchResultFragment
+    }
+  }
+  ${formSearchResultFragment}
+`;
 
 export default function ActTab() {
   const { siret } = useParams<{ siret: string }>();
-  const { error, data, fetchMore, refetch, networkStatus } = useQuery<
-    Pick<Query, "forms">,
-    Partial<QueryFormsArgs>
-  >(ACT_TAB_FORMS, {
+  const { data } = useQuery<Pick<Query, "searchForms">>(SEARCH_ACTS, {
     variables: {
       siret,
     },
-    notifyOnNetworkStatusChange: true,
   });
 
-  if (networkStatus === NetworkStatus.loading) return <Loader />;
-  if (error) return <InlineError apolloError={error} />;
-  if (!data?.forms?.length)
-    return (
-      <EmptyTab>
-        <img src="/illu/illu_sent.svg" alt="" />
-        <h4>Il n'y a aucun bordereau à signer</h4>
-        <p>
-          Bonne nouvelle, vous n'avez aucun bordereau à signer ! Des bordereaux
-          apparaissent dans cet onglet uniquement lorsque vous avez une action à
-          effectuer dans le cadre de leur cycle de vie (envoi, réception ou
-          traitement...)
-        </p>
-      </EmptyTab>
-    );
+  // TODO: blankslate
+  // TODO: loading state
 
-  return (
-    <TabContent
-      networkStatus={networkStatus}
-      refetch={refetch}
-      forms={data.forms}
-      fetchMore={fetchMore}
-    >
-      <Slips
-        siret={siret}
-        forms={data.forms}
-        columns={[
-          SlipsColumn.READABLE_ID,
-          SlipsColumn.SENT_AT,
-          SlipsColumn.EMITTER_COMPANY_NAME,
-          SlipsColumn.RECIPIENT_COMPANY_NAME,
-          SlipsColumn.WASTE_DETAILS_CODE,
-          SlipsColumn.QUANTITY,
-          SlipsColumn.STATUS,
-          SlipsColumn.WORKFLOW_ACTION,
-          SlipsColumn.SLIPS_ACTIONS,
-        ]}
-      />
-    </TabContent>
-  );
+  return <FormSearchResultTable searchResults={data?.searchForms ?? []} />;
 }
--- front/src/dashboard/slips/tabs/HistoryTab.tsx
+++ front/src/dashboard/slips/tabs/HistoryTab.tsx
@@ -1,63 +1,32 @@
-import { useQuery, NetworkStatus } from "@apollo/client";
 import React from "react";
-import { InlineError } from "common/components/Error";
-import Loader from "common/components/Loaders";
-import { Query, QueryFormsArgs } from "generated/graphql/types";
-import { HISTORY_TAB_FORMS } from "./queries";
-import EmptyTab from "./EmptyTab";
-import Slips, { SlipsColumn } from "../Slips";
-
-import TabContent from "./TabContent";
 import { useParams } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
+import { Query } from "generated/graphql/types";
+import { formSearchResultFragment } from "common/fragments";
+import { FormSearchResultTable } from "../../FormSearchResultTable";
+
+export const SEARCH_ARCHIVES = gql`
+  query SearchFollows($siret: String!) {
+    searchForms(
+      siret: $siret
+      status: ["PROCESSED", "NO_TRACEABILITY", "REFUSED"]
+    ) {
+      ...FormSearchResultFragment
+    }
+  }
+  ${formSearchResultFragment}
+`;
 
 export default function HistoryTab() {
   const { siret } = useParams<{ siret: string }>();
-  const { error, data, fetchMore, refetch, networkStatus } = useQuery<
-    Pick<Query, "forms">,
-    Partial<QueryFormsArgs>
-  >(HISTORY_TAB_FORMS, {
+  const { data } = useQuery<Pick<Query, "searchForms">>(SEARCH_ARCHIVES, {
     variables: {
       siret,
     },
-    notifyOnNetworkStatusChange: true,
   });
 
-  if (networkStatus === NetworkStatus.loading) return <Loader />;
-  if (error) return <InlineError apolloError={error} />;
-  if (!data?.forms?.length)
-    return (
-      <EmptyTab>
-        <img src="/illu/illu_hello.svg" alt="" />
-        <h4>Il n'y a aucun bordereau en archive</h4>
-        <p>
-          Des bordereaux apparaissent dans cet onnglet lorsqu'ils terminé leur
-          cycle de vie. Ils sont alors disponible en lecture seule pour
-          consultation.
-        </p>
-      </EmptyTab>
-    );
+  // TODO: blankslate
+  // TODO: loading state
 
-  return (
-    <TabContent
-      networkStatus={networkStatus}
-      refetch={refetch}
-      forms={data.forms}
-      fetchMore={fetchMore}
-    >
-      <Slips
-        siret={siret}
-        forms={data.forms}
-        columns={[
-          SlipsColumn.READABLE_ID,
-          SlipsColumn.SENT_AT,
-          SlipsColumn.EMITTER_COMPANY_NAME,
-          SlipsColumn.RECIPIENT_COMPANY_NAME,
-          SlipsColumn.WASTE_DETAILS_CODE,
-          SlipsColumn.QUANTITY,
-          SlipsColumn.STATUS,
-          SlipsColumn.SLIPS_ACTIONS,
-        ]}
-      />
-    </TabContent>
-  );
+  return <FormSearchResultTable searchResults={data?.searchForms ?? []} />;
 }
