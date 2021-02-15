5e867c3499258104a86563f485f428a382850e54
benoitguigal <benoit.guigal@gmail.com>
2021-02-15 13:47:17 +0100

    fix markAsSent.integration.ts

--- back/src/forms/resolvers/mutations/__tests__/markAsSent.integration.ts
+++ back/src/forms/resolvers/mutations/__tests__/markAsSent.integration.ts
@@ -21,7 +21,7 @@ const MARK_AS_SENT = `
 `;
 
 describe("{ mutation { markAsSent } }", () => {
-  afterAll(() => resetDatabase());
+  afterEach(resetDatabase);
 
   it("should fail when SENT is not a possible next step", async () => {
     const { user, company: emitterCompany } = await userWithCompanyFactory(
@@ -324,7 +324,7 @@ describe("{ mutation { markAsSent } }", () => {
     expect(form.sentAt).toEqual(sentAt);
   });
 
-  test.each(["20201211", "junk", "2020-12-33"])(
+  test.each(["junk", "2020-12-33"])(
     "sentAt must be a valid date, %p is not valid",
     async dateStr => {
       const { user, company: emitterCompany } = await userWithCompanyFactory(
@@ -333,7 +333,7 @@ describe("{ mutation { markAsSent } }", () => {
 
       const recipientCompany = await companyFactory();
 
-      let form = await formFactory({
+      const form = await formFactory({
         ownerId: user.id,
         opt: {
           status: "SEALED",
@@ -344,32 +344,18 @@ describe("{ mutation { markAsSent } }", () => {
 
       const { mutate } = makeClient(user);
 
-      const { errors } = await mutate(MARK_AS_SENT, {
-        variables: {
-          id: form.id,
-          sentInfo: { sentAt: dateStr, sentBy: "John Doe" }
-        }
-      });
-      expect(errors).toEqual([
-        new UserInputError(
-          `Variable "$sentInfo" got invalid value ${dateStr} at "sentInfo.sentAt"; Expected type DateTime.
-          Seul les chaînes de caractères au format ISO 8601 sont acceptées en tant que date. Reçu ${dateStr}.`
-        )
-      ]);
-      console.log(errors);
-      form = await prisma.form.findUnique({ where: { id: form.id } });
-
-      expect(form.status).toEqual("SEALED");
-
-      // check no SEALED statusLog is created
-      const statusLogs = await prisma.statusLog.findMany({
-        where: {
-          form: { id: form.id },
-          user: { id: user.id },
-          status: "SEALED"
-        }
-      });
-      expect(statusLogs.length).toEqual(0);
+      const markAsSent = () =>
+        mutate(MARK_AS_SENT, {
+          variables: {
+            id: form.id,
+            sentInfo: { sentAt: dateStr, sentBy: "John Doe" }
+          }
+        });
+      expect(markAsSent).rejects.toThrow(
+        `{"errors":[{"message":"Variable \\"$sentInfo\\" got invalid value \\"${dateStr}\\" at \\"sentInfo.sentAt\\"; ` +
+          `Expected type DateTime. Seul les chaînes de caractères au format ISO 8601 sont acceptées en tant que date. ` +
+          `Reçu ${dateStr}.","locations":[{"line":2,"column":33}],"extensions":{"code":"BAD_USER_INPUT"}}]}`
+      );
     }
   );
 
