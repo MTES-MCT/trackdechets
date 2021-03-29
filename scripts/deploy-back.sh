b9cb4280f43b768b3d91b2eaa802c0f813d53b0e
Orion C <orioncharlier@gmail.com>
2021-03-29 17:57:53 +0200

    Edit deploy & ci

--- scripts/deploy-back.sh
+++ scripts/deploy-back.sh
@@ -2,15 +2,13 @@
 
 set -e
 BACK_GIT_URL=""
-FRONT_GIT_URL=""
 
 BRANCH_NAME=$(echo "$GITHUB_REF" | cut -d/ -f3)
 
-echo "🪐 \e[1mGetting GIT deploy URLs...\e[m"
+echo "🪐 \e[1mGetting GIT deploy URL...\e[m"
 case $BRANCH_NAME in
     staging-1)
         BACK_GIT_URL=$GIT_STAGING_1_BACK
-        FRONT_GIT_URL=$GIT_STAGING_1_FRONT
         ;;
     dev)
         echo "⚠ Not handled yet, exiting."
@@ -26,7 +24,7 @@ case $BRANCH_NAME in
         ;;
 esac
 
-if [ -z "$BACK_GIT_URL" ] || [ -z "$FRONT_GIT_URL" ]
+if [ -z "$BACK_GIT_URL" ]
 then
       echo "No deployment target, exiting."
       exit 1
@@ -38,6 +36,5 @@ git config --global user.name "TD GitHub actions"
 
 echo "📦 \e[1mPushing for branch \e[36m<$BRANCH_NAME>\e[39m...\e[m"
 git push "$BACK_GIT_URL" HEAD:master --force 2>&1 | grep -e 'remote:' -e '->'
-git push "$FRONT_GIT_URL" HEAD:master --force 2>&1 | grep -e 'remote:' -e '->'
 
 echo "🚀 \e[1mDeploy successfull\e[m"
