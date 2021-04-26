ebebbd6de43e0f1f488d13b252a14b8a1445339b
Laurent Paoletti <lp@providenz.fr>
2021-04-26 14:21:54 +0000

    Improve db utils

--- back/src/dasris/errors.ts
+++ back/src/dasris/errors.ts
@@ -1,15 +1,7 @@
 import { UserInputError } from "apollo-server-express";
 
-export class DasriNotFound extends UserInputError {
+export class BsdasriNotFound extends UserInputError {
   constructor(id: string) {
     super(`Le bordereau avec l'identifiant "${id}" n'existe pas.`);
-  }
-}
-
-export class MissingIdOrReadableId extends UserInputError {
-  constructor() {
-    super(
-      "L'id ou le readableId doit être fourni pour identifier le bordereau."
-    );
   }
 }
