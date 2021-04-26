d331578f5a327120818db7308c926fe288473a1b
Laurent Paoletti <lp@providenz.fr>
2021-04-26 14:30:36 +0000

    Update dasri queries

---
+++ back/src/dasris/resolvers/queries/bsdasris.ts
@@ -0,0 +1,60 @@
+import { expandBsdasriFromDb } from "../../dasri-converter";
+
+import prisma from "../../../prisma";
+import { checkIsAuthenticated } from "../../../common/permissions";
+
+import { getCursorConnectionsArgs } from "../../cursorPagination";
+
+import { GraphQLContext } from "../../../types";
+import { buildDbFilter } from "./where";
+const defaultPaginateBy = 50;
+import { getUserCompanies } from "../../../users/database";
+
+export default async function dasris(_, args, context: GraphQLContext) {
+  const user = checkIsAuthenticated(context);
+
+  const { where: whereArgs, ...paginationArgs } = args;
+
+  const itemsPerPage =
+    paginationArgs.first ?? paginationArgs.last ?? defaultPaginateBy;
+  const { requiredItems, ...connectionsArgs } = await getCursorConnectionsArgs({
+    ...paginationArgs,
+    defaultPaginateBy: itemsPerPage,
+    maxPaginateBy: 500
+  });
+
+  const userCompanies = await getUserCompanies(user.id);
+  const userSirets = userCompanies.map(c => c.siret);
+
+  const where = {
+    ...buildDbFilter(whereArgs, userSirets),
+    isDeleted: false
+  };
+  const queried = await prisma.bsdasri.findMany({
+    ...connectionsArgs,
+    orderBy: { createdAt: "desc" },
+
+    where
+  });
+  const totalCount = await prisma.bsdasri.count({ where });
+  const queriedCount = queried.length;
+
+  const expanded = queried.map(f => expandBsdasriFromDb(f));
+  const pageInfo = {
+    startCursor: expanded[0]?.id || "",
+    endCursor: expanded[queriedCount - 1]?.id || "",
+    hasNextPage:
+      paginationArgs.after | paginationArgs.first
+        ? queriedCount > requiredItems
+        : false,
+    hasPreviousPage:
+      paginationArgs.before | paginationArgs.last
+        ? queriedCount > requiredItems
+        : false
+  };
+  return {
+    totalCount,
+    edges: expanded.map(bsd => ({ cursor: bsd.id, node: bsd })),
+    pageInfo
+  };
+}
