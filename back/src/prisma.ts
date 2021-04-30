0dee21dc1dbf7665ed2fe9962516a4ec5eccc4ff
gabinaureche <hello@gabinaureche.com>
2021-04-30 10:43:23 +0200

    Add query logs

--- back/src/prisma.ts
+++ back/src/prisma.ts
@@ -1,4 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import debug from "debug";
 
-const prisma = new PrismaClient();
+const logQuery = debug("prisma:query");
+const prisma = new PrismaClient({
+  log: [
+    {
+      level: "query",
+      emit: "event"
+    }
+  ]
+});
+prisma.$on("query", event => {
+  logQuery(`${event.query} took ${event.duration}ms`);
+});
+
 export default prisma;
