319359d4253fdbd5543d1297362ccb2b7517e384
gabinaureche <hello@gabinaureche.com>
2021-05-05 17:18:39 +0200

    Add tests plan

---
+++ back/src/bsffs/resolvers/mutations/__tests__/create.integration.ts
@@ -0,0 +1,4 @@
+describe("Mutation.createBsff", () => {
+  it.todo("should disallow unauthenticated user from creating a bsff");
+  it.todo("should disallow user that is not a contributor on the bsff");
+});
---
+++ back/src/bsffs/resolvers/mutations/__tests__/update.integration.ts
@@ -0,0 +1,18 @@
+describe("Mutation.updateBsff", () => {
+  it.todo("should disallow unauthenticated user from creating a bsff");
+  it.todo("should disallow user that is not a contributor on the bsff");
+  it.todo("should throw an error if the bsff being updated doesn't exist");
+  it.todo("should disallow removing a company from the bsff");
+  it.todo("should disallow updating emitter if they signed already");
+  it.todo("should allow updating emitter if they didn't sign");
+  it.todo(
+    "should disallow updating waste and quantity if emitter signed already"
+  );
+  it.todo("should allow updating waste and quantity if emitter didn't sign");
+  it.todo("should disallow updating transporter if they signed already");
+  it.todo("should allow updating transporter if they didn't sign");
+  it.todo("should disallow updating packagings if transporter signed alrady");
+  it.todo("should allow updating packagings if transporter didn't sign");
+  it.todo("should disallow updating destination if they signed already");
+  it.todo("should allow updating destination if they didn't sign");
+});
