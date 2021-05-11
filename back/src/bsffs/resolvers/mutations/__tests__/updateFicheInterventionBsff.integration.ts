7b57589a26196c5cebf8d10f2d8799599503a7c8
gabinaureche <hello@gabinaureche.com>
2021-05-11 14:36:08 +0200

    Add remaining tests

--- back/src/bsffs/resolvers/mutations/__tests__/updateFicheInterventionBsff.integration.ts
+++ back/src/bsffs/resolvers/mutations/__tests__/updateFicheInterventionBsff.integration.ts
@@ -62,12 +62,159 @@ describe("Mutation.updateFicheInterventionBsff", () => {
     expect(data.updateFicheInterventionBsff.numero).toBeTruthy();
   });
 
-  it.todo(
-    "should disallow unauthenticated user to update a fiche d'intervention"
-  );
-  it.todo(
-    "should disallow user to update a fiche d'intervention on a bsff they are not part of"
-  );
-  it.todo("should throw an error if the bsff doesn't exist");
-  it.todo("should throw an error if the fiche d'intervention doesn't exist");
+  it("should disallow unauthenticated user to update a fiche d'intervention", async () => {
+    const bsffId = getReadableId(ReadableIdPrefix.FF);
+    const ficheInterventionNumero = "ABCDEFGHIJK";
+    const ficheInterventionId = getFicheInterventionId(
+      bsffId,
+      ficheInterventionNumero
+    );
+    await prisma.bsff.create({
+      data: {
+        id: bsffId,
+        ficheInterventions: {
+          create: {
+            id: ficheInterventionId,
+            numero: ficheInterventionNumero,
+            ...flattenFicheInterventionBsffInput(
+              createBsffFicheInterventionInputMock({})
+            )
+          }
+        }
+      }
+    });
+
+    const { mutate } = makeClient();
+    const { errors } = await mutate<
+      Pick<Mutation, "updateFicheInterventionBsff">,
+      MutationUpdateFicheInterventionBsffArgs
+    >(UPDATE_FICHE_INTERVENTION, {
+      variables: {
+        id: bsffId,
+        numero: ficheInterventionNumero,
+        input: createBsffFicheInterventionInputMock({})
+      }
+    });
+
+    expect(errors).toEqual([
+      expect.objectContaining({
+        extensions: {
+          code: "UNAUTHENTICATED"
+        }
+      })
+    ]);
+  });
+
+  it("should disallow user to update a fiche d'intervention on a bsff they are not part of", async () => {
+    const { user } = await userWithCompanyFactory(UserRole.ADMIN);
+
+    const bsffId = getReadableId(ReadableIdPrefix.FF);
+    const ficheInterventionNumero = "ABCDEFGHIJK";
+    const ficheInterventionId = getFicheInterventionId(
+      bsffId,
+      ficheInterventionNumero
+    );
+    await prisma.bsff.create({
+      data: {
+        id: bsffId,
+        ficheInterventions: {
+          create: {
+            id: ficheInterventionId,
+            numero: ficheInterventionNumero,
+            ...flattenFicheInterventionBsffInput(
+              createBsffFicheInterventionInputMock({})
+            )
+          }
+        }
+      }
+    });
+
+    const { mutate } = makeClient(user);
+    const { errors } = await mutate<
+      Pick<Mutation, "updateFicheInterventionBsff">,
+      MutationUpdateFicheInterventionBsffArgs
+    >(UPDATE_FICHE_INTERVENTION, {
+      variables: {
+        id: bsffId,
+        numero: ficheInterventionNumero,
+        input: createBsffFicheInterventionInputMock({})
+      }
+    });
+
+    expect(errors).toEqual([
+      expect.objectContaining({
+        message:
+          "Vous ne pouvez pas éditer un bordereau sur lequel le SIRET de votre entreprise n'apparaît pas."
+      })
+    ]);
+  });
+
+  it("should throw an error if the bsff doesn't exist", async () => {
+    const { user } = await userWithCompanyFactory(UserRole.ADMIN);
+
+    const bsffId = getReadableId(ReadableIdPrefix.FF);
+    const ficheInterventionNumero = "ABCDEFGHIJK";
+    const ficheInterventionId = getFicheInterventionId(
+      bsffId,
+      ficheInterventionNumero
+    );
+    await prisma.bsffFicheIntervention.create({
+      data: {
+        id: ficheInterventionId,
+        numero: ficheInterventionNumero,
+        ...flattenFicheInterventionBsffInput(
+          createBsffFicheInterventionInputMock({})
+        )
+      }
+    });
+
+    const { mutate } = makeClient(user);
+    const { errors } = await mutate<
+      Pick<Mutation, "updateFicheInterventionBsff">,
+      MutationUpdateFicheInterventionBsffArgs
+    >(UPDATE_FICHE_INTERVENTION, {
+      variables: {
+        id: bsffId,
+        numero: ficheInterventionNumero,
+        input: createBsffFicheInterventionInputMock({})
+      }
+    });
+
+    expect(errors).toEqual([
+      expect.objectContaining({
+        message: `Le bordereau de fluides frigorigènes n°${bsffId} n'existe pas.`
+      })
+    ]);
+  });
+
+  it("should throw an error if the fiche d'intervention doesn't exist", async () => {
+    const { user, company } = await userWithCompanyFactory(UserRole.ADMIN);
+
+    const bsffId = getReadableId(ReadableIdPrefix.FF);
+    const ficheInterventionNumero = "ABCDEFGHIJK";
+    await prisma.bsff.create({
+      data: {
+        id: bsffId,
+        emitterCompanySiret: company.siret
+      }
+    });
+
+    const { mutate } = makeClient(user);
+    const { errors } = await mutate<
+      Pick<Mutation, "updateFicheInterventionBsff">,
+      MutationUpdateFicheInterventionBsffArgs
+    >(UPDATE_FICHE_INTERVENTION, {
+      variables: {
+        id: bsffId,
+        numero: ficheInterventionNumero,
+        input: createBsffFicheInterventionInputMock({})
+      }
+    });
+
+    expect(errors).toEqual([
+      expect.objectContaining({
+        message: `La fiche d'intervention n°${ficheInterventionNumero} n'existe pas pour le bordereau n°${bsffId}.`
+      })
+    ]);
+  });
 });
