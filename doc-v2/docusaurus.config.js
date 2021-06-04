2f0f648afb78c56c4ab26111bf486e7968191748
benoitguigal <benoit.guigal@gmail.com>
2021-06-04 14:51:19 +0200

    init docusaurus v2

---
+++ doc-v2/docusaurus.config.js
@@ -0,0 +1,116 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: "Documentation de l'API Trackdéchets",
+  description:
+    "Documentation de l'API Trackdéchets, plateforme de traçabilité des déchets dangeureux",
+  tagline:
+    "Connectez facilement vos systèmes d'information à l'API Trackdéchets",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "Trackdéchets",
+      logo: {
+        alt: "Logo Trackdéchets",
+        src: "img/trackdechets.png",
+      },
+      items: [
+        {
+          type: "doc",
+          docId: "intro",
+          position: "left",
+          label: "Documentation",
+        },
+        {
+          href: "https://github.com/MTES-MCT/trackdechets",
+          label: "Code source",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "API",
+          items: [
+            {
+              label: "Documentation",
+              to: "/docs/intro",
+            },
+            {
+              label: "Référence de l'API",
+              to: "/docs/api-reference",
+            },
+            {
+              label: "Statut de l'API",
+              to: "https://status.trackdechets.beta.gouv.fr",
+            },
+            {
+              label: "Playground GraphQL",
+              to: "https://api.trackdechets.beta.gouv.fr",
+            },
+            {
+              label: "Code source",
+              to: "https://github.com/MTES-MCT/trackdechets",
+            },
+          ],
+        },
+        {
+          title: "Communauté",
+          items: [
+            {
+              label: "Forum technique",
+              href: "https://developers.trackdechets.beta.gouv.fr",
+            },
+          ],
+        },
+        {
+          title: "Trackdéchets",
+          items: [
+            {
+              label: "Site web",
+              to: "https://trackdechets.beta.gouv.fr",
+            },
+            {
+              label: "Ressources",
+              to: "https://trackdechets.beta.gouv.fr/resources",
+            },
+            {
+              label: "Application web",
+              to: "https://app.trackdechets.beta.gouv.fr",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Ministère de la Transition Écologique`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
