d08c321eabdd983c22f29b529e43644eb73b50ab
benoitguigal <benoit.guigal@gmail.com>
2021-06-21 16:00:10 +0200

    allow configuring cron schedule from env variables

--- back/src/cron.ts
+++ back/src/cron.ts
@@ -1,4 +1,5 @@
 import * as cron from "cron";
+import cronValidator from "cron-validate";
 import {
   sendFirstOnboardingEmail,
   sendSecondOnboardingEmail
@@ -6,6 +7,7 @@ import {
 import { initSentry } from "./common/sentry";
 
 const {
+  CRON_ONBOARDING_SCHEDULE,
   FIRST_ONBOARDING_TEMPLATE_ID,
   PRODUCER_SECOND_ONBOARDING_TEMPLATE_ID,
   PROFESSIONAL_SECOND_ONBOARDING_TEMPLATE_ID
@@ -13,19 +15,27 @@ const {
 
 let jobs = [];
 
-const shouldOnboard =
-  !!FIRST_ONBOARDING_TEMPLATE_ID &&
-  !!PRODUCER_SECOND_ONBOARDING_TEMPLATE_ID &&
-  !!PROFESSIONAL_SECOND_ONBOARDING_TEMPLATE_ID;
+if (CRON_ONBOARDING_SCHEDULE) {
+  validateOnbardingCronSchedule(CRON_ONBOARDING_SCHEDULE);
 
-if (shouldOnboard) {
-  // every morning at 8h08. Avoid sending it at 8h00 to prevent traffic congestions.
-  const everyMorning = "8 8 * * *";
+  if (
+    !FIRST_ONBOARDING_TEMPLATE_ID ||
+    !PRODUCER_SECOND_ONBOARDING_TEMPLATE_ID ||
+    !PROFESSIONAL_SECOND_ONBOARDING_TEMPLATE_ID
+  ) {
+    throw new Error(
+      `Cannot start onboarding email cron job because some email templates were not configured :
+      - FIRST_ONBOARDING_TEMPLATE_ID
+      - PRODUCER_SECOND_ONBOARDING_TEMPLATE_ID
+      - PROFESSIONAL_SECOND_ONBOARDING_TEMPLATE_ID`
+    );
+  }
+
   jobs = [
     ...jobs,
     // first onboarding email
     new cron.CronJob({
-      cronTime: everyMorning,
+      cronTime: CRON_ONBOARDING_SCHEDULE,
       onTick: async () => {
         await sendFirstOnboardingEmail();
       },
@@ -33,7 +43,7 @@ if (shouldOnboard) {
     }),
     // second onbarding email
     new cron.CronJob({
-      cronTime: everyMorning,
+      cronTime: CRON_ONBOARDING_SCHEDULE,
       onTick: async () => {
         await sendSecondOnboardingEmail();
       },
@@ -58,3 +68,22 @@ if (Sentry) {
 }
 
 jobs.forEach(job => job.start());
+
+export function validateOnbardingCronSchedule(cronExp: string) {
+  // checks it is a valid cron quartz expression
+  const isValid = cronValidator(cronExp).isValid();
+
+  if (!isValid) {
+    throw new Error(`Invalid CRON expression : ${cronExp}`);
+  }
+
+  // checks it is set to run once every day
+  const everyDay = /^\d{1,2} \d{1,2} \* \* \*$/;
+  if (!everyDay.test(cronExp)) {
+    throw new Error(
+      "CRON expression should be set to run once every day : {m} {h} * * *"
+    );
+  }
+
+  return true;
+}
