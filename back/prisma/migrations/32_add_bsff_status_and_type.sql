678661afe61af4d9b3048db0de0eade9729ec243
gabinaureche <hello@gabinaureche.com>
2021-08-15 01:04:00 +0200

    Add migration

---
+++ back/prisma/migrations/32_add_bsff_status_and_type.sql
@@ -0,0 +1,16 @@
+-- CreateEnum
+CREATE TYPE "default$default"."BsffType" AS ENUM ('TRACER_FLUIDE', 'COLLECTE_PETITES_QUANTITES', 'GROUPEMENT', 'RECONDITIONNEMENT', 'REEXPEDITION');
+
+-- AlterEnum
+ALTER TABLE "default$default"."Bsff" ALTER COLUMN "status" DROP DEFAULT;
+ALTER TABLE "default$default"."Bsff" ALTER COLUMN "status" TYPE VARCHAR(255);
+DROP TYPE IF EXISTS "default$default"."BsffStatus";
+CREATE TYPE "default$default"."BsffStatus" AS ENUM ('INITIAL', 'SIGNED_BY_EMITTER', 'SENT', 'RECEIVED', 'INTERMEDIATELY_PROCESSED', 'PROCESSED', 'REFUSED');
+ALTER TABLE "default$default"."Bsff" ALTER COLUMN "status" SET DATA TYPE "default$default"."BsffStatus" using "status"::"default$default"."BsffStatus";
+ALTER TABLE "default$default"."Bsff" ALTER COLUMN "status" SET DEFAULT E'INITIAL';
+
+-- AlterTable
+ALTER TABLE "default$default"."Bsff" ADD COLUMN     "isDraft" BOOLEAN DEFAULT false,
+ADD COLUMN     "type" "default$default"."BsffType" NOT NULL DEFAULT E'TRACER_FLUIDE',
+ALTER COLUMN "quantityIsEstimate" DROP NOT NULL,
+ALTER COLUMN "quantityIsEstimate" DROP DEFAULT;
