7e7dfb12b23b12a38fb09e3c03279c2d1625b4be
gabinaureche <hello@gabinaureche.com>
2021-08-27 10:41:20 +0200

    Add publish validation

--- back/src/bsffs/resolvers/mutations/publishBsff.ts
+++ back/src/bsffs/resolvers/mutations/publishBsff.ts
@@ -5,6 +5,7 @@ import { isBsffContributor } from "../../permissions";
 import prisma from "../../../prisma";
 import { unflattenBsff } from "../../converter";
 import { indexBsff } from "../../elastic";
+import { validateBsff } from "../../validation";
 
 const publishBsffResolver: MutationResolvers["publishBsff"] = async (
   _,
@@ -13,7 +14,17 @@ const publishBsffResolver: MutationResolvers["publishBsff"] = async (
 ) => {
   const user = checkIsAuthenticated(context);
   const existingBsff = await getBsffOrNotFound({ id });
+
   await isBsffContributor(user, existingBsff);
+
+  const previousBsffs = await prisma.bsff.findMany({
+    where: { nextBsffId: existingBsff.id }
+  });
+  const ficheInterventions = await prisma.bsffFicheIntervention.findMany({
+    where: { bsffId: existingBsff.id }
+  });
+
+  await validateBsff(existingBsff, previousBsffs, ficheInterventions);
 
   const updatedBsff = await prisma.bsff.update({
     data: {
