b2ad91c9235a5a7122b24c34a04bc0d696213625
Orion C <orioncharlier@gmail.com>
2021-08-31 20:22:56 +0200

    Add dd-trace with prisma tracing

--- back/src/prisma.ts
+++ back/src/prisma.ts
@@ -1,11 +1,36 @@
 import { URL } from "url";
 import { unescape } from "querystring";
 import { PrismaClient } from "@prisma/client";
+import { tracer } from "./tracer";
 
 const prisma = new PrismaClient({
   datasources: {
     db: { url: getDbUrl() }
-  }
+  },
+  log: [
+    {
+      emit: "event",
+      level: "query"
+    }
+  ]
+});
+
+prisma.$use(async (params, next) => {
+  const tags = {
+    "span.kind": "client",
+    "span.type": "sql",
+    "prisma.model": params.model,
+    "prisma.action": params.action
+  };
+
+  return tracer.trace("prisma.query", { tags }, () => next(params));
+});
+
+prisma.$on("query" as any, async (e: any) => {
+  const span = tracer.scope().active();
+
+  span?.setTag("resource.name", e.query);
+  console.log(span);
 });
 
 function getDbUrl() {
