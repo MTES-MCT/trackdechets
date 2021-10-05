a29404f1a66986daa3a7c50601694d3c4cf0b7b0
benoitguigal <benoit.guigal@gmail.com>
2021-10-05 17:58:58 +0200

    update error message

--- back/src/companies/sirene/errors.ts
+++ back/src/companies/sirene/errors.ts
@@ -2,6 +2,9 @@ import { ForbiddenError } from "apollo-server-express";
 
 export class AnonymousCompanyError extends ForbiddenError {
   constructor() {
-    super(`Cet établissement n'est pas diffusable`);
+    super(
+      `Les informations de cet établissement ne sont pas disponibles car son propriétaire` +
+        ` a choisi de ne pas les rendre publiques lors de son enregistrement au Répertoire des Entreprises et des Établissements (SIRENE)`
+    );
   }
 }
