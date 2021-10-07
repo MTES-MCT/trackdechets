19480e71899906414e2620cc03ffe5471f70e41d
benoitguigal <benoit.guigal@gmail.com>
2021-10-07 16:08:18 +0200

    update prisma schema

--- back/prisma/migrations/33_update_bsff.sql
+++ back/prisma/migrations/33_update_bsff.sql
@@ -16,28 +16,15 @@ ALTER TABLE "Bsff"
 ALTER TABLE "default$default"."BsffFicheIntervention"
   RENAME COLUMN "kilos" TO "weight";
 
--- CreateTable BsffGroupement
-CREATE TABLE "BsffGroupement" (
-    "id" TEXT NOT NULL,
-    "previousId" TEXT NOT NULL,
-    "nextId" TEXT NOT NULL,
-    "weight" DOUBLE PRECISION NOT NULL,
-
-    PRIMARY KEY ("id")
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Bsff_forwardingId_unique" ON "Bsff"("forwardingId");
-
 -- AddForeignKey
 ALTER TABLE "Bsff" ADD FOREIGN KEY ("repackagedInId") REFERENCES "Bsff"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "Bsff" ADD FOREIGN KEY ("groupedInId") REFERENCES "Bsff"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "Bsff" ADD FOREIGN KEY ("forwardingId") REFERENCES "Bsff"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
--- AddForeignKey
-ALTER TABLE "BsffGroupement" ADD FOREIGN KEY ("previousId") REFERENCES "Bsff"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "BsffGroupement" ADD FOREIGN KEY ("nextId") REFERENCES "Bsff"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+-- CreateIndex
+CREATE UNIQUE INDEX "Bsff_forwardingId_unique" ON "Bsff"("forwardingId");
 
