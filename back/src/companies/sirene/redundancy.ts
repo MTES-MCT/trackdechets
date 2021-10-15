3871eed5b34ea5e4cfff93a46a599214683eb17f
gabinaureche <hello@gabinaureche.com>
2021-10-15 16:29:15 +0200

    Fix redundancy

--- back/src/companies/sirene/redundancy.ts
+++ back/src/companies/sirene/redundancy.ts
@@ -1,36 +1,26 @@
-import { ErrorCode } from "../../common/errors";
+import type { AsyncReturnType } from "type-fest";
+import { captureException } from "@sentry/node";
 
 /**
- * fallback to fn2 if fn1 fails
+ * Loop over the functions until one of them returns a result
  */
-export function redundant<T>(
-  fn1: (...args) => Promise<T>,
-  fn2: (...args) => Promise<T>
-) {
-  const redundantFn = async (...args) => {
-    try {
-      // try fn1
-      const response1 = await fn1(...args);
-      return response1;
-    } catch (err1) {
-      if (
-        // fn1 has failed
-        err1.response?.status >= 500 ||
-        err1.extensions?.code === ErrorCode.TOO_MANY_REQUESTS
-      ) {
-        try {
-          // try fn2
-          const response2 = await fn2(...args);
-          return response2;
-        } catch (err2) {
-          // fn2 has also fails, throw err1
-          throw err1;
+export function redundant<F extends (...args: any[]) => any>(...fns: F[]) {
+  return async (...args: Parameters<F>): Promise<AsyncReturnType<F>> => {
+    let firstError: Error | null = null;
+
+    for (const fn of fns) {
+      try {
+        const response = await fn(...args);
+        return response;
+      } catch (error) {
+        captureException(error);
+
+        if (firstError == null) {
+          firstError = error;
         }
-      } else {
-        throw err1;
       }
     }
-  };
 
-  return redundantFn;
+    throw firstError;
+  };
 }
