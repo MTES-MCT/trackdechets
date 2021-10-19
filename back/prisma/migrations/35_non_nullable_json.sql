0f44b487a228d313d456a73a5b79635164124f90
Orion C <orioncharlier@gmail.com>
2021-10-19 10:03:53 +0200

    Update migrations after rebase

--- back/prisma/migrations/35_non_nullable_json.sql
+++ back/prisma/migrations/35_non_nullable_json.sql
@@ -4,13 +4,13 @@ UPDATE "default$default"."TemporaryStorageDetail" SET "wasteDetailsPackagingInfo
 ALTER TABLE "default$default"."Form" ALTER COLUMN "wasteDetailsPackagingInfos" SET NOT NULL;
 ALTER TABLE "default$default"."TemporaryStorageDetail" ALTER COLUMN "wasteDetailsPackagingInfos" SET NOT NULL;
 
-UPDATE "default$default"."Bsdasri" SET "emitterWastePackagingsInfo" = '[]' WHERE "emitterWastePackagingsInfo" IS NULL;
-UPDATE "default$default"."Bsdasri" SET "transporterWastePackagingsInfo" = '[]' WHERE "transporterWastePackagingsInfo" IS NULL;
-UPDATE "default$default"."Bsdasri" SET "recipientWastePackagingsInfo" = '[]' WHERE "recipientWastePackagingsInfo" IS NULL;
+UPDATE "default$default"."Bsdasri" SET "emitterWastePackagings" = '[]' WHERE "emitterWastePackagings" IS NULL;
+UPDATE "default$default"."Bsdasri" SET "transporterWastePackagings" = '[]' WHERE "transporterWastePackagings" IS NULL;
+UPDATE "default$default"."Bsdasri" SET "destinationWastePackagings" = '[]' WHERE "destinationWastePackagings" IS NULL;
 
-ALTER TABLE "default$default"."Bsdasri" ALTER COLUMN "emitterWastePackagingsInfo" SET NOT NULL;
-ALTER TABLE "default$default"."Bsdasri" ALTER COLUMN "transporterWastePackagingsInfo" SET NOT NULL;
-ALTER TABLE "default$default"."Bsdasri" ALTER COLUMN "recipientWastePackagingsInfo" SET NOT NULL;
+ALTER TABLE "default$default"."Bsdasri" ALTER COLUMN "emitterWastePackagings" SET NOT NULL;
+ALTER TABLE "default$default"."Bsdasri" ALTER COLUMN "transporterWastePackagings" SET NOT NULL;
+ALTER TABLE "default$default"."Bsdasri" ALTER COLUMN "destinationWastePackagings" SET NOT NULL;
 
 UPDATE "default$default"."Bsff" SET "packagings" = '[]' WHERE "packagings" IS NULL;
 ALTER TABLE "default$default"."Bsff" ALTER COLUMN "packagings" SET NOT NULL;
