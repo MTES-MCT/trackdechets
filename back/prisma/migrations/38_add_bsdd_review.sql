592b3e8767ac2a6e21b21085151ab5eaeec4fc20
Orion C <orioncharlier@gmail.com>
2021-11-03 16:07:58 +0100

    Update migration script

--- back/prisma/migrations/38_add_bsdd_review.sql
+++ back/prisma/migrations/38_add_bsdd_review.sql
@@ -4,20 +4,33 @@ CREATE TABLE "default$default"."BsddReview" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "bsddId" TEXT NOT NULL,
-    "fromCompanyId" TEXT NOT NULL,
-    "toCompanyId" TEXT NOT NULL,
+    "requestedById" TEXT NOT NULL,
     "content" JSONB NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "default$default"."BsddReviewValidation" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "bsddReviewId" TEXT NOT NULL,
+    "companyId" TEXT NOT NULL,
     "isAccepted" BOOLEAN,
-    "isArchived" BOOLEAN NOT NULL DEFAULT false,
+    "isSettled" BOOLEAN NOT NULL DEFAULT false,
 
     PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
-ALTER TABLE "default$default"."BsddReview" ADD FOREIGN KEY ("fromCompanyId") REFERENCES "default$default"."Company"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "default$default"."BsddReview" ADD FOREIGN KEY ("toCompanyId") REFERENCES "default$default"."Company"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "default$default"."BsddReview" ADD FOREIGN KEY ("requestedById") REFERENCES "default$default"."Company"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "default$default"."BsddReview" ADD FOREIGN KEY ("bsddId") REFERENCES "default$default"."Form"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "default$default"."BsddReviewValidation" ADD FOREIGN KEY ("bsddReviewId") REFERENCES "default$default"."BsddReview"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "default$default"."BsddReviewValidation" ADD FOREIGN KEY ("companyId") REFERENCES "default$default"."Company"("id") ON DELETE CASCADE ON UPDATE CASCADE;
