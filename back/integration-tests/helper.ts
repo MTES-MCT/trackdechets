cada82f4f62c38f6e6083c23892f5c3a3c4c412e
elias@showk.me <elias@huggingface.co>
2021-12-13 14:49:33 +0100

    remplace sql function truncate_tables

--- back/integration-tests/helper.ts
+++ back/integration-tests/helper.ts
@@ -4,6 +4,7 @@ import { redisClient } from "../src/common/redis";
 import prisma from "../src/prisma";
 import { app } from "../src/server";
 import { client as elasticSearch, index } from "../src/common/elastic";
+import { Prisma } from "@prisma/client";
 
 let httpServerInstance: HttpServer | HttpsServer = null;
 
@@ -27,6 +28,31 @@ export async function closeServer() {
   });
 }
 
+/**
+ * Special fast path to drop data from a postgres database.
+ * Taken from https://github.com/prisma/prisma/issues/742#issuecomment-776901281
+ * This is an optimization which is particularly crucial in a unit testing context.
+ * This code path takes milliseconds, vs ~7 seconds for a migrate reset + db push
+ *
+ **/
+export async function truncateDatabase() {
+  const dbSchemaName = "default$default";
+  const tables: Array<{ tablename: string }> =
+    await prisma.$queryRaw`SELECT tablename FROM pg_tables WHERE schemaname=${dbSchemaName};`;
+
+  for (const { tablename } of tables) {
+    await prisma.$executeRawUnsafe(
+      `TRUNCATE TABLE \"${dbSchemaName}\".\"${tablename}\" CASCADE;`
+    );
+  }
+  const sequences: Array<{ relname: string }> =
+    await prisma.$queryRaw`SELECT c.relname FROM pg_class AS c JOIN pg_namespace AS n ON c.relnamespace = n.oid WHERE c.relkind='S' AND n.nspname=${dbSchemaName};`;
+
+  for (const { relname } of sequences) {
+    await prisma.$queryRaw`ALTER SEQUENCE \"${dbSchemaName}\".\"${relname}\" RESTART WITH 1;`;
+  }
+}
+
 export async function resetDatabase() {
   // We need a longer than 5sec timeout...
   jest.setTimeout(10000);
@@ -41,7 +67,7 @@ export async function resetDatabase() {
     },
     refresh: true
   });
-  await prisma.$executeRaw`SELECT truncate_tables();`;
+  await truncateDatabase();
 }
 
 export function refreshElasticSearch() {
