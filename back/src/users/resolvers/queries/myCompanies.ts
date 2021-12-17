e2ace95483dca7f3a8f0db26f78766bb957aea07
benoitguigal <benoit.guigal@gmail.com>
2021-12-17 11:56:31 +0100

    add myCompanies resolver

--- back/src/users/resolvers/queries/myCompanies.ts
+++ back/src/users/resolvers/queries/myCompanies.ts
@@ -1,22 +1,49 @@
-import { QueryResolvers } from "../../../generated/graphql/types";
+import { Company } from "@prisma/client";
+import { nafCodes } from "../../../common/constants/NAF";
+import { getConnection } from "../../../common/pagination";
+import { checkIsAuthenticated } from "../../../common/permissions";
+import { convertUrls } from "../../../companies/database";
+import {
+  CompanyPrivate,
+  QueryResolvers
+} from "../../../generated/graphql/types";
+import prisma from "../../../prisma";
 
 const myCompaniesResolver: QueryResolvers["myCompanies"] = async (
   _parent,
-  _args,
-  _context
+  args,
+  context
 ) => {
-  //const me = checkIsAuthenticated(context);
+  const me = checkIsAuthenticated(context);
 
-  return {
-    totalCount: 0,
-    edges: [],
-    pageInfo: {
-      startCursor: "",
-      endCursor: "",
-      hasNextPage: false,
-      hasPreviousPage: false
-    }
-  };
+  const where = { userId: me.id };
+
+  // retrieves all companies ids
+  const associations = await prisma.companyAssociation.findMany({
+    where,
+    select: { companyId: true }
+  });
+
+  const companyIds = associations.map(a => a.companyId);
+
+  const totalCount = companyIds.length;
+
+  return getConnection({
+    totalCount,
+    findMany: prismaPaginationArgs =>
+      prisma.company.findMany({
+        where: { id: { in: companyIds } },
+        ...prismaPaginationArgs,
+        orderBy: { createdAt: "desc" }
+      }),
+    formatNode: (company: Company) => {
+      const companyPrivate: CompanyPrivate = convertUrls(company);
+      const { codeNaf: naf, address } = company;
+      const libelleNaf = naf in nafCodes ? nafCodes[naf] : "";
+      return { ...companyPrivate, naf, libelleNaf, address };
+    },
+    ...args
+  });
 };
 
 export default myCompaniesResolver;
