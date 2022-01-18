c01d205affbc634334ddea135a249ba775333056
Elias Showk <elias.showk@beta.gouv.fr>
2022-01-18 11:43:53 +0100

    add console logging when NODE_ENV is dev

--- back/src/logging/logger.ts
+++ back/src/logging/logger.ts
@@ -14,4 +14,15 @@ const logger = createLogger({
   transports: [new transports.File({ filename: LOG_PATH })]
 });
 
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV === "dev") {
+  logger.add(
+    new transports.Console({
+      format: format.simple()
+    })
+  );
+}
+
 export default logger;
