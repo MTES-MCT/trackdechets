e6deafad9e2cb0e5bd00503886789557c7d1e729
benoitguigal <benoit.guigal@gmail.com>
2022-01-26 15:41:01 +0100

    refactor

--- back/src/companies/sirene/searchCompanies.ts
+++ back/src/companies/sirene/searchCompanies.ts
@@ -2,26 +2,34 @@ import { searchCompanies as searchCompaniesInsee } from "./insee/client";
 import { searchCompanies as searchCompaniesDataGouv } from "./entreprise.data.gouv.fr/client";
 import { backoffIfTooManyRequests, throttle } from "./ratelimit";
 import { redundant } from "./redundancy";
-import { setInMaintenanceIf } from "./maintenance";
 
 const { INSEE_MAINTENANCE } = process.env;
+
+const searchCompaniesInseeThrottled = backoffIfTooManyRequests(
+  searchCompaniesInsee,
+  {
+    service: "insee"
+  }
+);
+
+const searchCompaniesDataGouvThrottled = throttle(searchCompaniesDataGouv, {
+  service: "data_gouv",
+  requestsPerSeconds: 8
+});
+
+// list differents implementations of searchCompanies by
+// order of priority.
+const searchCompaniesProviders = [
+  ...(INSEE_MAINTENANCE === "true" ? [] : [searchCompaniesInseeThrottled]),
+  searchCompaniesDataGouvThrottled,
+  searchCompaniesInseeThrottled
+];
 
 /**
  * Apply throttle and redundant decorator to searchCompanies functions
  * We use entreprise.data.gouv.fr API in priority and fallback to INSEE
  * because fuzzy search is way better in entreprise.data.gouv.fr
  */
-const decoratedSearchCompanies = redundant(
-  setInMaintenanceIf(
-    backoffIfTooManyRequests(searchCompaniesInsee, {
-      service: "insee"
-    }),
-    INSEE_MAINTENANCE === "true"
-  ),
-  throttle(searchCompaniesDataGouv, {
-    service: "data_gouv",
-    requestsPerSeconds: 8
-  })
-);
+const decoratedSearchCompanies = redundant(...searchCompaniesProviders);
 
 export default decoratedSearchCompanies;
--- back/src/companies/sirene/searchCompany.ts
+++ back/src/companies/sirene/searchCompany.ts
@@ -3,28 +3,33 @@ import { searchCompany as searchCompanyDataGouv } from "./entreprise.data.gouv.f
 import { backoffIfTooManyRequests, throttle } from "./ratelimit";
 import { redundant } from "./redundancy";
 import { cache } from "./cache";
-import { setInMaintenanceIf } from "./maintenance";
 
 const { INSEE_MAINTENANCE } = process.env;
+
+const searchCompanyInseeThrottled = backoffIfTooManyRequests(
+  searchCompanyInsee,
+  {
+    service: "insee"
+  }
+);
+
+const searchCompanyDataGouvThrottled = throttle(searchCompanyDataGouv, {
+  service: "data_gouv",
+  requestsPerSeconds: 8
+});
+
+// list differents implementations of searchCompany by
+// order of priority.
+const searchCompanyProviders = [
+  ...(INSEE_MAINTENANCE === "true" ? [] : [searchCompanyInseeThrottled]),
+  searchCompanyDataGouvThrottled
+];
 
 /**
  * Apply throttle, redundant and cache decorators to searchCompany functions
  * We use INSEE API in priority and fall back to entreprise.data.gouv.fr
  */
-const decoratedSearchCompany = cache(
-  redundant(
-    setInMaintenanceIf(
-      backoffIfTooManyRequests(searchCompanyInsee, {
-        service: "insee"
-      }),
-      INSEE_MAINTENANCE === "true"
-    ),
-    throttle(searchCompanyDataGouv, {
-      service: "data_gouv",
-      requestsPerSeconds: 8
-    })
-  )
-);
+const decoratedSearchCompany = cache(redundant(...searchCompanyProviders));
 
 export default function searchCompany(siret: string) {
   return decoratedSearchCompany(siret);
