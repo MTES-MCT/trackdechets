1548d3eb682c809a6dde07d77525360a0ff5590e
gabinaureche <hello@gabinaureche.com>
2022-02-17 11:18:21 +0100

    Fix cleave.js

--- front/src/common/components/AutoFormattingSiret.tsx
+++ front/src/common/components/AutoFormattingSiret.tsx
@@ -11,6 +11,12 @@ export default function AutoFormattingSiret({ field, form, ...props }) {
       options={{ blocks: [3, 3, 3, 5] }}
       {...field}
       {...props}
+      onInit={owner => {
+        // cleave.js doesn't seem to be maintained anymore
+        // there's a know issue that's been reported since june 2020:
+        // https://github.com/nosir/cleave.js/issues/601#issuecomment-902747682
+        (owner as any).lastInputValue = "";
+      }}
       className={`td-input ${styles.autoformattingSiret}`}
     />
   );
