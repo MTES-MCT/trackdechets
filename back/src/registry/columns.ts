60ba1880823ea589f2ed953fc2d6fb97253ca9b7
benoitguigal <benoit.guigal@gmail.com>
2022-02-21 16:08:11 +0100

    add transporter 2 and transporter 3 in registry

--- back/src/registry/columns.ts
+++ back/src/registry/columns.ts
@@ -151,7 +151,43 @@ const columns: Column[] = [
     field: "destinationOperationNoTraceability",
     label: "Rupture de traçabilité autorisée",
     format: formatBoolean
-  }
+  },
+  { field: "transporter2CompanyAddress", label: "Transporteur n°2 adresse" },
+  {
+    field: "transporter2CompanyName",
+    label: "Transporteur n°2 raison sociale"
+  },
+  { field: "transporter2CompanySiret", label: "Transporteur n°2 SIRET" },
+  {
+    field: "transporter2RecepisseIsExempted",
+    label: "Transporteur n°2 exemption de récépissé",
+    format: formatBoolean
+  },
+  { field: "transporter2RecepisseNumber", label: "Transporteur n°2 récépissé" },
+  {
+    field: "transporter2NumberPlates",
+    label: "Transporteur n°2 immatriculation",
+    format: formatArray
+  },
+  { field: "transporter2CompanyMail", label: "Transporteur n°2 contact" },
+  { field: "transporter3CompanyAddress", label: "Transporteur n°3 adresse" },
+  {
+    field: "transporter3CompanyName",
+    label: "Transporteur n°3 raison sociale"
+  },
+  { field: "transporter3CompanySiret", label: "Transporteur n°3 SIRET" },
+  {
+    field: "transporter3RecepisseIsExempted",
+    label: "Transporteur n°3 exemption de récépissé",
+    format: formatBoolean
+  },
+  { field: "transporter3RecepisseNumber", label: "Transporteur n°3 récépissé" },
+  {
+    field: "transporter3NumberPlates",
+    label: "Transporteur n°3 immatriculation",
+    format: formatArray
+  },
+  { field: "transporter3CompanyMail", label: "Transporteur n°3 contact" }
 ];
 
 export function formatRow(waste: GenericWaste, useLabelAsKey = false) {
