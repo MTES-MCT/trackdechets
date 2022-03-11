9017374b632d8ff1aee03686876ce085d234a7b8
Elias Showk <elias.showk@beta.gouv.fr>
2022-03-11 11:49:43 +0100

    remove cache from searchCompany

--- back/src/companies/sirene/searchCompany.ts
+++ back/src/companies/sirene/searchCompany.ts
@@ -4,7 +4,6 @@ import { searchCompany as searchCompanySocialGouv } from "./social.gouv/client";
 import { searchCompany as searchCompanyTD } from "./trackdechets/client";
 import { backoffIfTooManyRequests, throttle } from "./ratelimit";
 import { redundant } from "./redundancy";
-import { cache } from "./cache";
 
 const searchCompanyInseeThrottled = backoffIfTooManyRequests(
   searchCompanyInsee,
@@ -36,7 +35,7 @@ const searchCompanyProviders = [
  * Apply throttle, redundant and cache decorators to searchCompany functions
  * We use INSEE API in priority and fall back to entreprise.data.gouv.fr
  */
-const decoratedSearchCompany = cache(redundant(...searchCompanyProviders));
+const decoratedSearchCompany = redundant(...searchCompanyProviders);
 
 export default function searchCompany(siret: string) {
   return decoratedSearchCompany(siret);
