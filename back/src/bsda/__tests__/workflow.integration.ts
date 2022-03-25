797069399f92306b343db0d17bf4f71eb551c520
Orion C <orioncharlier@gmail.com>
2022-03-25 09:23:50 +0100

    Linting error

--- back/src/bsda/__tests__/workflow.integration.ts
+++ back/src/bsda/__tests__/workflow.integration.ts
@@ -16,9 +16,6 @@ describe("Exemples de circuit du bordereau de suivi des déchets d'amiante", () 
   }
 
   it("Déchet déposé en déchetterie (collecte en 2710-1)", async () => {
-    const { company: producteurCompany } = await userWithCompanyFactory(
-      "MEMBER"
-    );
     const { user: exutoireUser, company: exutoireCompany } =
       await userWithCompanyFactory("MEMBER", {
         companyTypes: {
