e4145b5956712f357016b562f012b3136fa1ec7d
Orion C <orioncharlier@gmail.com>
2022-03-31 18:14:47 +0200

    Remove metadata format

--- back/src/logging/logger.ts
+++ back/src/logging/logger.ts
@@ -8,7 +8,6 @@ const logger = createLogger({
   exitOnError: false,
   format: format.combine(
     format.errors({ stack: true }),
-    format.metadata(),
     format.json()
   ),
   transports: [new transports.File({ filename: LOG_PATH })]
