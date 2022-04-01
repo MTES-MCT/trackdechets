292309715332aa402ddadc6726b385a1975512c1
Orion C <orioncharlier@gmail.com>
2022-04-01 09:36:35 +0200

    Linting error

--- back/src/logging/logger.ts
+++ back/src/logging/logger.ts
@@ -6,10 +6,7 @@ const LOG_PATH = `${appRoot}/logs/app.log`;
 const logger = createLogger({
   level: "info",
   exitOnError: false,
-  format: format.combine(
-    format.errors({ stack: true }),
-    format.json()
-  ),
+  format: format.combine(format.errors({ stack: true }), format.json()),
   transports: [new transports.File({ filename: LOG_PATH })]
 });
 
