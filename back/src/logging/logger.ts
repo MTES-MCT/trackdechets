e032b4983689acaaafbf3645d84aae370c810b8a
Orion C <orioncharlier@gmail.com>
2022-04-01 10:05:20 +0200

    Keep metadata but with exceptions

--- back/src/logging/logger.ts
+++ back/src/logging/logger.ts
@@ -6,7 +6,11 @@ const LOG_PATH = `${appRoot}/logs/app.log`;
 const logger = createLogger({
   level: "info",
   exitOnError: false,
-  format: format.combine(format.errors({ stack: true }), format.json()),
+  format: format.combine(
+    format.errors({ stack: true }),
+    format.metadata({ fillExcept: ["level", "message", "dd"] }), // `dd` enable connecting logs & traces
+    format.json()
+  ),
   transports: [new transports.File({ filename: LOG_PATH })]
 });
 
