9bfc60b6c1ae18b853a5874b1cb3e88a7a2b2e4e
Laurent Paoletti <lp@providenz.fr>
2022-05-05 07:33:44 +0000

    Ensure dasri emitter registration is not required

--- back/src/bsdasris/resolvers/mutations/__tests__/createBsdasriEcoOrganisme.integration.ts
+++ back/src/bsdasris/resolvers/mutations/__tests__/createBsdasriEcoOrganisme.integration.ts
@@ -178,6 +178,57 @@ describe("Mutation.createDasri", () => {
 
     expect(data.createBsdasri.ecoOrganisme.siret).toEqual(ecoOrg.siret);
   });
+  it("create a dasri with an eco-organisme and an unregistered emitter(eco-org user)", async () => {
+    const count = await prisma.company.count();
+    const ecoOrg = await ecoOrganismeFactory({ count, handleBsdasri: true });
+    const { user } = await userWithCompanyFactory("MEMBER", {
+      siret: ecoOrg.siret
+    });
+
+    const input = {
+      waste: { adr: "xyz 33", code: "18 01 03*" },
+      emitter: {
+        company: {
+          name: "hopital blanc",
+          siret: "51299999900099", // unregistered company
+          contact: "jean durand",
+          phone: "06 18 76 02 00",
+
+          address: "avenue de la mer"
+        },
+        emission: {
+          weight: { value: 23.2, isEstimate: false },
+
+          packagings: [
+            {
+              type: "BOITE_CARTON",
+              volume: 22,
+              quantity: 3
+            }
+          ]
+        }
+      },
+
+      ecoOrganisme: { siret: ecoOrg.siret, name: "eco-org" }
+    };
+
+    const { mutate } = makeClient(user);
+    const { data } = await mutate<Pick<Mutation, "createBsdasri">>(
+      CREATE_DASRI,
+      {
+        variables: {
+          input
+        }
+      }
+    );
+
+    expect(data.createBsdasri.isDraft).toEqual(false);
+    expect(data.createBsdasri.status).toEqual("INITIAL");
+    expect(data.createBsdasri.type).toEqual("SIMPLE");
+
+    expect(data.createBsdasri.ecoOrganisme.siret).toEqual(ecoOrg.siret);
+    expect(data.createBsdasri.emitter.company.siret).toEqual("51299999900099");
+  });
   it("create a dasri with an eco-organism (emitter user)", async () => {
     const count = await prisma.company.count();
     const ecoOrg = await ecoOrganismeFactory({ count, handleBsdasri: true });
