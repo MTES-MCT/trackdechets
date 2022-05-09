0f348ac14cd66f973159604e80dac98113d866b1
benoitguigal <benoit.guigal@gmail.com>
2022-05-09 11:06:19 +0200

    64_appendix2_fraction.sql

--- back/prisma/migrations/64_appendix2_fraction.sql
+++ back/prisma/migrations/64_appendix2_fraction.sql
@@ -1,39 +1,25 @@
 -- CreateTable
-CREATE TABLE "FormGroupement" (
+CREATE TABLE "default$default"."FormGroupement" (
     "id" TEXT NOT NULL,
     "nextFormId" TEXT NOT NULL,
     "initialFormId" TEXT NOT NULL,
     "quantity" DOUBLE PRECISION NOT NULL,
 
-    CONSTRAINT "FormGroupement_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "FormGroupement_pkey" PRIMARY KEY ("id"),
     CONSTRAINT "FormGroupement_pkey_unique_together" UNIQUE ("nextFormId", "initialFormId")
 );
 
 -- CreateIndex
-CREATE INDEX "_FormGroupementNextFormId" ON "FormGroupement"("nextFormId");
+CREATE INDEX "_FormGroupementNextFormId" ON "default$default"."FormGroupement"("nextFormId");
 
 -- CreateIndex
-CREATE INDEX "_FormGroupementInitialFormId" ON "FormGroupement"("initialFormId");
+CREATE INDEX "_FormGroupementInitialFormId" ON "default$default"."FormGroupement"("initialFormId");
 
 -- AddForeignKey
-ALTER TABLE "FormGroupement" ADD CONSTRAINT "FormGroupement_nextFormId_fkey" FOREIGN KEY ("nextFormId") REFERENCES "Form"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "default$default"."FormGroupement" ADD CONSTRAINT "FormGroupement_nextFormId_fkey" FOREIGN KEY ("nextFormId") REFERENCES "default$default"."Form"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "FormGroupement" ADD CONSTRAINT "FormGroupement_initialFormId_fkey" FOREIGN KEY ("initialFormId") REFERENCES "Form"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- Migrate data
-INSERT INTO "default$default"."FormGroupement" ("initialFormId", "nextFormId", "quantity")
-SELECT "id", "appendix2RootFormId", "quantityReceived" FROM "default$default"."Form" WHERE "appendix2RootFormId" IS NOT NULL;
-
-
--- DropForeignKey
-ALTER TABLE "Form" DROP CONSTRAINT "Form_appendix2RootFormId_fkey";
-
--- DropIndex
-DROP INDEX "_FormAppendix2RootFormIdIdx";
+ALTER TABLE "default$default"."FormGroupement" ADD CONSTRAINT "FormGroupement_initialFormId_fkey" FOREIGN KEY ("initialFormId") REFERENCES "default$default"."Form"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AlterTable
-ALTER TABLE "Form" DROP COLUMN "appendix2RootFormId";
-
--- AlterTable
-ALTER TABLE "Form" ADD COLUMN "quantityGrouped" DOUBLE PRECISION NOT NULL DEFAULT 0;
+ALTER TABLE "default$default"."Form" ADD COLUMN "quantityGrouped" DOUBLE PRECISION NOT NULL DEFAULT 0;
