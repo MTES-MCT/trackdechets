5b7dc3c8ca8d2bf821b22881d3d789dd271ae9d9
Laurent Paoletti <lp@providenz.fr>
2022-06-07 12:59:34 +0000

    Post review fix

--- back/src/users/resolvers/MembershipRequest.ts
+++ back/src/users/resolvers/MembershipRequest.ts
@@ -1,16 +1,12 @@
 import { MembershipRequestResolvers } from "../../generated/graphql/types";
-import { getEmailDomain, redactOrShowEmail } from "../utils";
+import { redactOrShowEmail } from "../utils";
 import { checkIsAuthenticated } from "../../common/permissions";
 
 const membershipRequestResolvers: MembershipRequestResolvers = {
   sentTo: (parent, arg, context) => {
     const user = checkIsAuthenticated(context);
 
-    const userEmailDomain = getEmailDomain(user?.email);
-
-    return parent.sentTo.map(email =>
-      redactOrShowEmail(email, userEmailDomain)
-    );
+    return parent.sentTo.map(email => redactOrShowEmail(email, user.email));
   }
 };
 
