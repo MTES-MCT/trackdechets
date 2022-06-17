c47608055aa28e3e09168fce7544485e5ed62b65
benoitguigal <benoit.guigal@gmail.com>
2022-06-17 11:50:51 +0200

    fix inconsistant recipientIsTempStorage

--- back/prisma/migrations/74_add_forwardedIn.sql
+++ back/prisma/migrations/74_add_forwardedIn.sql
@@ -7,6 +7,9 @@ UPDATE "default$default"."Form" SET "quantityReceivedType" = 'REAL';
 -- CreateIndex
 CREATE UNIQUE INDEX "Form_forwardedInId_key" ON "default$default"."Form"("forwardedInId");
 
-
 -- AddForeignKey
 ALTER TABLE "default$default"."Form" ADD CONSTRAINT "Form_forwardedInId_fkey" FOREIGN KEY ("forwardedInId") REFERENCES "default$default"."Form"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+--- Fix inconsistant data
+UPDATE "default$default"."Form" SET "recipientIsTempStorage" = FALSE
+WHERE "recipientIsTempStorage" = TRUE and "temporaryStorageDetailId" is NULL;
