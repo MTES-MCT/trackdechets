7b153999c50db252af38c994358503415929c5f4
benoitguigal <benoit.guigal@gmail.com>
2022-06-28 12:23:48 +0200

    fix objectEquals

--- back/src/common/constants/__tests__/formHelpers.test.ts
+++ back/src/common/constants/__tests__/formHelpers.test.ts
@@ -1,16 +1,40 @@
-import { packagingsEqual } from "../formHelpers";
+import { objectsEqual, packagingsEqual } from "../formHelpers";
+
+describe("objectEquals", () => {
+  test("empty objects", () => {
+    expect(objectsEqual({}, {})).toEqual(true);
+    expect(objectsEqual({}, { type: "FUT", quantity: 1 })).toEqual(false);
+  });
+
+  test("null values", () => {
+    expect(objectsEqual(null, null)).toEqual(true);
+    expect(objectsEqual(null, { type: "FUT", quantity: 1 })).toEqual(false);
+    expect(objectsEqual({ type: "FUT", quantity: 1 }, null)).toEqual(false);
+  });
+
+  test("undefined values", () => {
+    expect(objectsEqual(undefined, undefined)).toEqual(true);
+    expect(objectsEqual({ type: "FUT", quantity: 1 }, undefined)).toEqual(
+      false
+    );
+    expect(objectsEqual(undefined, { type: "FUT", quantity: 1 })).toEqual(
+      false
+    );
+  });
+});
 
 describe("packagingEquals", () => {
   test("empty packaging", () => {
     expect(packagingsEqual([], [])).toEqual(true);
     expect(packagingsEqual([{ type: "FUT", quantity: 1 }], [])).toEqual(false);
   });
-  test("null packaging", () => {
-    expect(packagingsEqual(null, null)).toEqual(true);
-    expect(packagingsEqual([{ type: "FUT", quantity: 1 }], null)).toEqual(
-      false
-    );
+
+  test("undefined key in packaging", () => {
+    expect(
+      packagingsEqual([{ type: "FUT", quantity: 1 }], [{ type: "FUT" }])
+    ).toEqual(false);
   });
+
   test("different packagings", () => {
     expect(
       packagingsEqual(
