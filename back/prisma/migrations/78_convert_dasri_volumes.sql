0dd1dc6687f3da220df7c297f8364b48ebba5b75
benoitguigal <benoit.guigal@gmail.com>
2022-07-01 15:18:36 +0200

    handle decimal volumes

---
+++ back/prisma/migrations/78_convert_dasri_volumes.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "default$default"."Bsdasri" ALTER COLUMN "emitterWasteVolume" SET DATA TYPE DOUBLE PRECISION;
+ALTER TABLE "default$default"."Bsdasri" ALTER COLUMN "transporterWasteVolume" SET DATA TYPE DOUBLE PRECISION;
+ALTER TABLE "default$default"."Bsdasri" ALTER COLUMN "destinationReceptionWasteVolume" SET DATA TYPE DOUBLE PRECISION;
