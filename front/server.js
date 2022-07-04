5944e3a7b50a6176bcbb5c9d869813d2fa83d310
Julien Seren-Rosso <julien.serenrosso@beta.gouv.fr>
2022-07-04 12:35:53 +0200

    Adds a robot meta tag

--- front/server.js
+++ front/server.js
@@ -1,15 +1,26 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
-const directory = '/' + (process.env.STATIC_DIR || 'build');
+const directory = "/" + (process.env.STATIC_DIR || "build");
 app.use(express.static(__dirname + directory));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, directory, 'index.html'));
+const pathToIndex = path.join(__dirname, directory, "index.html");
+app.get("/*", function (req, res) {
+  if (process.env.BUILD_ENV !== "production") {
+    const raw = fs.readFileSync(pathToIndex, "utf8");
+    const updated = raw.replace(
+      '<meta name="robots" content="all" />',
+      '<meta name="robots" content="noindex nofollow" />'
+    );
+    res.send(updated);
+  } else {
+    res.sendFile(pathToIndex);
+  }
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Trackdechets front listening on', port);
+  console.log("Trackdechets front listening on", port);
 });
