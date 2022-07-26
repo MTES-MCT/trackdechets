95b65a96443f4659af0e17b966fa3d7cde0153d4
chrischarousset <95855269+chrischarousset@users.noreply.github.com>
2022-07-26 17:26:41 +0200

    Update update-eco-organismes.ts
    ajout cyclevia
    suppression ecofolio + recylum
    uniformisation adresses

--- back/prisma/scripts/update-eco-organismes.ts
+++ back/prisma/scripts/update-eco-organismes.ts
@@ -5,7 +5,7 @@ const ecoOrganismes = [
   {
     name: "Corepile",
     siret: "42248908800035",
-    address: "17 rue Georges Bizet, 75016 Paris"
+    address: "17 rue Georges Bizet, 75016 PARIS"
   },
   {
     name: "Screlec",
@@ -15,32 +15,32 @@ const ecoOrganismes = [
   {
     name: "CITEO",
     siret: "38838007300162",
-    address: "CITEO, 50 BD HAUSSMANN 75009 PARIS"
+    address: "50 BD HAUSSMANN, 75009 PARIS"
   },
   {
     name: "Adelphe",
     siret: "39091301000042",
-    address: "93 RUE DE PROVENCE 75009 PARIS"
+    address: "93 RUE DE PROVENCE, 75009 PARIS"
   },
   {
     name: "Eco-Mobilier",
     siret: "53849587000031",
-    address: "50 AV DAUMESNIL 75012 PARIS"
+    address: "50 AV DAUMESNIL, 75012 PARIS"
   },
   {
     name: "PV Cycle",
     siret: "800 547 499 00023",
-    address: "13 RUE DU QUATRE SEPTEMBRE 75002 PARIS"
+    address: "13 RUE DU QUATRE SEPTEMBRE, 75002 PARIS"
   },
   {
     name: "Valdelia",
     siret: "53740637300036",
-    address: "RUE DU LAC 31670 LABEGE"
+    address: "RUE DU LAC, 31670 LABEGE"
   },
   {
     name: "Eco-DDS",
     siret: "75113994000025",
-    address: "117 AV VICTOR HUGO 92100 BOULOGNE-BILLANCOURT"
+    address: "117 AV VICTOR HUGO, 92100 BOULOGNE-BILLANCOURT"
   },
   {
     name: "Ecologic",
@@ -50,57 +50,52 @@ const ecoOrganismes = [
   {
     name: "Eco-TLC",
     siret: "50929280100024",
-    address: "4 Cité Paradis - 75010 PARIS - France"
+    address: "4 Cité Paradis, 75010 PARIS"
   },
   {
     name: "DASTRI",
     siret: "79250555400024",
-    address: "17, rue de l’Amiral Hamelin à Paris (75016)"
+    address: "17, rue de l’Amiral Hamelin, 75016 PARIS"
   },
   {
     name: "ECOSYSTEM",
     siret: "83033936200022",
-    address: "34 RUE HENRI REGNAULT 92400 COURBEVOIE"
+    address: "34 RUE HENRI REGNAULT, 92400 COURBEVOIE"
   },
   {
     name: "LEKO",
     siret: "82330882000021",
-    address: "29 RUE TRONCHET 75008 PARIS"
+    address: "29 RUE TRONCHET, 75008 PARIS"
   },
   {
     name: "CYCLAMED",
     siret: "39316301900044",
-    address: "60 T RUE DE BELLEVUE - 92100 BOULOGNE-BILLANCOURT"
+    address: "60 T RUE DE BELLEVUE, 92100 BOULOGNE-BILLANCOURT"
   },
   {
-    name: "ECOFOLIO",
-    siret: "49337909300039",
-    address: "3 PL DES VICTOIRES 75001 PARIS"
-  },
-  {
-    name: "RECYLUM",
-    siret: "48232394600012",
-    address: "17 RUE DE L'AMIRAL HAMELIN 75116 PARIS"
+    name: "CYCLEVIA",
+    siret: "90377711800022",
+    address: "4 RUE JACQUES DAGUERRE, 92500 RUEIL-MALMAISON"
   },
   {
     name: "ERP FRANCE",
     siret: "51436434800045",
-    address: "10 RUE DE PENTHIEVRE 75008 PARIS"
+    address: "10 RUE DE PENTHIEVRE, 75008 PARIS"
   },
   {
     name: "APER",
     siret: "51319280700024",
-    address: "PONT MIRABEAU 75015 PARIS"
+    address: "PONT MIRABEAU, 75015 PARIS"
   },
   {
     name: "APER PYRO",
     siret: "81761178300017",
-    address: "PORT DE JAVEL 75015 PARIS"
+    address: "PORT DE JAVEL, 75015 PARIS"
   },
   {
     name: "A.D.I VALOR",
     siret: "43836840900043",
-    address: "68 CRS ALBERT THOMAS 69008 LYON"
+    address: "68 CRS ALBERT THOMAS, 69008 LYON"
   }
 ];
 
