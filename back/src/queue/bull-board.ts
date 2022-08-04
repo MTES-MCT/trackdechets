e7e2293f99bcd652fcf3ceabbe90a4c577e54cd8
Orion C <orioncharlier@gmail.com>
2022-08-04 12:37:46 +0200

    Add queues to bull board

--- back/src/queue/bull-board.ts
+++ back/src/queue/bull-board.ts
@@ -1,13 +1,18 @@
 import { createBullBoard } from "@bull-board/api";
 import { BullAdapter } from "@bull-board/api/bullAdapter";
 import { ExpressAdapter } from "@bull-board/express";
+import { indexQueue, updatesQueue } from "./producers/elastic";
 import { mailQueue } from "./producers/mail";
 
 export const serverAdapter = new ExpressAdapter();
 export const bullBoardPath = `/queue/monitor/${process.env.QUEUE_MONITOR_TOKEN}`;
 
 createBullBoard({
-  queues: [new BullAdapter(mailQueue)],
+  queues: [
+    new BullAdapter(mailQueue),
+    new BullAdapter(indexQueue),
+    new BullAdapter(updatesQueue)
+  ],
   serverAdapter: serverAdapter
 });
 
