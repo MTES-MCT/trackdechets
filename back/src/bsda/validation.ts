5b577d15c64454a4bd3cd7d22819ce9551891621
Orion C <orioncharlier@gmail.com>
2022-08-24 17:14:59 +0200

    Stadardize error messages

--- back/src/bsda/validation.ts
+++ back/src/bsda/validation.ts
@@ -450,7 +450,7 @@ const destinationSchema: FactorySchemaOf<BsdaValidationContext, Destination> =
               !isWasteCenter(company)
             ) {
               throw ctx.createError({
-                message: `L'installation de destination ou d’entreposage ou de reconditionnement avec le SIRET "${siret}" n'est pas inscrite sur Trackdéchets en tant qu'installation de traitement, de tri transit regroupement ou déchetterie. Cette installation ne peut donc pas être visée sur le bordereau. Veuillez vous rapprocher de l'administrateur de cette installation pour qu'il modifie le profil de l'établissement depuis l'interface Trackdéchets Mon Compte > Établissements`
+                message: `L'installation de destination ou d'entreposage ou de reconditionnement avec le SIRET ${siret} n'est pas inscrite sur Trackdéchets en tant qu'installation de traitement, de tri transit regroupement ou déchetterie. Cette installation ne peut donc pas être visée sur le bordereau. Veuillez vous rapprocher de l'administrateur de cette installation pour qu'il modifie le profil de l'établissement depuis l'interface Trackdéchets Mon Compte > Établissements`
               });
             }
 
@@ -728,7 +728,7 @@ const transporterSchema: FactorySchemaOf<BsdaValidationContext, Transporter> =
         .test(
           "is-transporter-registered-with-right-profile",
           ({ value }) =>
-            `Le transporteur qui a été renseigné sur le bordereau (SIRET: ${value}) n'est pas inscrit sur Trackdéchets`,
+            `Le transporteur avec le SIRET ${value} n'est pas inscrit sur Trackdéchets`,
           async (siret, ctx) => {
             if (!siret) return true;
 
@@ -741,7 +741,7 @@ const transporterSchema: FactorySchemaOf<BsdaValidationContext, Transporter> =
 
             if (!isTransporter(company)) {
               throw ctx.createError({
-                message: `Le transporteur saisi sur le bordereau (SIRET: ${siret}) n'est pas inscrit sur Trackdéchets en tant qu'entreprise de transport. Cette installation ne peut donc pas être visée sur le bordereau. Veuillez vous rapprocher de l'administrateur de cette installation pour qu'il modifie le profil de l'établissement depuis l'interface Trackdéchets Mon Compte > Établissements`
+                message: `Le transporteur avec le SIRET ${siret} n'est pas inscrit sur Trackdéchets en tant qu'entreprise de transport. Cette installation ne peut donc pas être visée sur le bordereau. Veuillez vous rapprocher de l'administrateur de cette installation pour qu'il modifie le profil de l'établissement depuis l'interface Trackdéchets Mon Compte > Établissements`
               });
             }
 
