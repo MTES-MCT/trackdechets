552f44aa4507dfc84da419875084dbc2591ebbfc
Orion C <orioncharlier@gmail.com>
2022-09-05 16:01:46 +0200

    Hotfix schema & nextDestination

--- front/src/form/bsda/stepper/schema.ts
+++ front/src/form/bsda/stepper/schema.ts
@@ -2,7 +2,14 @@ import * as Yup from "yup";
 
 const packagingsSchema = Yup.object({
   type: Yup.string().required("Le type de conditionnement est obligatoire"),
-  other: Yup.string().optional(),
+  other: Yup.string()
+    .label("Détail du conditionnement")
+    .when("type", {
+      is: "OTHER",
+      then: schema =>
+        schema.required("Vous devez saisir la description du conditionnement"),
+      otherwise: schema => schema.optional().nullable(),
+    }),
   quantity: Yup.number()
     .min(1, "La quantité d'un conditionnement doit être supérieure à 1")
     .required("La quantité associée à un conditionnement est obligatoire"),
