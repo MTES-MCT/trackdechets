c3a1ba00a5429d986ea5bebd1da6215764a66fdb
Elias Showk <elias.showk@beta.gouv.fr>
2022-09-06 15:25:54 +0200

    fix script update of creatring FOLLOWED_WITH_PNTTD

--- back/prisma/scripts/update-status-followed_with_pnttd.ts
+++ back/prisma/scripts/update-status-followed_with_pnttd.ts
@@ -8,12 +8,24 @@ import { registerUpdater, Updater } from "./helper/helper";
 )
 export class UpdateBsddStatusFollowedWithPnttd implements Updater {
   async run() {
+    // ensure nextDestinationCompanyCountry is always set to FR
+    await prisma.form.updateMany({
+      data: {
+        nextDestinationCompanyCountry: "FR"
+      },
+      where: {
+        nextDestinationCompanySiret: { not: null },
+        nextDestinationCompanyCountry: null
+      }
+    });
+    // update data.
     await prisma.form.updateMany({
       data: {
         status: "FOLLOWED_WITH_PNTTD"
       },
       where: {
         status: "AWAITING_GROUP",
+        groupedIn: undefined,
         noTraceability: false,
         nextDestinationCompanyCountry: { not: "FR" }
       }
