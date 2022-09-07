5f466f2847415d4db6d7c9633c6f12ccd2fa0a2b
Orion C <orioncharlier@gmail.com>
2022-09-07 09:36:39 +0200

    Upload heapdump

--- back/src/logging/logger.ts
+++ back/src/logging/logger.ts
@@ -1,5 +1,9 @@
 import appRoot from "app-root-path";
 import { createLogger, format, transports } from "winston";
+import v8 from "v8";
+import { createWriteStream } from "fs";
+import { S3Client } from "@aws-sdk/client-s3";
+import { Upload } from "@aws-sdk/lib-storage";
 
 const LOG_PATH = `${appRoot}/logs/app.log`;
 
@@ -14,6 +18,46 @@ const logger = createLogger({
 setInterval(() => {
   const memoryUsage = process.memoryUsage();
   logger.info("Memory usage - TEMP", memoryUsage);
+
+  if (process.env.DD_ENV === "sandbox") {
+    createHeapSnapshotAndUploadToS3();
+  }
 }, 1000 * 60 * 3);
+
+async function createHeapSnapshotAndUploadToS3() {
+  const snapshotStream = v8.getHeapSnapshot();
+  // It's important that the filename end with `.heapsnapshot`,
+  // otherwise Chrome DevTools won't open it.
+  const fileName = `${Date.now()}.heapsnapshot`;
+  const fileStream = createWriteStream(fileName);
+  snapshotStream.pipe(fileStream);
+
+  try {
+    const parallelUploads3 = new Upload({
+      client: new S3Client({
+        endpoint: process.env.S3_ENDPOINT,
+        region: process.env.S3_REGION,
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID,
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+        }
+      }),
+      params: {
+        Bucket: process.env.S3_BUCKET,
+        Key: fileName,
+        Body: snapshotStream
+      },
+      leavePartsOnError: false
+    });
+
+    parallelUploads3.on("httpUploadProgress", progress => {
+      console.log(progress);
+    });
+
+    await parallelUploads3.done();
+  } catch (e) {
+    console.log("Error while uploading heapdump", e);
+  }
+}
 
 export default logger;
