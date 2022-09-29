2fc370e89dd55f9cc9565dd3c6dbff071cf9086c
Laurent Paoletti <lp@providenz.fr>
2022-09-29 16:23:57 +0000

    Add updated at indices

---
+++ back/prisma/migrations/90_add_bsds_updated_at_indices.sql
@@ -0,0 +1,5 @@
+CREATE INDEX IF NOT EXISTS "_BsdaUpdatedAtIdx" ON "default$default"."Bsda"("updatedAt");
+CREATE INDEX IF NOT EXISTS "_BsdasriUpdatedAtIdx" ON "default$default"."Bsdasri"("updatedAt");
+CREATE INDEX IF NOT EXISTS "_BsffUpdatedAtIdx" ON "default$default"."Bsff"("updatedAt");
+CREATE INDEX IF NOT EXISTS "_BsvhuUpdatedAtIdx" ON "default$default"."Bsvhu"("updatedAt");
+CREATE INDEX IF NOT EXISTS "_FormUpdatedAtIdx" ON "default$default"."Form"("updatedAt");
