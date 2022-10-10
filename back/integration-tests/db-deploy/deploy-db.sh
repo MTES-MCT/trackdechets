74cc284062c10adc6e5ff20192ba442f01feabd9
Elias Showk <elias.showk@beta.gouv.fr>
2022-10-10 15:07:02 +0200

    restore deploy-dh.sh

--- back/integration-tests/db-deploy/deploy-db.sh
+++ back/integration-tests/db-deploy/deploy-db.sh
@@ -10,16 +10,12 @@ until PGPASSWORD="no_pass" docker exec -t $psql_container_id bash -c "psql -U \"
   sleep 1
 done
 
-echo "1/3 - Drop and Create prisma DB";
+echo "1/2 - Drop and Create prisma DB";
 docker exec -t $psql_container_id bash -c "psql -U test -c \"DROP DATABASE IF EXISTS prisma;\"";
 docker exec -t $psql_container_id bash -c "psql -U test -c \"CREATE DATABASE prisma;\"";
 
-echo "2/3 - Wait for Elastic Search";
+echo "2/2 - Wait for Elastic Search";
 until docker exec -t $api_container_id bash -c "curl -XGET http://elasticsearch:9200 2> /dev/null"; do
   >&2 echo "⏳ Elastic Search is unavailable - sleeping"
   sleep 1
 done
-
-echo "3/3 - Init for Elastic Search";
-curl -XPUT http://elasticsearch:9200/bsds_test_index
-curl -XPUT http://elasticsearch:9200/bsds_test_index/_alias/bsds
