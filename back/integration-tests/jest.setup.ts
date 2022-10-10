2a3461b031d5db457f80e292636a49a30202e830
Elias Showk <elias.showk@beta.gouv.fr>
2022-10-10 14:56:50 +0200

    retry configuring integration tests

--- back/integration-tests/jest.setup.ts
+++ back/integration-tests/jest.setup.ts
@@ -1,15 +1,9 @@
 import { redisClient } from "../src/common/redis";
 import prisma from "../src/prisma";
 import { startApolloServer } from "../src/server";
-import { client as elasticSearch, index } from "../src/common/elastic";
-import { declareNewIndex } from "../src/scripts/bin/indexElasticSearch.helpers";
+import { client as elasticSearch } from "../src/common/elastic";
 
 beforeAll(async () => {
-  const newIndex = await declareNewIndex(index);
-  await elasticSearch.indices.putAlias({
-    name: index.alias,
-    index: newIndex
-  });
   await startApolloServer();
 });
 
