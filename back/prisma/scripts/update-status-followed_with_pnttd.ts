22b74edb3ac880a07dfcbd6ba61b7d509a29714a
benoitguigal <benoit.guigal@gmail.com>
2022-10-10 17:10:12 +0200

    deactivate scripts

--- back/prisma/scripts/update-status-followed_with_pnttd.ts
+++ back/prisma/scripts/update-status-followed_with_pnttd.ts
@@ -4,7 +4,7 @@ import { registerUpdater, Updater } from "./helper/helper";
 @registerUpdater(
   "Set status FOLLOWED_WITH_PNTTD when next destination is foreign and tracability is not broken",
   "Set status FOLLOWED_WITH_PNTTD when next destination is foreign and tracability is not broken",
-  true
+  false
 )
 export class UpdateBsddStatusFollowedWithPnttd implements Updater {
   async run() {
