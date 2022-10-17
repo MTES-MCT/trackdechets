dbaef702de47e88d677df310cd68a07cae503040
Elias Showk <elias.showk@beta.gouv.fr>
2022-10-17 15:35:51 +0200

    abort early script

--- back/src/scripts/bin/reindexAllInBulk.ts
+++ back/src/scripts/bin/reindexAllInBulk.ts
@@ -1,12 +1,28 @@
 import logger from "../../logging/logger";
+// only meant to be executed for the api deployment, not notifier, except if --dev is passed
+// notifier deployment has STARTUP_FILE set to "dist/src/notifier/index.js"
+const { STARTUP_FILE } = process.env;
+const dev = process.argv.includes("--dev") || process.argv.includes("-d");
+
+if (STARTUP_FILE && STARTUP_FILE !== "dist/src/index.js") {
+  if (!!dev) {
+    logger.info(
+      "Starting reindexAllInBulk with --dev bypassing api deployment protection"
+    );
+  } else {
+    logger.info(
+      "Abort reindexAllInBulk: not in a TD api deployment ($STARTUP_FILE is not targeting the api server index.js)"
+    );
+    process.exit(1);
+  }
+}
+
 import prisma from "../../prisma";
 import { closeQueues } from "../../queue/producers";
 import {
   reindexAllBsdsInBulk,
   addReindexAllInBulkJob
 } from "../../bsds/indexation/bulkIndexBsds";
-
-const { STARTUP_FILE } = process.env;
 
 async function exitScript() {
   logger.info("Done reindexAllInBulk script, exiting");
@@ -16,21 +32,6 @@ async function exitScript() {
 
 (async function () {
   const force = process.argv.includes("--force") || process.argv.includes("-f");
-  const dev = process.argv.includes("--dev") || process.argv.includes("-d");
-  // only meant to be used for api production deployment, exxcept if --dev is passed
-  if (STARTUP_FILE && STARTUP_FILE !== "dist/src/index.js") {
-    if (!!dev) {
-      logger.info(
-        "Starting reindexAllInBulk with --dev bypassing api deployment protection"
-      );
-    } else {
-      logger.info(
-        "Abort reindexAllInBulk: not in a TD api deployment ($STARTUP_FILE is not targeting the api server index.js)"
-      );
-      await exitScript();
-      return;
-    }
-  }
   try {
     // launch job by chunks in the queue only if argument is specified
     const useQueue = process.argv.includes("--useQueue");
