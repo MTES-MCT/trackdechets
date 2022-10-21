b812ace32aef93cfbc21eecf651ff7951df9f776
Elias Showk <elias.showk@beta.gouv.fr>
2022-10-21 10:01:39 +0200

    add autoscaling feature to reindexAllInBulk queueing system

--- back/src/queue/jobs/indexAllBsds.ts
+++ back/src/queue/jobs/indexAllBsds.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { Job } from "bull";
 import { BsdIndex } from "../../common/elastic";
 import {
@@ -47,12 +48,87 @@ export async function indexAllInBulk(job: Job<string>) {
     const { index, force }: { index: BsdIndex; force: boolean } = JSON.parse(
       job.data
     );
+    const {
+      SCALINGO_API_URL,
+      SCALINGO_APP_NAME,
+      SCALINGO_API_TOKEN,
+      BULK_INDEX_SCALINGO_ACTIVE_AUTOSCALING,
+      BULK_INDEX_SCALINGO_CONTAINER_NAME,
+      BULK_INDEX_SCALINGO_CONTAINER_SIZE_UP,
+      BULK_INDEX_SCALINGO_CONTAINER_SIZE_DOWN,
+      BULK_INDEX_SCALINGO_CONTAINER_AMOUNT_UP,
+      BULK_INDEX_SCALINGO_CONTAINER_AMOUNT_DOWN
+    } = process.env;
+
+    if (BULK_INDEX_SCALINGO_ACTIVE_AUTOSCALING === "true") {
+      // scale-up indexqueue workers
+      try {
+        const resp = await axios({
+          method: "post",
+          url: `https://${SCALINGO_API_URL}/v1/apps/${SCALINGO_APP_NAME}/scale`,
+          data: {
+            containers: [
+              {
+                name: BULK_INDEX_SCALINGO_CONTAINER_NAME,
+                amount:
+                  parseInt(BULK_INDEX_SCALINGO_CONTAINER_AMOUNT_UP, 10) || 4,
+                size: BULK_INDEX_SCALINGO_CONTAINER_SIZE_UP || "2XL"
+              }
+            ]
+          },
+          headers: {
+            Authorization: `Bearer ${SCALINGO_API_TOKEN}`
+          }
+        });
+        logger.info(
+          `Scaled-up indexqueue workers to ${BULK_INDEX_SCALINGO_CONTAINER_AMOUNT_UP} ${BULK_INDEX_SCALINGO_CONTAINER_SIZE_UP}`,
+          resp.data
+        );
+      } catch (e) {
+        logger.error(
+          `Failed to scale-up indexqueue workers, please take a manual action instead`,
+          e
+        );
+      }
+    }
+
     // will index all BSD without downtime, only if need because of a mapping change
     await reindexAllBsdsInBulk({
       index,
       force,
       useQueue: true
     });
+    if (BULK_INDEX_SCALINGO_ACTIVE_AUTOSCALING === "true") {
+      // scale-down indexqueue workers
+      try {
+        const resp = await axios({
+          method: "post",
+          url: `https://${SCALINGO_API_URL}/v1/apps/${SCALINGO_APP_NAME}/scale`,
+          data: {
+            containers: [
+              {
+                name: BULK_INDEX_SCALINGO_CONTAINER_NAME,
+                amount:
+                  parseInt(BULK_INDEX_SCALINGO_CONTAINER_AMOUNT_DOWN, 10) || 1,
+                size: BULK_INDEX_SCALINGO_CONTAINER_SIZE_DOWN || "M"
+              }
+            ]
+          },
+          headers: {
+            Authorization: `Bearer ${SCALINGO_API_TOKEN}`
+          }
+        });
+        logger.info(
+          `Scaled-down indexqueue workers to ${BULK_INDEX_SCALINGO_CONTAINER_AMOUNT_DOWN} ${BULK_INDEX_SCALINGO_CONTAINER_SIZE_DOWN}`,
+          resp.data
+        );
+      } catch (e) {
+        logger.error(
+          `Failed to scale-up indexqueue workers, please take a manual action instead`,
+          e
+        );
+      }
+    }
     return null;
   } catch (error) {
     logger.error(`Error in indexAllInBulk.`, error);
