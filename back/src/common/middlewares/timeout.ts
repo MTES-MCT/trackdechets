d0a8beef4bf579d32786a5917d8b710e06249f58
Orion C <orioncharlier@gmail.com>
2022-11-16 18:54:20 +0100

    2 minutes

--- back/src/common/middlewares/timeout.ts
+++ back/src/common/middlewares/timeout.ts
@@ -1,8 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 
-// Scalingo times out after 1min
+// Scalingo times out after 1min, we take a margin
 // See https://doc.scalingo.com/platform/internals/routing#timeouts for more details
-const TIMEOUT_MS = 1000 * 60;
+const TIMEOUT_MS = 1000 * 60 * 2;
 
 export function timeoutMiddleware(delay?: number) {
   return function (_: Request, res: Response, next: NextFunction) {
