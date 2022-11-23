8d4fb8ed0bfa1a89795911ecfcd3a52895541426
Benoit Guigal <benoit.guigal@protonmail.com>
2022-11-23 15:28:41 +0100

    update prerun.sh

--- back/datadog/prerun.sh
+++ back/datadog/prerun.sh
@@ -9,26 +9,23 @@ if [[ -n "${SCALINGO_USER_ID}" ]] || [[ "${CONTAINER}" == "one-off"* ]]; then
   DISABLE_DATADOG_AGENT="true"
 fi
 
-
 FIRST_NODE="web-1"
-
-echo "Print DD_CONF_DIR"
-echo $DD_CONF_DIR
+POSTGRES_CONF_FILE="${DD_CONF_DIR}/conf.d/postgres.d/conf.yaml"
 
 if [[ -n "${DATABASE_URL}" ]] && [[ "${CONTAINER}" == "${FIRST_NODE}" ]]; then
   echo 'Container N°1 detected : activating datadog agent postgres monitoring'
   POSTGREGEX='^postgres://([^:]+):([^@]+)@([^:]+):([^/]+)/(.*)\?(.*)$'
   if [[ $DATABASE_URL =~ $POSTGREGEX ]]; then
-    sed -i "s/<DD_DATABASE_HOST>/${BASH_REMATCH[3]}/g" "${DD_CONF_DIR}/conf.d/postgres.yaml"
-    sed -i "s/<DD_DATABASE_USER>/${BASH_REMATCH[1]}/g" "${DD_CONF_DIR}/conf.d/postgres.yaml"
-    sed -i "s/<DD_DATABASE_PWD>/${BASH_REMATCH[2]}/g" "${DD_CONF_DIR}/conf.d/postgres.yaml"
-    sed -i "s/<DD_DATABASE_PORT>/${BASH_REMATCH[4]}/g" "${DD_CONF_DIR}/conf.d/postgres.yaml"
-    sed -i "s/<DD_DATABASE_DB>/${BASH_REMATCH[5]}/g" "${DD_CONF_DIR}/conf.d/postgres.yaml"
+    sed -i "s/<DD_DATABASE_HOST>/${BASH_REMATCH[3]}/g" "${POSTGRES_CONF_FILE}"
+    sed -i "s/<DD_DATABASE_USER>/${BASH_REMATCH[1]}/g" "${POSTGRES_CONF_FILE}"
+    sed -i "s/<DD_DATABASE_PWD>/${BASH_REMATCH[2]}/g" "${POSTGRES_CONF_FILE}"
+    sed -i "s/<DD_DATABASE_PORT>/${BASH_REMATCH[4]}/g" "${POSTGRES_CONF_FILE}"
+    sed -i "s/<DD_DATABASE_DB>/${BASH_REMATCH[5]}/g" "${POSTGRES_CONF_FILE}"
   fi
-  cat "${DD_CONF_DIR}/conf.d/postgres.yaml"
+  cat "${POSTGRES_CONF_FILE}"
 else 
   # Disable psql integration for every nodes but the first
   # There seems to be no env to disable the psql integration, so we simply remove the config file
   echo 'Container N° > 1 detected : de-activating datadog agent postgres monitoring'
-  rm "${DD_CONF_DIR}/conf.d/postgres.yaml"
+  rm "${POSTGRES_CONF_FILE}"
 fi
