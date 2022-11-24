603103514ad770bb5cf965406e962df9618da191
Orion C <orioncharlier@gmail.com>
2022-11-24 18:05:08 +0100

    Organize imports

--- back/src/queue/bull-board.ts
+++ back/src/queue/bull-board.ts
@@ -1,13 +1,13 @@
 import { createBullBoard } from "@bull-board/api";
 import { BullAdapter } from "@bull-board/api/bullAdapter";
 import { ExpressAdapter } from "@bull-board/express";
-import { indexQueue, updatesQueue } from "./producers/elastic";
-import { mailQueue } from "./producers/mail";
 import {
   geocodeCompanyQueue,
   setCompanyDepartementQueue
 } from "./producers/company";
+import { indexQueue, updatesQueue } from "./producers/elastic";
 import { syncEventsQueue } from "./producers/events";
+import { mailQueue } from "./producers/mail";
 
 export const serverAdapter = new ExpressAdapter();
 export const bullBoardPath = `/queue/monitor/${process.env.QUEUE_MONITOR_TOKEN}`;
