e4187500a5cb5d7acc3743eebbd959ba2d1fa8e1
elishowk <elias.showk@beta.gouv.fr>
2022-12-02 09:36:03 +0100

    create script FixNameAddressCompanyUpdater (#1890)

---
+++ back/prisma/scripts/fix-empty-company-name.ts
@@ -0,0 +1,52 @@
+import { Updater, registerUpdater } from "./helper/helper";
+import prisma from "../../src/prisma";
+import { searchCompany } from "../../src/companies/search";
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+@registerUpdater(
+  "Fix name and address to companies",
+  "Fix name and address to companies",
+  true
+)
+export class FixNameAddressCompanyUpdater implements Updater {
+  async run() {
+    try {
+      const companies = await prisma.company.findMany({
+        where: {
+          OR: [{ name: "---" }, { name: "" }]
+        }
+      });
+      for (const company of companies) {
+        try {
+          const companyInfo = await searchCompany(company.siret);
+          await prisma.company.update({
+            data: {
+              name:
+                companyInfo.name !== "---" && companyInfo.name
+                  ? companyInfo.name
+                  : "",
+              address:
+                companyInfo.address !== "---" && companyInfo.address
+                  ? companyInfo.address
+                  : ""
+            },
+            where: { id: company.id }
+          });
+        } catch (_) {
+          console.log(
+            `Failed retrieving address and name info for SIRET ${company.siret}`
+          );
+        } finally {
+          // Wait some time to avoid 429 Too many requests
+          await sleep(200);
+        }
+      }
+    } catch (err) {
+      console.error("☠ Something went wrong during the update", err);
+      throw new Error();
+    }
+  }
+}
