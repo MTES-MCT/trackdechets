ee02fafb5a6208ed96ec64cafc4fc4505c8acef1
Julien Seren-Rosso <julien.serenrosso@beta.gouv.fr>
2022-12-16 11:53:45 +0100

    Adds Figma addon

--- front/.storybook/main.js
+++ front/.storybook/main.js
@@ -9,6 +9,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
+    "storybook-addon-designs",
   ],
   framework: "@storybook/react",
   core: {
