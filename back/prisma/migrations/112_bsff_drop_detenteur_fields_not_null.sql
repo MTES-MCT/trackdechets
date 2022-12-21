57088cfb432c50b889ec0b009cc3e9c1fce08a51
Benoit Guigal <benoit.guigal@protonmail.com>
2022-12-21 12:26:15 +0100

    fix fiche intervention detenteur schema

---
+++ back/prisma/migrations/112_bsff_drop_detenteur_fields_not_null.sql
@@ -0,0 +1,8 @@
+ALTER TABLE
+    "default$default"."BsffFicheIntervention"
+ALTER COLUMN
+    "detenteurCompanyContact" DROP NOT NULL,
+ALTER COLUMN
+    "detenteurCompanyMail" DROP NOT NULL,
+ALTER COLUMN
+    "detenteurCompanyPhone" DROP NOT NULL;
