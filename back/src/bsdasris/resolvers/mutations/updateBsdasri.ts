045d35d184b4b7512dacccfa15ab68c8f3b204f6
elias showk <elias.showk@beta.gouv.fr>
2022-12-27 19:42:41 +0100

    Généralisation transporteurs étrangers : support transporterCompanyVatNumber on permissions (#1978)

--- back/src/bsdasris/resolvers/mutations/updateBsdasri.ts
+++ back/src/bsdasris/resolvers/mutations/updateBsdasri.ts
@@ -20,7 +20,7 @@ import updateBsdasri from "./updateSimpleBsdasri";
 
 /**
  * Bsdasri update mutation
- 
+
  */
 const updateBsdasriResolver = async (
   _: ResolversParentTypes["Mutation"],
@@ -42,6 +42,7 @@ const updateBsdasriResolver = async (
     emitterCompanySiret: dbBsdasri?.emitterCompanySiret,
     destinationCompanySiret: dbBsdasri?.destinationCompanySiret,
     transporterCompanySiret: dbBsdasri?.transporterCompanySiret,
+    transporterCompanyVatNumber: dbBsdasri?.transporterCompanyVatNumber,
     ecoOrganismeSiret: dbBsdasri?.ecoOrganismeSiret
   };
 
