0c7c65d1f472d17773164ba7a3d6733f8a2a083a
Laurent Paoletti <lp@providenz.fr>
2023-01-09 09:39:19 +0000

    Display loader when captcha loads

--- front/src/common/components/captcha/Captcha.tsx
+++ front/src/common/components/captcha/Captcha.tsx
@@ -2,6 +2,8 @@ import React, { useState, useCallback } from "react";
 import { TextInput, Button } from "@dataesr/react-dsfr";
 import classNames from "classnames";
 import "./Captcha.scss";
+import { Loader } from "common/components";
+
 const { VITE_API_ENDPOINT } = import.meta.env;
 
 const CaptchaAudio = ({ captchaToken }) => {
@@ -11,6 +13,7 @@ const CaptchaAudio = ({ captchaToken }) => {
     () => getCaptchaAudio(captchaToken),
     [captchaToken]
   );
+
   const getCaptchaAudio = async captchaToken => {
     fetch(`${VITE_API_ENDPOINT}/captcha-audio/${captchaToken}`)
       .then(res => {
@@ -67,7 +70,11 @@ export const Captcha = ({
           "captcha__wrapper--narrow": narrow,
         })}
       >
-        <img src={captchaImg} alt="Captcha visuel" />
+        {!!captchaImg ? (
+          <img src={captchaImg} alt="Captcha visuel" />
+        ) : (
+          <Loader />
+        )}
 
         <div
           className={classNames("captcha__components", {
