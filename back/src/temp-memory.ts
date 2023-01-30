429910971289f081b5c8b96d13c69f57ec6d309a
Orion C <orioncharlier@gmail.com>
2023-01-30 15:27:20 +0100

    Clean gql cache & enhance snapshot

---
+++ back/src/temp-memory.ts
@@ -0,0 +1,10 @@
+import { resetCaches } from "graphql-tag";
+import logger from "./logging/logger";
+
+const GQL_CLEANUP_INTERVAL = 1000 * 60 * 60;
+
+export function cleanGqlCaches() {
+  resetCaches();
+  logger.info("MEM TMP - Cleaned gql cache");
+  setTimeout(cleanGqlCaches, GQL_CLEANUP_INTERVAL);
+}
