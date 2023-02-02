a2804544cd04180092553772ffd8664c0552818c
Orion C <orioncharlier@gmail.com>
2023-02-02 16:54:24 +0100

    Graphql-tag patch

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,15 +1,11 @@
 import { app, startApolloServer } from "./server";
 import { closeQueues } from "./queue/producers";
-import { cleanGqlCaches } from "./temp-memory";
 
 const port = process.env.API_PORT || 80;
 
 async function start() {
   await startApolloServer();
   app.listen(port, () => console.info(`Server is running on port ${port}`));
-
-  // TODO - To remove. Either completely if it doesnt work or with a better fix if it does
-  cleanGqlCaches();
 
   function shutdown() {
     return closeQueues().finally(process.exit());
