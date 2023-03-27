545d67dbc01dab327c0b3f239c4fa19cb5003371
Benoit Guigal <benoit.guigal@protonmail.com>
2023-03-27 13:08:43 +0200

    use VARCHAR instead of TEXT

--- back/prisma/migrations/127_bsff_add_acceptation_numero.sql
+++ back/prisma/migrations/127_bsff_add_acceptation_numero.sql
@@ -1,7 +1,12 @@
 ALTER TABLE
   "default$default"."BsffPackaging"
 ADD
-  COLUMN "emissionNumero" TEXT;
+  COLUMN "emissionNumero" VARCHAR(100);
+
+ALTER TABLE
+  "default$default"."BsffPackaging"
+ALTER COLUMN
+  "numero" TYPE VARCHAR(100);
 
 UPDATE
   "default$default"."BsffPackaging"
