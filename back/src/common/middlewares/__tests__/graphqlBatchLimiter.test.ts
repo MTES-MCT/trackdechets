5577570644b7e59464868b27f57c660294815a9c
Elias Showk <elias.showk@beta.gouv.fr>
2023-04-05 12:03:14 +0200

    recode graphqlBatchLmiter for query merging

--- back/src/common/middlewares/__tests__/graphqlBatchLimiter.test.ts
+++ back/src/common/middlewares/__tests__/graphqlBatchLimiter.test.ts
@@ -45,11 +45,26 @@ describe("graphqlBatchLimiterMiddleware", () => {
 
   it("should allow query batching that respect the batch limit", async () => {
     const body = JSON.stringify({
-      foo: "bar",
-      zou: "pop",
-      ket: "fol",
-      bip: "bip",
-      pap: "pif"
+      operationName: null,
+      variables: {},
+      query:
+        "{\n" +
+        '  _34035399400023: companyInfos(siret: "34035399400023") {\n' +
+        "    allowBsdasriTakeOverWithoutSignature\n" +
+        "  }\n" +
+        '  _20002305900112: companyInfos(siret: "20002305900112") {\n' +
+        "    allowBsdasriTakeOverWithoutSignature\n" +
+        "  }\n" +
+        '  _26030017300010: companyInfos(siret: "26030017300010") {\n' +
+        "    allowBsdasriTakeOverWithoutSignature\n" +
+        "  }\n" +
+        '  _53063610900429: companyInfos(siret: "53063610900429") {\n' +
+        "    allowBsdasriTakeOverWithoutSignature\n" +
+        "  }\n" +
+        '  _18008901307630: companyInfos(siret: "18008901307630") {\n' +
+        "    allowBsdasriTakeOverWithoutSignature\n" +
+        "  }\n" +
+        "}\n"
     });
 
     const response = await request
@@ -61,20 +76,39 @@ describe("graphqlBatchLimiterMiddleware", () => {
 
   it("should fail if query batching has too many operations", async () => {
     const body = JSON.stringify({
-      foo: "bar",
-      zou: "pop",
-      ket: "fol",
-      bip: "bip",
-      pap: "pif",
-      pof: "bim"
+      operationName: null,
+      variables: {},
+      query:
+        "{\n" +
+        '  _34035399400023: companyInfos(siret: "34035399400023") {\n' +
+        "    allowBsdasriTakeOverWithoutSignature\n" +
+        "  }\n" +
+        '  _20002305900112: companyInfos(siret: "20002305900112") {\n' +
+        "    allowBsdasriTakeOverWithoutSignature\n" +
+        "  }\n" +
+        '  _26030017300010: companyInfos(siret: "26030017300010") {\n' +
+        "    allowBsdasriTakeOverWithoutSignature\n" +
+        "  }\n" +
+        '  _53063610900429: companyInfos(siret: "53063610900429") {\n' +
+        "    allowBsdasriTakeOverWithoutSignature\n" +
+        "  }\n" +
+        '  _18008901307630: companyInfos(siret: "18008901307630") {\n' +
+        "    allowBsdasriTakeOverWithoutSignature\n" +
+        "  }\n" +
+        '  _82944026200014: companyInfos(siret: "82944026200014") {\n' +
+        "    allowBsdasriTakeOverWithoutSignature\n" +
+        "  }\n" +
+        "}\n"
     });
+
     const response = await request
       .post(graphQLPath)
       .set("Content-Type", "application/json")
       .send(body);
     expect(response.status).toEqual(400);
-    expect(response.text).toContain(
-      "Batching is limited to 5 operations per query."
+    const { error } = JSON.parse(response.text) as any;
+    expect(error).toContain(
+      "Batching by query merging is limited to 5 operations per query."
     );
   });
 });
--- back/src/common/middlewares/graphqlBatchLimiter.ts
+++ back/src/common/middlewares/graphqlBatchLimiter.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { OperationDefinitionNode, parse } from "graphql";
 
 const MAX_OPERATIONS_PER_REQUEST = 5;
 
@@ -10,18 +11,23 @@ export function graphqlBatchLimiterMiddleware() {
   ) {
     const { body } = req;
 
-    if (Array.isArray(body)) {
-      if (body.length > MAX_OPERATIONS_PER_REQUEST) {
-        return res.status(400).send({
-          error: `Batching is limited to ${MAX_OPERATIONS_PER_REQUEST} operations per request.`
-        });
-      }
+    // query batching with arrays is a Transport-level batching technique
+    if (Array.isArray(body) && body.length > MAX_OPERATIONS_PER_REQUEST) {
+      return res.status(400).send({
+        error: `Batching is limited to ${MAX_OPERATIONS_PER_REQUEST} operations per request.`
+      });
     }
 
-    if (Object.keys(body).length > MAX_OPERATIONS_PER_REQUEST) {
-      return res.status(400).send({
-        error: `Batching is limited to ${MAX_OPERATIONS_PER_REQUEST} operations per query.`
-      });
+    if (!!body.query && typeof body.query === "string") {
+      // query merging is a batching technique that serialize multiple queries withing the same gql query
+      const ast = parse(body.query);
+      const [definition] = ast?.definitions as OperationDefinitionNode[];
+      const mergedQueries = definition?.selectionSet?.selections?.length;
+      if (mergedQueries > MAX_OPERATIONS_PER_REQUEST) {
+        return res.status(400).send({
+          error: `Batching by query merging is limited to ${MAX_OPERATIONS_PER_REQUEST} operations per query.`
+        });
+      }
     }
 
     next();
