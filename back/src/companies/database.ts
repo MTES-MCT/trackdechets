ecf9361fa35da63dc29647a67f4407c670511cac
GaelFerrand <45355989+GaelFerrand@users.noreply.github.com>
2023-04-14 11:57:34 +0200

    [TRA-11063] Envoi d'un mail aux admins d'une entreprise ayant ignoré une RevisionRequest pendant 5 jours (BSD + BSDA) (#2288)
    * feat: started implementing feature with BSDs
    
    * feat: added implementation for bsdas
    
    * refactor: added mail template and big refactorings
    
    * fix: fixes, tests and improvements
    
    * doc: added comment to explicit function's return value
    
    * fix: removed useless function
    
    * fix: fixes + added tests
    
    * fix: removed test.only
    
    * fix: prettify

--- back/src/companies/database.ts
+++ back/src/companies/database.ts
@@ -196,13 +196,17 @@ export async function getCompanyAdminUsers(orgId: string) {
 
 /**
  * Get all the admins from companies, by companyIds
- * @param companyIds
- * @returns
  */
-export async function getActiveAdminsByCompanyIds(companyIds: string[]) {
+export async function getActiveAdminsByCompanyIds(
+  companyIds: string[]
+): Promise<Record<string, User[]>> {
   const users = await prisma.companyAssociation
     .findMany({
-      where: { companyId: { in: companyIds }, role: "ADMIN" },
+      where: {
+        companyId: { in: companyIds },
+        role: "ADMIN",
+        user: { isActive: true }
+      },
       include: { user: true }
     })
     .then(associations =>
@@ -214,17 +218,47 @@ export async function getActiveAdminsByCompanyIds(companyIds: string[]) {
       })
     );
 
-  const res = {};
+  const res: Record<string, User[]> = {};
 
-  users
-    .filter(user => user.isActive)
-    .forEach(user => {
-      if (res[user.companyId]) res[user.companyId].push(user);
-      else res[user.companyId] = [user];
-    });
+  users.forEach(user => {
+    if (res[user.companyId]) res[user.companyId].push(user);
+    else res[user.companyId] = [user];
+  });
 
   return res;
 }
+
+/**
+ * Get all the companies and admins from companies, by companyOrgIds
+ * Will return an object like:
+ * {
+ *   [ordId]: { ...company, admins: user[] }
+ * }
+ */
+export const getCompaniesAndActiveAdminsByCompanyOrgIds = async (
+  orgIds: string[]
+): Promise<Record<string, Company & { admins: User[] }>> => {
+  const companies = await prisma.company.findMany({
+    where: { orgId: { in: orgIds } },
+    include: {
+      companyAssociations: {
+        where: { role: "ADMIN", user: { isActive: true } },
+        include: { user: true, company: true }
+      }
+    }
+  });
+
+  return companies.reduce<Record<string, Company & { admins: User[] }>>(
+    (companiesAndAdminsByOrgId, { companyAssociations, ...company }) => ({
+      ...companiesAndAdminsByOrgId,
+      [company.orgId]: {
+        ...company,
+        admins: companyAssociations.map(({ user }) => user)
+      }
+    }),
+    {}
+  );
+};
 
 export async function getTraderReceiptOrNotFound({
   id
