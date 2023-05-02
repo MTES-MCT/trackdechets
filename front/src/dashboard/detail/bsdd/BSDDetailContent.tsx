cb0247f2e01b4344c32dfc024149021e265ae2ea
GaelFerrand <gael.ferrand@gmail.com>
2023-05-02 16:36:02 +0200

    fix: ui should not crash if no recipient

--- front/src/dashboard/detail/bsdd/BSDDetailContent.tsx
+++ front/src/dashboard/detail/bsdd/BSDDetailContent.tsx
@@ -123,8 +123,8 @@ const TempStorage = ({ form }) => {
     <>
       <div className={styles.detailColumns}>
         <div className={styles.detailGrid}>
-          <DetailRow value={form?.recipient?.company.name} label="Nom" />
-          <DetailRow value={form?.recipient?.company.siret} label="Siret" />
+          <DetailRow value={form?.recipient?.company?.name} label="Nom" />
+          <DetailRow value={form?.recipient?.company?.siret} label="Siret" />
           <DetailRow
             value={temporaryStorageDetail?.wasteDetails?.code}
             label="Code déchet"
