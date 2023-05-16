2207166d8539e652831bb2682d6c98fbf5ef7550
elias showk <elias.showk@beta.gouv.fr>
2023-05-16 14:45:44 +0200

    tra-11803 Impossible de compléter le BSD de suite quand un brouillon de BSD suite contient un siret fermé (#2392)
    * Revert "Revert "Replace checkForCloedCompanies by Sirenify""
    * remove siret validation in markAsSealed
    * remove markAsSealed deprecated tests

--- back/src/forms/resolvers/mutations/markAsSealed.ts
+++ back/src/forms/resolvers/mutations/markAsSealed.ts
@@ -7,8 +7,7 @@ import {
   beforeSignedByTransporterSchema,
   checkCanBeSealed,
   validateForwardedInCompanies,
-  wasteDetailsSchema,
-  checkForClosedCompanies
+  wasteDetailsSchema
 } from "../../validation";
 import transitionForm from "../../workflow/transitionForm";
 import { EventType } from "../../workflow/types";
@@ -63,13 +62,6 @@ const markAsSealedResolver: MutationResolvers["markAsSealed"] = async (
         where: { siret: form.emitterCompanySiret }
       })) > 0
     : false;
-
-  /**
-   * Check for closed companies or throw an exception
-   */
-  if (process.env.VERIFY_COMPANY === "true") {
-    await checkForClosedCompanies(form.id);
-  }
 
   const resultingForm = await runInTransaction(async transaction => {
     const formRepository = getFormRepository(user, transaction);
