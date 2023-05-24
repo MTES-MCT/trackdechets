d65092720e3f2b2d781a7a8be29b99e2975c13db
Benoit Guigal <benoit.guigal@protonmail.com>
2023-05-24 12:48:55 +0200

    use updated data in duplicateBsvhu

--- back/src/bsvhu/resolvers/mutations/duplicate.ts
+++ back/src/bsvhu/resolvers/mutations/duplicate.ts
@@ -1,4 +1,4 @@
-import { Prisma, BsvhuStatus } from "@prisma/client";
+import { Prisma, BsvhuStatus, Bsvhu } from "@prisma/client";
 import { checkIsAuthenticated } from "../../../common/permissions";
 import getReadableId, { ReadableIdPrefix } from "../../../forms/readableId";
 import { MutationDuplicateBsvhuArgs } from "../../../generated/graphql/types";
@@ -6,6 +6,7 @@ import { expandVhuFormFromDb } from "../../converter";
 import { getBsvhuOrNotFound } from "../../database";
 import { getBsvhuRepository } from "../../repository";
 import { checkCanDuplicate } from "../../permissions";
+import prisma from "../../../prisma";
 
 export default async function duplicate(
   _,
@@ -18,36 +19,110 @@ export default async function duplicate(
 
   await checkCanDuplicate(user, prismaBsvhu);
   const bsvhuRepository = getBsvhuRepository(user);
-  const newBsvhu = await bsvhuRepository.create(getDuplicateData(prismaBsvhu));
+  const newBsvhu = await bsvhuRepository.create(
+    await getDuplicateData(prismaBsvhu)
+  );
 
   return expandVhuFormFromDb(newBsvhu);
 }
 
-function getDuplicateData({
-  id,
-  createdAt,
-  updatedAt,
-  emitterEmissionSignatureAuthor,
-  emitterEmissionSignatureDate,
-  transporterTransportSignatureAuthor,
-  transporterTransportSignatureDate,
-  destinationReceptionQuantity,
-  destinationReceptionWeight,
-  destinationReceptionAcceptationStatus,
-  destinationReceptionRefusalReason,
-  destinationReceptionIdentificationNumbers,
-  destinationReceptionIdentificationType,
-  destinationReceptionDate,
-  destinationOperationDate,
-  destinationOperationCode,
-  destinationOperationSignatureAuthor,
-  destinationOperationSignatureDate,
-  ...rest
-}): Prisma.BsvhuCreateInput {
+async function getDuplicateData(
+  bsvhu: Bsvhu
+): Promise<Prisma.BsvhuCreateInput> {
+  const {
+    id,
+    createdAt,
+    updatedAt,
+    emitterEmissionSignatureAuthor,
+    emitterEmissionSignatureDate,
+    transporterTransportSignatureAuthor,
+    transporterTransportSignatureDate,
+    destinationReceptionQuantity,
+    destinationReceptionWeight,
+    destinationReceptionAcceptationStatus,
+    destinationReceptionRefusalReason,
+    destinationReceptionIdentificationNumbers,
+    destinationReceptionIdentificationType,
+    destinationReceptionDate,
+    destinationOperationDate,
+    destinationOperationCode,
+    destinationOperationSignatureAuthor,
+    destinationOperationSignatureDate,
+    ...rest
+  } = bsvhu;
+
+  const { emitter, transporter, destination } = await getBsvhuCompanies(bsvhu);
+
   return {
     ...rest,
     id: getReadableId(ReadableIdPrefix.VHU),
     status: BsvhuStatus.INITIAL,
-    isDraft: true
+    isDraft: true,
+    emitterCompanyName: emitter?.name ?? bsvhu.emitterCompanyName,
+    emitterCompanyAddress: emitter?.address ?? bsvhu.emitterCompanyAddress,
+    emitterCompanyContact: emitter?.contact ?? bsvhu.emitterCompanyContact,
+    emitterCompanyPhone: emitter?.contactPhone ?? bsvhu.emitterCompanyPhone,
+    emitterCompanyMail: emitter?.contactEmail ?? bsvhu.emitterCompanyMail,
+    destinationCompanyName: destination?.name ?? bsvhu.destinationCompanyName,
+    destinationCompanyAddress:
+      destination?.address ?? bsvhu.destinationCompanyAddress,
+    destinationCompanyContact:
+      destination?.contact ?? bsvhu.destinationCompanyContact,
+    destinationCompanyPhone:
+      destination?.contactPhone ?? bsvhu.destinationCompanyPhone,
+    destinationCompanyMail:
+      destination?.contactEmail ?? bsvhu.destinationCompanyMail,
+    transporterCompanyName: transporter?.name ?? bsvhu.transporterCompanyName,
+    transporterCompanyAddress:
+      transporter?.address ?? bsvhu.transporterCompanyAddress,
+    transporterCompanyContact:
+      transporter?.contact ?? bsvhu.transporterCompanyContact,
+    transporterCompanyPhone:
+      transporter?.contactPhone ?? bsvhu.transporterCompanyPhone,
+    transporterCompanyMail:
+      transporter?.contactEmail ?? bsvhu.transporterCompanyMail,
+    transporterCompanyVatNumber: bsvhu.transporterCompanyVatNumber,
+    transporterRecepisseNumber:
+      transporter?.transporterReceipt?.receiptNumber ??
+      bsvhu.transporterRecepisseNumber,
+    transporterRecepisseDepartment:
+      transporter?.transporterReceipt?.department ??
+      bsvhu.transporterRecepisseDepartment,
+    transporterRecepisseValidityLimit:
+      transporter?.transporterReceipt?.validityLimit ??
+      bsvhu.transporterRecepisseValidityLimit
   };
+}
+
+async function getBsvhuCompanies(bsvhu: Bsvhu) {
+  const companiesOrgIds = [
+    bsvhu.emitterCompanySiret,
+    bsvhu.transporterCompanySiret,
+    bsvhu.transporterCompanyVatNumber,
+    bsvhu.destinationCompanySiret
+  ].filter(Boolean);
+
+  // Batch fetch all companies involved in the BSVHU
+  const companies = await prisma.company.findMany({
+    where: { orgId: { in: companiesOrgIds } },
+    include: {
+      transporterReceipt: true
+    }
+  });
+
+  const emitter = companies.find(
+    company => company.orgId === bsvhu.emitterCompanySiret
+  );
+
+  const destination = companies.find(
+    company => company.orgId === bsvhu.destinationCompanySiret
+  );
+
+  const transporter = companies.find(
+    company =>
+      company.orgId === bsvhu.transporterCompanySiret ||
+      company.orgId === bsvhu.transporterCompanyVatNumber
+  );
+
+  return { emitter, destination, transporter };
 }
