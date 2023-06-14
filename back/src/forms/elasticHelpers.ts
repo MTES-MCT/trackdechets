e8f3a92fd321b9387e78dad897e05d0a29d735b4
Benoit Guigal <benoit.guigal@protonmail.com>
2023-06-14 08:45:25 +0200

    fix getSiretByTab

--- back/src/forms/elasticHelpers.ts
+++ back/src/forms/elasticHelpers.ts
@@ -141,19 +141,24 @@ export function getSiretsByTab(form: FullForm): Pick<BsdElastic, WhereKeys> {
     }
     case Status.SENT: {
       setFieldTab("recipientCompanySiret", "isForActionFor");
-      setFieldTab("transporterCompanySiret", "isCollectedFor");
+      // whether or not this BSD has been handed over by transporter n°1
+      let hasBeenHandedOver = false;
 
-      (form.transporters ?? [])
-        .filter(t => t.number && t.number > 1)
-        .forEach(transporter => {
-          if (transporter.takenOverAt) {
-            setFieldTab(transporter.id, "isCollectedFor");
-          } else {
-            if (transporter.readyToTakeOver) {
-              setFieldTab(transporter.id, "isToCollectFor");
-            }
+      form.transporters
+        ?.filter(t => t.number && t.number >= 2)
+        .forEach(segment => {
+          if (segment.readyToTakeOver) {
+            hasBeenHandedOver = hasBeenHandedOver || !!segment.takenOverAt;
+            setFieldTab(
+              segment.id,
+              segment.takenOverAt ? "isCollectedFor" : "isToCollectFor"
+            );
           }
         });
+
+      if (!hasBeenHandedOver) {
+        setFieldTab("transporterCompanySiret", "isCollectedFor");
+      }
 
       break;
     }
