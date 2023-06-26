2ff346388aea8b0f503d5bf17e29236ef1701c0b
Orion C <orioncharlier@gmail.com>
2023-06-26 17:43:52 +0200

    Fix

--- back/src/bsda/validation/validate.ts
+++ back/src/bsda/validation/validate.ts
@@ -271,6 +271,7 @@ async function validateDestination(
   const currentBsda = await findUnique({ id: bsda.id });
 
   if (
+    currentBsda &&
     currentBsda.destinationCompanySiret !== bsda.destinationCompanySiret &&
     bsda.destinationOperationNextDestinationCompanySiret !==
       currentBsda.destinationCompanySiret
