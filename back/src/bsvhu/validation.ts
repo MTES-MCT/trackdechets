db282655cda0ff2be288a06280233863f633d748
GaelFerrand <gael.ferrand@gmail.com>
2023-07-05 09:27:24 +0200

    fix: reverting changes to VHU

--- back/src/bsvhu/validation.ts
+++ back/src/bsvhu/validation.ts
@@ -239,20 +239,6 @@ const transporterSchema: FactorySchemaOf<
   Transporter
 > = context =>
   yup.object({
-    transporterNumberPlate: yup
-      .string()
-      .nullable()
-      .test((transporterNumberPlate, ctx) => {
-        const { transporterTransportMode } = ctx.parent;
-
-        if (transporterTransportMode === "ROAD" && !transporterNumberPlate) {
-          return new yup.ValidationError(
-            "La plaque d'immatriculation est requise"
-          );
-        }
-
-        return true;
-      }),
     transporterCompanyName: yup
       .string()
       .requiredIf(
