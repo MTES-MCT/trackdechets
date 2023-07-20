8f8e52726833aae93b4d9374b812a068a04ef703
Benoit Guigal <benoit.guigal@protonmail.com>
2023-07-20 08:26:21 +0200

    use GIN index for companyTypes

--- back/prisma/migrations/148_create_company_indices.sql
+++ back/prisma/migrations/148_create_company_indices.sql
@@ -3,10 +3,14 @@
 CREATE INDEX IF NOT EXISTS "_CompanyCreatedAtIdx" ON "default$default"."Company"("createdAt");
 
 -- CreateIndex
-CREATE INDEX IF NOT EXISTS "_CompanyTypesIdx" ON "default$default"."Company"("companyTypes");
-
--- CreateIndex
 CREATE INDEX IF NOT EXISTS "_CompanyVerificationStatusIdx" ON "default$default"."Company"("verificationStatus");
 
 -- CreateIndex
 CREATE INDEX IF NOT EXISTS "_CompanyAssociationRoleIdx" ON "default$default"."CompanyAssociation"("role");
+
+-- Create GIN Index
+-- > GIN indexes are “inverted indexes” which are appropriate for data values that 
+-- > contain multiple component values, such as arrays.
+-- > An inverted index contains a separate entry for each component value, 
+-- > and can efficiently handle queries that test for the presence of specific component values.
+CREATE INDEX IF NOT EXISTS "_CompanyTypesIdx" ON "default$default"."Company" USING GIN ("companyTypes");
