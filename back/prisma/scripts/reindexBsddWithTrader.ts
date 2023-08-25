142148a18a324ee0b8d8006195691b9b236d29d4
Benoit Guigal <benoit.guigal@protonmail.com>
2023-08-25 10:00:15 +0200

    add script to reindex bsd with trader

---
+++ back/prisma/scripts/reindexBsddWithTrader.ts
@@ -0,0 +1,17 @@
+import { Updater, registerUpdater } from "./helper/helper";
+import prisma from "../../src/prisma";
+import { enqueueUpdatedBsdToIndex } from "../../src/queue/producers/elastic";
+
+@registerUpdater("Reindex BSDD with traders", "Reindex BSDD with traders", true)
+export class ReindexBsddWitTrader implements Updater {
+  async run() {
+    const bsds = await prisma.form.findMany({
+      where: { traderCompanySiret: { not: null } },
+      select: { readableId: true }
+    });
+    for (const bsd of bsds) {
+      // ~ 150 000 bordereaux en prod
+      enqueueUpdatedBsdToIndex(bsd.readableId);
+    }
+  }
+}
