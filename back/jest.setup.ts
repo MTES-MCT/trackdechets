4cacea3c160b5d50f615878a584ae8f8a298f399
elias@showk.me <elias@showk.me>
2023-09-07 12:22:45 +0200

    remove mongo from tests setup

--- back/jest.setup.ts
+++ back/jest.setup.ts
@@ -1,8 +1,6 @@
 import { redisClient } from "./src/common/redis";
-import { closeMongoClient } from "./src/events/mongodb";
 
 afterAll(async () => {
   jest.restoreAllMocks();
-  await closeMongoClient();
   await redisClient.quit();
 });
