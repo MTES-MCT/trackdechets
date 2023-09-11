f09e0078f84dc5bbfa268d20407d0054cca91067
elias@showk.me <elias@showk.me>
2023-09-11 22:28:47 +0200

    lint

--- back/src/companies/types.ts
+++ back/src/companies/types.ts
@@ -1,16 +1,3 @@
-
-export const allFavoriteTypes: FavoriteType[] = [
-  "BROKER",
-  "DESTINATION",
-  "EMITTER",
-  "NEXT_DESTINATION",
-  "RECIPIENT",
-  "TEMPORARY_STORAGE_DETAIL",
-  "TRADER",
-  "TRANSPORTER",
-  "WORKER"
-];
-
 import { FavoriteType } from "../generated/graphql/types";
 
 export { CompanySearchResult } from "../generated/graphql/types";
@@ -23,3 +10,15 @@ export interface CompanyBaseIdentifiers {
   siret: string;
   vatNumber?: string;
 }
+
+export const allFavoriteTypes: FavoriteType[] = [
+  "BROKER",
+  "DESTINATION",
+  "EMITTER",
+  "NEXT_DESTINATION",
+  "RECIPIENT",
+  "TEMPORARY_STORAGE_DETAIL",
+  "TRADER",
+  "TRANSPORTER",
+  "WORKER"
+];
