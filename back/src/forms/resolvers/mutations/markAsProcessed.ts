1cc955fb1494e41b9d18470ba5cf09c7ad50a739
Benoit Guigal <benoit.guigal@protonmail.com>
2023-09-13 11:55:13 +0200

    POC traçabilité complète

--- back/src/forms/resolvers/mutations/markAsProcessed.ts
+++ back/src/forms/resolvers/mutations/markAsProcessed.ts
@@ -1,5 +1,8 @@
-import { EmitterType, Prisma, Status } from "@prisma/client";
-import { PROCESSING_OPERATIONS } from "../../../common/constants";
+import { EmitterType, Form, Prisma, Status } from "@prisma/client";
+import {
+  PROCESSING_OPERATIONS,
+  ProcessingOperationType
+} from "../../../common/constants";
 import { checkIsAuthenticated } from "../../../common/permissions";
 import { MutationResolvers } from "../../../generated/graphql/types";
 import { getFormOrFormNotFound } from "../../database";
@@ -21,6 +24,7 @@ import {
   isSiret,
   isVat
 } from "../../../common/constants/companySearchHelpers";
+import prisma from "../../../prisma";
 
 const markAsProcessedResolver: MutationResolvers["markAsProcessed"] = async (
   parent,
@@ -124,7 +128,79 @@ const markAsProcessedResolver: MutationResolvers["markAsProcessed"] = async (
     return processedForm;
   });
 
+  await operationHook(processedForm, processedForm.id);
+
   return getAndExpandFormFromDb(processedForm.id);
 };
 
 export default markAsProcessedResolver;
+
+type OperationHookArgs = Pick<
+  Form,
+  | "readableId"
+  | "quantityReceived"
+  | "processingOperationDone"
+  | "recipientCompanySiret"
+  | "recipientCompanyName"
+  | "noTraceability"
+>;
+
+// Hook qui est appelé à chaque fois qu'un applique une opération
+// de traitement sur un bordereau
+// À ajouter dans une queue plutôt que de faire ça en synchrone
+export async function operationHook(
+  // Informations sur le traitement final
+  operation: OperationHookArgs,
+  // Identifiant du bordereau
+  formId: string
+) {
+  // Codes de traitement finaux
+  const finalOperationCodes = PROCESSING_OPERATIONS.filter(
+    p =>
+      p.type === ProcessingOperationType.Eliminiation ||
+      p.type === ProcessingOperationType.Valorisation
+  ).map(p => p.code);
+  if (
+    finalOperationCodes.includes(operation.processingOperationDone!) ||
+    operation.noTraceability
+  ) {
+    // On va chercher tous les bordereaux initiaux
+
+    const formWithInitialForms = await prisma.form.findUniqueOrThrow({
+      where: { id: formId },
+      include: {
+        forwarding: true,
+        grouping: { include: { initialForm: true } }
+      }
+    });
+
+    const initialForms = [
+      formWithInitialForms.forwarding,
+      ...(formWithInitialForms.grouping ?? []).map(g => g.initialForm)
+    ].filter(Boolean);
+
+    for (const initialForm of initialForms) {
+      await prisma.form.update({
+        where: { id: initialForm.id },
+        data: {
+          finalOperations: {
+            create: {
+              finalFormReadableId: operation.readableId,
+              quantity: operation.quantityReceived!,
+              operationCode: operation.processingOperationDone!,
+              destinationCompanySiret: operation.recipientCompanySiret!,
+              destinationCompanyName: operation.recipientCompanyName!
+            }
+          }
+        }
+      });
+
+      // Applique le hook de façon récursive sur les bordereaux initiaux
+      // TODO gérer ça avec une queue
+      // Potentiel point particulier : s'il y a eu scission puis regroupement
+      // le hook pourrait être appelé plusieurs fois, il faudrait donc ajouter
+      // un check sur l'unicité de finalFormReadableId
+      await operationHook(operation, initialForm.id);
+    }
+  }
+}
