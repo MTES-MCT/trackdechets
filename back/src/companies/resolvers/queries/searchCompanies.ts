6baa264869d381867cd42ac76e31c188f9227574
elias showk <elias.showk@beta.gouv.fr>
2023-10-24 14:00:22 +0200

    Deny access to unauthenticated searchCompanies queries (#2781)

--- back/src/companies/resolvers/queries/searchCompanies.ts
+++ back/src/companies/resolvers/queries/searchCompanies.ts
@@ -1,10 +1,14 @@
+import { checkIsAuthenticated } from "../../../common/permissions";
 import { QueryResolvers } from "../../../generated/graphql/types";
 import { searchCompanies } from "../../search";
 
 const searchCompaniesResolver: QueryResolvers["searchCompanies"] = async (
   _,
   { clue, department, allowForeignCompanies },
-  _context
-) => searchCompanies(clue, department, allowForeignCompanies);
+  context
+) => {
+  checkIsAuthenticated(context);
+  return searchCompanies(clue, department, allowForeignCompanies);
+};
 
 export default searchCompaniesResolver;
