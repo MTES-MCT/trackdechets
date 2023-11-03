c29aed0ecec61746bbbbd6aee6335be1d3697bb6
GaelFerrand <gael.ferrand@gmail.com>
2023-11-03 17:07:57 +0100

    feat: added Playwright to repo + added first recorded e2e test (user registration)

---
+++ front/e2e/registration.spec.ts
@@ -0,0 +1,16 @@
+import { test } from "@playwright/test";
+
+test("User ONE should be able to register", async ({ page }) => {
+  await page.goto("http://trackdechets.local/login");
+  await page.getByLabel("Email").click();
+  await page.getByRole("link", { name: "Créer un compte" }).click();
+  await page.getByLabel("Nom et prénom").click();
+  await page.getByLabel("Nom et prénom").fill("User ONE");
+  await page.getByLabel("Nom et prénom").press("Tab");
+  await page.getByLabel("Email").fill("user.one@beta.gouv.fr");
+  await page.getByLabel("Email").press("Tab");
+  await page.getByLabel("Mot de passe", { exact: true }).click();
+  await page.getByLabel("Mot de passe", { exact: true }).fill("Tr4ckDeChEt7!");
+  await page.getByText("Je certifie avoir lu les conditions générales").click();
+  await page.getByRole("button", { name: "Créer mon compte" }).click();
+});
