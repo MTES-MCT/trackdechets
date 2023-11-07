12de02270af25c73d2085df3fbaa7438a68c441b
Orion C <orioncharlier@gmail.com>
2023-11-07 23:07:23 +0100

    Fix dependencies problem

--- scripts/build/scalingo.sh
+++ scripts/build/scalingo.sh
@@ -8,7 +8,14 @@ npx nx run-many -t build
 echo "Pruning dev dependencies (this is only done by Scalingo when NODE_ENV=production)"
 npm prune --production
 
-# Until the monorepo is properly split, the back folder holds several apps.
+####
+# Hacks while we have to run non main entries files
+####
+
+# Copy shared/constants output to node_modules so that import works
+cp -R dist/shared node_modules/
+
+# The back folder holds several apps.
 # Some scripts must only run for the main API
 if [[ "$APP" == *api ]]; then
     echo "Running migrate for $APP"
