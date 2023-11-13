4e30b817540d739dfa4044748527f0b44ac5acb4
GaelFerrand <45355989+GaelFerrand@users.noreply.github.com>
2023-11-13 11:15:52 +0100

    [TRA-11095] Correction sur les quantités estimées dans les PDFs (#2850)
    * fix: fixing layout
    
    * fix: format

--- back/src/forms/pdf/generateBsddPdf.tsx
+++ back/src/forms/pdf/generateBsddPdf.tsx
@@ -84,7 +84,9 @@ function QuantityFields({ quantity, quantityType }: QuantityFieldsProps) {
         checked={quantityType === QuantityType.ESTIMATED}
         readOnly
       />{" "}
-      Estimée "QUANTITÉE ESTIMÉE CONFORMÉMENT AU 5.4.1.1.3.2" de l'ADR 2023
+      Estimée
+      <br />
+      "QUANTITÉE ESTIMÉE CONFORMÉMENT AU 5.4.1.1.3.2" de l'ADR 2023
     </p>
   );
 }
