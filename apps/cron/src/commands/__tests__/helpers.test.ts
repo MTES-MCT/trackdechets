f28cb3224de6a51decd1ebcf3e9523c4ae441c7b
Orion C <orioncharlier@gmail.com>
2023-11-15 17:38:02 +0100

    Fix test

--- apps/cron/src/commands/__tests__/helpers.test.ts
+++ apps/cron/src/commands/__tests__/helpers.test.ts
@@ -1,5 +1,7 @@
 import { xDaysAgo } from "../onboarding.helpers";
 
+jest.mock("back");
+
 describe("xDaysAgo", () => {
   it("should return a relative past date", () => {
     const someDate = new Date("2019-10-03T00:00:00.000Z");
