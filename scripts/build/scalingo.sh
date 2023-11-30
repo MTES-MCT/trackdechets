633b5c8e5c9c892b21aa4eeb58d3959db6761d37
Orion C <orioncharlier@gmail.com>
2023-11-30 10:11:06 +0100

    Slugignore per app

--- scripts/build/scalingo.sh
+++ scripts/build/scalingo.sh
@@ -8,16 +8,18 @@ npx nx run-many -t build
 echo "Pruning dev dependencies (this is only done by Scalingo when NODE_ENV=production)"
 npm prune --production
 
-####
-# Hacks while we have to run non main entries files
-####
-
-# Copy shared/constants output to node_modules so that import works
-cp -R dist/shared node_modules/
-
-# The back folder holds several apps.
 # Some scripts must only run for the main API
 if [[ "$APP" == *api ]]; then
     echo "Running migrate for $APP"
     npx nx run back:migrate
+fi
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT_DIR="$(dirname $(dirname "$SCRIPT_DIR"))"
+if [[ "$APP" == *storybook ]]; then
+    cp "$ROOT_DIR/.slugignore.storybook" "$ROOT_DIR/.slugignore"
+elif [[ "$APP" == *front ]]; then
+    cp "$ROOT_DIR/.slugignore.front" "$ROOT_DIR/.slugignore"
+else
+    cp "$ROOT_DIR/.slugignore.back" "$ROOT_DIR/.slugignore"
 fi
