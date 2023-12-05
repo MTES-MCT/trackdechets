d80d7b891f2ca41bbce1ee6531ec61702fa84910
GaelFerrand <gael.ferrand@gmail.com>
2023-12-05 15:03:39 +0100

    debug CI

---
+++ e2e/src/utils/debug.ts
@@ -0,0 +1,6 @@
+import { Page } from "@playwright/test";
+
+export const logScreenshot = async (page: Page) => {
+    const buffer = await page.screenshot();
+    console.log(buffer.toString("base64"));
+};
