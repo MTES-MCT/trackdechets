f44d30e8b8b7b05a28ecdcee9e7f55436d8837c8
GaelFerrand <gael.ferrand@gmail.com>
2023-12-06 15:12:04 +0100

    fix: increasing timeout

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -15,6 +15,8 @@ export default defineConfig({
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
+  /* Timeout of 30 seconds */
+  timeout: 30 * 1000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
