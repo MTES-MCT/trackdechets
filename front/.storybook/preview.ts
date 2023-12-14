e5276d8d8a5beb85c57bd5f4863bbd0a79ff8b21
Orion C <orioncharlier@gmail.com>
2023-12-14 09:56:52 +0100

    Add dsfr css

--- front/.storybook/preview.ts
+++ front/.storybook/preview.ts
@@ -1,4 +1,6 @@
 import "../src/scss/index.scss";
+import "../public/dsfr/utility/icons/icons.min.css";
+import "../public/dsfr/dsfr.min.css";
 import { MockedProvider } from "@apollo/client/testing"; // Use for Apollo Version 3+
 
 export const parameters = {
