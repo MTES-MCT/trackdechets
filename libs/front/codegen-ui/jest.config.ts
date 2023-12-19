5529682aa1ccc7cd6cb1aff01811b61497d30159
Orion C <orioncharlier@gmail.com>
2023-12-19 18:20:40 +0100

    Move codegen-ui to libs/front

--- libs/front/codegen-ui/jest.config.ts
+++ libs/front/codegen-ui/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: "codegen-ui",
-  preset: "../jest.preset.js",
+  preset: "../../../jest.preset.js",
   testEnvironment: "node",
   transform: {
     "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }]
   },
   moduleFileExtensions: ["ts", "js", "html"],
-  coverageDirectory: "../coverage/codegen-ui"
+  coverageDirectory: "../../../coverage/libs/front/codegen-ui"
 };
