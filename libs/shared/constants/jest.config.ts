a86f2b0a5877c89a1c1b41666e87792e41dbf642
Orion C <orioncharlier@gmail.com>
2023-12-19 18:23:00 +0100

    Move constants to libs/shared

--- libs/shared/constants/jest.config.ts
+++ libs/shared/constants/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: "constants",
-  preset: "../../jest.preset.js",
+  preset: "../../../jest.preset.js",
   testEnvironment: "node",
   transform: {
     "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }]
   },
   moduleFileExtensions: ["ts", "js", "html"],
-  coverageDirectory: "../../coverage/shared/constants"
+  coverageDirectory: "../../../coverage/libs/shared/constants"
 };
