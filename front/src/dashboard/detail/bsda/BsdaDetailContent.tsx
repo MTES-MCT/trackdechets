f5d8ad5426f56a4a198f28cc20b9d31f48de07e8
Julien Seren-Rosso <julien.serenrosso@beta.gouv.fr>
2024-01-10 10:33:37 +0100

    Changes dashboard v2 url to dashboard/

--- front/src/dashboard/detail/bsda/BsdaDetailContent.tsx
+++ front/src/dashboard/detail/bsda/BsdaDetailContent.tsx
@@ -430,7 +430,7 @@ export default function BsdaDetailContent({ form }: SlipDetailContentProps) {
     variables: { id: form.id },
     onCompleted: () => {
       navigate(
-        generatePath(routes.dashboardv2.bsds.drafts, {
+        generatePath(routes.dashboard.bsds.drafts, {
           siret
         })
       );
