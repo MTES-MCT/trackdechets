01c4c4fd7a123d12805328b692ab7cf5c0ee53e5
elias@showk.me <elias@showk.me>
2024-01-30 10:16:59 +0100

    lint

--- back/src/companies/sirene/trackdechets/client.ts
+++ back/src/companies/sirene/trackdechets/client.ts
@@ -177,54 +177,83 @@ export const searchCompanies = (
     });
 };
 
+/**
+ * Construit une Raison Sociale composite
+ * @param etablissement
+ * @returns
+ */
 function buildCompanyName(etablissement: SearchStockEtablissement) {
-  let companyName = etablissement.denominationUniteLegale !== "[ND]" ?
-    etablissement.denominationUniteLegale : "";
+  // Certaines unités légales sont non-diffusibles (ND) alors que l'établissement sont ouvertes.
+  let companyName =
+    etablissement.denominationUniteLegale !== "[ND]"
+      ? etablissement.denominationUniteLegale
+      : "";
 
   const secondaryNames = [
-    // > Dénomination usuelle de l’établissement
-    // > Cette variable désigne le nom sous lequel l'établissement est connu du grand public.
-    // > Cet élément d'identification de l'établissement a été enregistré au niveau établissement depuis l'application
-    // > de la norme d'échanges CFE de 2008. Avant la norme 2008, la dénomination usuelle était enregistrée au
-    // > niveau de l'unité légale sur trois champs (cf. variables denominationUsuelle1UniteLegale à
-    // > denominationUsuelle3UniteLegale dans le descriptif des variables du fichier StockUniteLegale).
+    // Dénomination usuelle de l’établissement
+    // Cette variable désigne le nom sous lequel l'établissement est connu du grand public.
+    // Cet élément d'identification de l'établissement a été enregistré au niveau établissement depuis l'application
+    // de la norme d'échanges CFE de 2008. Avant la norme 2008, la dénomination usuelle était enregistrée au
+    // niveau de l'unité légale sur trois champs (cf. variables denominationUsuelle1UniteLegale à
+    // denominationUsuelle3UniteLegale dans le descriptif des variables du fichier StockUniteLegale).
     "denominationUsuelleEtablissement",
     "denominationUsuelle1UniteLegale",
     "denominationUsuelle2UniteLegale",
     "denominationUsuelle3UniteLegale",
-    // > Les trois variables enseigne1Etablissement, enseigne2Etablissement et enseigne3Etablissement
-    // > contiennent la ou les enseignes de l'établissement.
-    // > L'enseigne identifie l'emplacement ou le local dans lequel est exercée l'activité. Un établissement peut
-    // > posséder une enseigne, plusieurs enseignes ou aucune.
-    // > L'analyse des enseignes et de son découpage en trois variables dans Sirene montre deux cas possibles :
-    // > soit les 3 champs concernent 3 enseignes bien distinctes, soit ces trois champs correspondent au
-    // > découpage de l'enseigne qui est déclarée dans la liasse (sur un seul champ) avec une continuité des trois
-    // > champs.
+    // Les trois variables enseigne1Etablissement, enseigne2Etablissement et enseigne3Etablissement
+    // contiennent la ou les enseignes de l'établissement.
+    // L'enseigne identifie l'emplacement ou le local dans lequel est exercée l'activité. Un établissement peut
+    // posséder une enseigne, plusieurs enseignes ou aucune.
+    // L'analyse des enseignes et de son découpage en trois variables dans Sirene montre deux cas possibles :
+    // soit les 3 champs concernent 3 enseignes bien distinctes, soit ces trois champs correspondent au
+    // découpage de l'enseigne qui est déclarée dans la liasse (sur un seul champ) avec une continuité des trois
+    // champs.
     "enseigne1Etablissement",
     "enseigne2Etablissement",
     "enseigne3Etablissement",
-    // > Sigle de l’unité légale
-    // > Un sigle est une forme réduite de la raison sociale ou de la dénomination d'une personne morale ou d'un
-    // > organisme public.
-    // > Il est habituellement constitué des initiales de certains des mots de la dénomination. Afin d'en faciliter la
-    // > prononciation, il arrive qu'on retienne les deux ou trois premières lettres de certains mots : il s'agit alors, au
-    // > sens strict, d'un acronyme; mais l'usage a étendu à ce cas l'utilisation du terme sigle.
-    // > Cette variable est à null pour les personnes physiques.
-    // > Elle peut être non renseignée pour les personnes morales
+    // Sigle de l’unité légale
+    // Un sigle est une forme réduite de la raison sociale ou de la dénomination d'une personne morale ou d'un
+    // organisme public.
+    // Il est habituellement constitué des initiales de certains des mots de la dénomination. Afin d'en faciliter la
+    // prononciation, il arrive qu'on retienne les deux ou trois premières lettres de certains mots : il s'agit alors, au
+    // sens strict, d'un acronyme; mais l'usage a étendu à ce cas l'utilisation du terme sigle.
+    // Cette variable est à null pour les personnes physiques.
+    // Elle peut être non renseignée pour les personnes morales
     "sigleUniteLegale"
   ];
 
   // Try to grab useful secondary naming information in different secondary fields
   for (const secondaryName of secondaryNames) {
-    if (etablissement[secondaryName] &&
+    if (
+      etablissement[secondaryName] &&
       etablissement[secondaryName].length > 0 &&
-      etablissement[secondaryName] !== companyName) {
-      companyName = companyName ? companyName.concat(` (${etablissement[secondaryName]})`)
+      etablissement[secondaryName] !== companyName
+    ) {
+      companyName = companyName
+        ? companyName.concat(` (${etablissement[secondaryName]})`)
         : companyName.concat(` ${etablissement[secondaryName]}`);
 
       break;
     }
   }
-  return companyName.trim();
-}
 
+  // Fallback
+  if (!companyName) {
+    const isEntrepreneurIndividuel =
+      etablissement.categorieJuridiqueUniteLegale === "1000";
+    if (isEntrepreneurIndividuel) {
+      // concatener prénom et nom
+      companyName = [
+        etablissement.prenom1UniteLegale,
+        etablissement.nomUniteLegale
+      ]
+        .join(" ")
+        .trim();
+    } else {
+      // afficher denominationUniteLegale quoiqu'il arrive
+      companyName = etablissement.denominationUniteLegale;
+    }
+  }
+
+  return companyName?.trim();
+}
