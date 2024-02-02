946a63ac3f0e7a59c3f425e924cc1decd932a90d
GaelFerrand <gael.ferrand@gmail.com>
2024-02-02 08:59:48 +0100

    fix: fixed query to include nextDestinationCompanyVatNumber

--- libs/back/prisma/src/migrate/migrations/165_update_bsdd_with_final_operation_code.sql
+++ libs/back/prisma/src/migrate/migrations/165_update_bsdd_with_final_operation_code.sql
@@ -2,11 +2,11 @@
 -- Final operation code and nextDestination => remove nextDestination
 -- ==================================================================
 
--- EXPLAIN: actual time=145.152..3056.319, rows=2251, loops=1
+-- EXPLAIN: actual time=145.152..3056.319, rows=2258, loops=1
 update "default$default"."Form" 
 set "nextDestinationCompanyName" = '', "nextDestinationCompanySiret" = '', "nextDestinationCompanyAddress" = '', "nextDestinationCompanyContact" = '', "nextDestinationCompanyPhone" = '', "nextDestinationCompanyMail" = '', "nextDestinationCompanyCountry" = '', "nextDestinationCompanyVatNumber" = '', "nextDestinationNotificationNumber" = '', "nextDestinationProcessingOperation" = ''
 where "processingOperationDone" in ('R 0', 'R 1', 'R 2', 'R 3', 'R 4', 'R 5', 'R 6', 'R 7', 'R 8', 'R 9', 'R 10', 'R 11', 'D 1', 'D 2', 'D 3', 'D 4', 'D 5', 'D 6', 'D 7', 'D 8', 'D 9 F', 'D 10', 'D 12')
-and coalesce(TRIM("nextDestinationCompanySiret"), '') != '';
+and (coalesce(TRIM("nextDestinationCompanySiret"), '') != '' or coalesce(TRIM("nextDestinationCompanyVatNumber"), '') != '');
 
 -- ========================================================================
 -- Final operation code and noTraceability = true => noTraceability = false
