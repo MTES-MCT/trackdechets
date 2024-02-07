1c670c3653d74e496d4d428bd4e6f40504b576ed
GaelFerrand <gael.ferrand@gmail.com>
2024-02-07 13:45:05 +0100

    config: lowering the nbr of workers to 2

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: 0,
   /* Opt out of parallel tests on CI. */
-  workers: 4,
+  workers: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Timeout */
