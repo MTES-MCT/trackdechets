0c6ebb59312066e7956a3b0a744c4c84fe8e0731
elias@showk.me <elias@showk.me>
2024-02-09 17:33:37 +0100

    add operationHook job tests

--- back/src/queue/jobs/__tests__/operationHook.integration.ts
+++ back/src/queue/jobs/__tests__/operationHook.integration.ts
@@ -1,9 +1,159 @@
 import { resetDatabase } from "../../../../integration-tests/helper";
+import { operationHook } from "../operationHook";
+import { UserRole, CompanyType, Status, Company, User } from "@prisma/client";
+import { prisma } from "@td/prisma";
+import {
+  userWithCompanyFactory,
+  companyFactory,
+  formWithTempStorageFactory
+} from "../../../__tests__/factories";
 
-describe("Form OperationHook job", () => {
+describe("Test Form OperationHook job", () => {
+  let emitter: { user: User; company: Company };
+  let transporter: { user: User; company: Company };
+  let destination: { user: User; company: Company };
+
   afterEach(resetDatabase);
-  // beforeEach(() => {});
-  it("should ignore drafts", async () => {
-    return;
+  beforeAll(async () => {
+    emitter = await userWithCompanyFactory(UserRole.ADMIN, {
+      companyTypes: {
+        set: [CompanyType.PRODUCER]
+      }
+    });
+
+    transporter = await userWithCompanyFactory(UserRole.ADMIN, {
+      companyTypes: {
+        set: [CompanyType.TRANSPORTER]
+      }
+    });
+
+    destination = await userWithCompanyFactory(UserRole.ADMIN, {
+      companyTypes: {
+        set: [CompanyType.WASTEPROCESSOR]
+      }
+    });
+  });
+
+  it("updates final operations for final operation code", async () => {
+    const { user: ttrUser, company: ttr } = await userWithCompanyFactory(
+      UserRole.MEMBER,
+      {
+        companyTypes: { set: [CompanyType.COLLECTOR] }
+      }
+    );
+
+    const formWithTempStorage = await formWithTempStorageFactory({
+      ownerId: ttrUser.id,
+      opt: {
+        emitterCompanySiret: emitter.company.siret,
+        wasteDetailsCode: "05 01 02*",
+        status: Status.PROCESSED,
+        quantityReceived: 1000,
+        createdAt: new Date("2021-04-01"),
+        sentAt: new Date("2021-04-01"),
+        receivedAt: new Date("2021-04-01"),
+        processedAt: new Date("2021-04-01"),
+        processingOperationDone: "R 1",
+        transporters: {
+          create: {
+            transporterCompanySiret: transporter.company.siret,
+            number: 1
+          }
+        },
+        recipientCompanySiret: ttr.siret
+      },
+      forwardedInOpts: {
+        emitterCompanySiret: ttr.siret,
+        emitterCompanyName: ttr.name,
+        recipientCompanySiret: destination.company.siret,
+        quantityReceived: 100,
+        receivedAt: new Date(),
+        processingOperationDone: "R 1"
+      }
+    });
+
+    expect(formWithTempStorage.forwardedIn).toBeDefined();
+    // Manually execute operationHook to simulate markAsProcessed
+    await operationHook({
+      operationId: formWithTempStorage.forwardedIn!.id,
+      formId: formWithTempStorage.forwardedIn!.id
+    });
+    const updatedForm = await prisma.form.findUniqueOrThrow({
+      where: { id: formWithTempStorage.id },
+      include: { finalOperations: true, forwardedIn: true }
+    });
+
+    expect(updatedForm?.finalOperations[0]).toMatchObject({
+      formId: updatedForm.id,
+      finalBsdReadableId: updatedForm.forwardedIn!.readableId,
+      quantity: updatedForm.forwardedIn!.quantityReceived!,
+      operationCode: updatedForm.forwardedIn!.processingOperationDone!,
+      destinationCompanySiret: updatedForm.forwardedIn!.recipientCompanySiret!,
+      destinationCompanyName: updatedForm.forwardedIn!.recipientCompanyName!
+    });
+  });
+
+  it("updates final operations for noTraceability", async () => {
+    const { user: ttrUser, company: ttr } = await userWithCompanyFactory(
+      UserRole.MEMBER,
+      {
+        companyTypes: { set: [CompanyType.COLLECTOR] }
+      }
+    );
+    const destination = await companyFactory({
+      companyTypes: { set: [CompanyType.WASTEPROCESSOR] }
+    });
+
+    const formWithTempStorage = await formWithTempStorageFactory({
+      ownerId: ttrUser.id,
+      opt: {
+        emitterCompanySiret: emitter.company.siret,
+        wasteDetailsCode: "05 01 02*",
+        status: Status.PROCESSED,
+        quantityReceived: 1000,
+        createdAt: new Date("2021-04-01"),
+        sentAt: new Date("2021-04-01"),
+        receivedAt: new Date("2021-04-01"),
+        processedAt: new Date("2021-04-01"),
+        processingOperationDone: "R 1",
+        transporters: {
+          create: {
+            transporterCompanySiret: transporter.company.siret,
+            number: 1
+          }
+        },
+        recipientCompanySiret: ttr.siret
+      },
+      forwardedInOpts: {
+        emitterCompanySiret: ttr.siret,
+        emitterCompanyName: ttr.name,
+        recipientCompanySiret: destination.siret,
+        quantityReceived: 100,
+        receivedAt: new Date(),
+        processingOperationDone: "NOT_APPLICABLE",
+        noTraceability: true
+      }
+    });
+
+    expect(formWithTempStorage.forwardedIn).toBeDefined();
+    // Manually execute operationHook to simulate markAsProcessed
+    await operationHook({
+      operationId: formWithTempStorage.forwardedIn!.id,
+      formId: formWithTempStorage.forwardedIn!.id
+    });
+
+    const updatedForm = await prisma.form.findUniqueOrThrow({
+      where: { id: formWithTempStorage.id },
+      include: { finalOperations: true, forwardedIn: true }
+    });
+
+    expect(updatedForm?.finalOperations[0]).toMatchObject({
+      formId: updatedForm.id,
+      finalBsdReadableId: updatedForm.forwardedIn!.readableId,
+      quantity: updatedForm.forwardedIn!.quantityReceived!,
+      operationCode: updatedForm.forwardedIn!.processingOperationDone!,
+      destinationCompanySiret: updatedForm.forwardedIn!.recipientCompanySiret!,
+      destinationCompanyName: updatedForm.forwardedIn!.recipientCompanyName!
+    });
   });
 });
