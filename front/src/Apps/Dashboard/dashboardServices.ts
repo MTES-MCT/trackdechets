6ea2f8aba04fc1811e37496c72138b19a4ad445d
JulianaJM <juliana.jeanmarie972@gmail.com>
2024-02-26 15:38:57 +0100

    [tra-13411]Afficher le statut avec rupture de traçabilité lorsque mentionné lors du traitement

--- front/src/Apps/Dashboard/dashboardServices.ts
+++ front/src/Apps/Dashboard/dashboardServices.ts
@@ -97,12 +97,6 @@ export const getBsdStatusLabel = (
     case BsdStatusCode.Accepted:
       return ACCEPTE;
     case BsdStatusCode.Processed:
-      if (
-        bsdType === BsdType.Bsff &&
-        (operationCode === "R12" || operationCode === "D13")
-      ) {
-        return TRAITE_AVEC_RUPTURE_TRACABILITE;
-      }
       return TRAITE;
     case BsdStatusCode.AwaitingChild:
     case BsdStatusCode.Grouped:
