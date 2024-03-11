b86c0837605cadc8ecef1a031777764d1fdbc25d
GaelFerrand <gael.ferrand@gmail.com>
2024-03-11 14:08:59 +0100

    fix: increased timeout for CI

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Timeout */
-  timeout: 20 * 1000,
+  timeout: 90 * 1000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
