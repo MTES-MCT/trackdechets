6c3272fb5b4851f014dcda35d898aceaba8ca5d3
Riron <orioncharlier@gmail.com>
2024-03-12 17:52:31 +0100

    Edit migration 178 (#3180)

--- libs/back/prisma/src/migrate/migrations/178_forms_sort.sql
+++ libs/back/prisma/src/migrate/migrations/178_forms_sort.sql
@@ -2,15 +2,17 @@
 ALTER TABLE "default$default"."Form"
 ADD COLUMN IF NOT EXISTS "rowNumber" SERIAL;
 
+-- Next steps were too long => They were done manually before the release
+
 -- Step 2: Update the new column with auto-incremented values based on the createdAt column
-UPDATE "default$default"."Form"
-SET "rowNumber" = subquery."row_number"
-FROM (
-  SELECT id, ROW_NUMBER() OVER (ORDER BY "createdAt") AS row_number
-  FROM "default$default"."Form"
-) AS subquery
-WHERE "default$default"."Form".id = subquery.id;
+-- UPDATE "default$default"."Form"
+-- SET "rowNumber" = subquery."row_number"
+-- FROM (
+--   SELECT id, ROW_NUMBER() OVER (ORDER BY "createdAt") AS row_number
+--   FROM "default$default"."Form"
+-- ) AS subquery
+-- WHERE "default$default"."Form".id = subquery.id;
 
 -- Step 3: Add a unique constraint on the new column
-ALTER TABLE "default$default"."Form"
-ADD CONSTRAINT "Form_rowNumber_ukey" UNIQUE ("rowNumber");
+-- ALTER TABLE "default$default"."Form"
+-- ADD CONSTRAINT "Form_rowNumber_ukey" UNIQUE ("rowNumber");
