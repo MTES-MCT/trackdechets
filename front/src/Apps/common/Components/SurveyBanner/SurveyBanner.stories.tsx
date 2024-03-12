290c1443bd0789d55837509b07fc99a74b13561e
Julien Seren-Rosso <julien.serenrosso@beta.gouv.fr>
2024-03-12 10:36:23 +0100

    Upgrades storybook

--- front/src/Apps/common/Components/SurveyBanner/SurveyBanner.stories.tsx
+++ front/src/Apps/common/Components/SurveyBanner/SurveyBanner.stories.tsx
@@ -1,21 +1,15 @@
-import { Meta, StoryObj } from "@storybook/react";
+import { Meta } from "@storybook/react";
 import SurveyBanner from "./SurveyBanner";
 
-const meta: Meta<typeof SurveyBanner> = { component: SurveyBanner };
-export default meta;
-
-type Story = StoryObj<typeof SurveyBanner>;
-
-export const Primary: Story = {
+const meta: Meta<typeof SurveyBanner> = {
+  component: SurveyBanner,
   args: {
     message:
       "Afin de mesurer votre ressenti vis à vis de l'application Trackdéchets, nous vous invitons à répondre à ce questionnaire.",
     button: {
       title: "Répondre au questionnaire",
       href: "https://trackdechets.beta.gouv.fr"
-    },
-    onClickClose: () => {
-      console.log("Fermer la fenêtre");
     }
   }
 };
+export default meta;
