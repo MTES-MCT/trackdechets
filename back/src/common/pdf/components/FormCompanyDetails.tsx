a021f6431f6a5a7b665aea7314c2c75459fe99a0
GaelFerrand <45355989+GaelFerrand@users.noreply.github.com>
2024-03-20 16:41:05 +0100

    [TRA-13705] Cadre 1.1 du PDF d'un BSDD: revoir l'ordre des options, renommer Entreprise française et supprimer Entreprise étrangère  (#3181)
    * feat: fixed PDF with new label
    
    * fix: lint issues

---
+++ back/src/common/pdf/components/FormCompanyDetails.tsx
@@ -0,0 +1,78 @@
+import * as React from "react";
+import { FormCompany } from "../../../generated/graphql/types";
+import {
+  getReadableCompanyCountry,
+  isEUCompany,
+  isFrenchCompany
+} from "../../../companies/validation";
+
+type FormCompanyDetailsProps = {
+  company?: FormCompany | null;
+  isForeignShip?: boolean;
+  isPrivateIndividual?: boolean;
+};
+
+/**
+ * Common PDF Company formatting
+ * @param company FormCompany
+ * @param isForeignShip boolean Emitter is a Foreign Ship
+ * @param isPrivateIndividual boolean Emitter is a private person not a company
+ */
+export function FormCompanyDetails({
+  company,
+  isForeignShip,
+  isPrivateIndividual
+}: FormCompanyDetailsProps) {
+  return (
+    <>
+      <p>
+        {isFrenchCompany({ company, isForeignShip, isPrivateIndividual }) && (
+          <div>
+            N° SIRET : {company?.siret}
+            <br />
+          </div>
+        )}
+        {isEUCompany({ company, isForeignShip, isPrivateIndividual }) && (
+          <div>
+            N° TVA intracommunautaire (le cas échéant) : {company?.vatNumber}
+            <br />
+          </div>
+        )}
+        {company?.extraEuropeanId && (
+          <div>
+            Identifiant d'entreprise extra-européenne :{" "}
+            {company?.extraEuropeanId}
+            <br />
+          </div>
+        )}
+        {company?.omiNumber && (
+          <div>
+            Numéro navire OMI : {company?.omiNumber}
+            <br />
+          </div>
+        )}
+        {isPrivateIndividual ||
+        (!company?.siret && !company?.vatNumber && !company?.extraEuropeanId)
+          ? "Nom Prénom"
+          : "RAISON SOCIALE"}{" "}
+        : {company?.name}
+        <br />
+        Adresse complète : {company?.address}
+        <br />
+        Pays (le cas échéant) : {getReadableCompanyCountry(company)}
+      </p>
+      <p>
+        Tel : {company?.phone}
+        <br />
+        {`Mail : `}
+        {company?.mail}
+        {!isPrivateIndividual && (
+          <>
+            <br />
+            Personne à contacter : {company?.contact}
+          </>
+        )}
+      </p>
+    </>
+  );
+}
--- back/src/companies/validation.ts
+++ back/src/companies/validation.ts
@@ -1,6 +1,15 @@
 import * as yup from "yup";
 import { Company, CompanyType } from "@prisma/client";
-import { isForeignVat } from "@td/constants";
+import {
+  cleanClue,
+  isForeignVat,
+  isSiret,
+  isVat,
+  countries as vatCountries
+} from "@td/constants";
+import { checkVAT } from "jsvat";
+import { FormCompany } from "../generated/graphql/types";
+import countries, { Country } from "world-countries";
 
 export const receiptSchema = yup.object().shape({
   validityLimit: yup.date().required()
@@ -46,3 +55,75 @@ export function isWorker(company: Company) {
 export function isCrematorium(company: Company) {
   return company.companyTypes.includes(CompanyType.CREMATORIUM);
 }
+
+const FRENCH_COUNTRY = countries.find(country => country.cca2 === "FR");
+export const getcompanyCountry = (
+  company: FormCompany | null | undefined
+): Country | undefined => {
+  if (!company) return FRENCH_COUNTRY; // default
+
+  // forcer FR si le siret est valide
+  if (company.siret && isSiret(company.siret)) {
+    return countries.find(country => country.cca2 === "FR");
+  } else if (company.vatNumber && isVat(company.vatNumber)) {
+    // trouver automatiquement le pays selon le numéro de TVA
+    const vatCountryCode = checkVAT(cleanClue(company.vatNumber), vatCountries)
+      ?.country?.isoCode.short;
+
+    return countries.find(country => country.cca2 === vatCountryCode);
+  }
+
+  // reconnaitre le pays directement dans le champ country
+  return countries.find(country => country.cca2 === company.country);
+};
+
+export const getReadableCompanyCountry = (company?: FormCompany | null) => {
+  const companyCountry = getcompanyCountry(company);
+
+  if (!companyCountry || companyCountry?.cca2 === "FR") {
+    return "France";
+  }
+
+  return companyCountry?.name.common;
+};
+
+interface IsCompanyProps {
+  company?: FormCompany | null;
+  isForeignShip?: boolean;
+  isPrivateIndividual?: boolean;
+}
+
+export const isFrenchCompany = ({
+  company,
+  isForeignShip,
+  isPrivateIndividual
+}: IsCompanyProps) => {
+  const companyCountry = getcompanyCountry(company);
+
+  return (
+    isSiret(company?.siret) &&
+    !isForeignShip &&
+    !isPrivateIndividual &&
+    companyCountry?.cca2 === "FR"
+  );
+};
+
+export const isEUCompany = ({
+  company,
+  isForeignShip,
+  isPrivateIndividual
+}: IsCompanyProps) => {
+  return isVat(company?.vatNumber) && !isForeignShip && !isPrivateIndividual;
+};
+
+export const isForeignCompany = ({
+  company,
+  isForeignShip
+}: Omit<IsCompanyProps, "isPrivateIndividual">) => {
+  const companyCountry = getcompanyCountry(company);
+
+  return (
+    Boolean(company?.extraEuropeanId || company?.vatNumber || isForeignShip) &&
+    companyCountry?.cca2 !== "FR"
+  );
+};
