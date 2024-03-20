9de7198611c1e8d9dd60932d837df24926357889
Orion C <orioncharlier@gmail.com>
2024-03-20 15:05:14 +0100

    send to mattermost

--- scripts/gh-pr.js
+++ scripts/gh-pr.js
@@ -4,15 +4,13 @@ module.exports = async ({ github, context, core }) => {
     repo: context.repo.repo,
     state: "open"
   });
-  console.log(prs.data)
+
   const prsReadyToReview = prs.data.filter(pr =>
     pr.labels.some(label => label.name === "ready for review")
   );
 
-  console.log(prsReadyToReview)
-  
   const output = prsReadyToReview
-  .map(pr => `- ${pr.title} (${pr.html_url})`)
+  .map(pr => `- ${pr.title} | [PR ${pr.number}](${pr.html_url})`)
   .join("\n");
   console.log(output)
 
