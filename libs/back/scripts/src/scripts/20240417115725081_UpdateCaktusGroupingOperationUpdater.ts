4e5d1fc643833d8ec4f9ac3b15f8084754faa606
elias@showk.me <elias@showk.me>
2024-04-18 12:01:05 +0200

    make script idempotent

--- libs/back/scripts/src/scripts/20240417115725081_UpdateCaktusGroupingOperationUpdater.ts
+++ libs/back/scripts/src/scripts/20240417115725081_UpdateCaktusGroupingOperationUpdater.ts
@@ -1,6 +1,5 @@
 import fs from "fs";
 import path from "path";
-import { logger } from "@td/logger";
 import * as ExcelJS from "exceljs";
 import {
   getFormRepository,
@@ -19,20 +18,26 @@ async function loadExcelData(filePath: string) {
   try {
     await workbook.xlsx.readFile(filePath);
   } catch (error) {
-    logger.error(
+    console.error(
       `Erreur lors de la lecture du fichier Excel : ${error.message}`
     );
     process.exit(1);
   }
   const worksheet = workbook.worksheets[0];
   const rows: Array<Row> = [];
+  let count = 0;
   worksheet.eachRow((row, rowNumber) => {
     // Ignorer l'en-tête
     if (rowNumber > 1 && row?.cellCount) {
       const nextFormId = row.getCell(1) as unknown as string;
+      if (!nextFormId) {
+        console.info(`Parsed ${count} lines of XLSX`);
+        return rows;
+      }
       const cell2 = row.getCell(2) as unknown as string;
       const initialFormIds = cell2.split ? cell2.split(",") : [];
       rows.push({ nextFormId, initialFormIds });
+      count++;
     }
   });
   return rows;
@@ -46,7 +51,7 @@ export async function run() {
   );
   const user = { id: "support-td", authType: "script" };
   if (!fs.existsSync(pathXlsx)) {
-    logger.info(`Missing file ${pathXlsx}, aborting script`);
+    console.info(`Missing file ${pathXlsx}, aborting script`);
   } else {
     const rows = await loadExcelData(pathXlsx);
 
@@ -55,6 +60,11 @@ export async function run() {
         const { updateAppendix2Forms } = getFormRepository(user as any, prisma);
 
         const nextForm = await getFormOrFormNotFound({ id: row.nextFormId });
+
+        const existingFormFractions = await prisma.form
+          .findUnique({ where: { id: nextForm.id } })
+          .grouping({ include: { initialForm: true } });
+
         const initialForms = await prisma.form.findMany({
           where: { id: { in: row.initialFormIds } }
         });
@@ -67,27 +77,82 @@ export async function run() {
 
         const formFractions = await validateGroupement(nextForm, grouping);
 
+        const existingAppendixForms =
+          existingFormFractions?.map(({ initialForm }) => initialForm) ?? [];
+
         const formGroupementToCreate: {
           nextFormId: string;
           initialFormId: string;
           quantity: number;
         }[] = [];
 
+        const formGroupementToUpdate: {
+          initialFormId: string;
+          quantity: number;
+        }[] = [];
+
         for (const { form: initialForm, quantity } of formFractions) {
-          formGroupementToCreate.push({
-            nextFormId: nextForm.id,
-            initialFormId: initialForm.id,
-            quantity: quantity
-          });
+          if (
+            existingAppendixForms &&
+            existingAppendixForms.map(f => f.id).includes(initialForm.id)
+          ) {
+            formGroupementToUpdate.push({
+              initialFormId: initialForm.id,
+              quantity
+            });
+          } else {
+            formGroupementToCreate.push({
+              nextFormId: nextForm.id,
+              initialFormId: initialForm.id,
+              quantity
+            });
+          }
         }
+
+        console.log(`formGroupementToCreate = ${formGroupementToCreate.length}`);
 
         if (formGroupementToCreate.length > 0) {
           await prisma.formGroupement.createMany({
             data: formGroupementToCreate
           });
+        }
 
-          const dirtyFormIds = initialForms.map(f => f.id);
+        if (formGroupementToUpdate.length > 0) {
+          // We compare existing groupements with the updates. If the quantity hasn't changed, we skip the update
+          const existingGroupements = await prisma.formGroupement.findMany({
+            where: {
+              nextFormId: nextForm.id
+            }
+          });
+          const validUpdates = formGroupementToUpdate.filter(update => {
+            const existingGroupement = existingGroupements.find(
+              grp => grp.initialFormId === update.initialFormId
+            );
+            return existingGroupement?.quantity !== update.quantity;
+          });
 
+          console.log(`formGroupementToUpdate = ${validUpdates.length}`);
+
+          await Promise.all(
+            validUpdates.map(({ initialFormId, quantity }) =>
+              prisma.formGroupement.updateMany({
+                where: {
+                  nextFormId: nextForm.id,
+                  initialFormId
+                },
+                data: { quantity }
+              })
+            )
+          );
+        }
+
+        if (formGroupementToCreate.length) {
+          const dirtyFormIds = [
+            ...new Set([
+              ...(existingAppendixForms?.map(f => f.id) ?? []),
+              ...initialForms.map((form) => form.id)
+            ])
+          ];
           const dirtyForms = await prisma.form.findMany({
             where: { id: { in: dirtyFormIds } },
             include: { forwardedIn: true }
@@ -98,6 +163,6 @@ export async function run() {
       });
     }
 
-    console.log("Insertion et mise à jour terminées.");
+    console.info("Insertion et mise à jour terminées.");
   }
 }
