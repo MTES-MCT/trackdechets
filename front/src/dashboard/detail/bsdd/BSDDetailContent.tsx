9bea2050157c553c73f30d11a6b727bc1b8bbf8b
silto <silte00@gmail.com>
2024-05-01 00:17:55 +0200

    fix(BSDDetailContent): fix status always appearing as "Awaiting transporter signature" for private individuals + replace some magic strings with corresponding enums

--- front/src/dashboard/detail/bsdd/BSDDetailContent.tsx
+++ front/src/dashboard/detail/bsdd/BSDDetailContent.tsx
@@ -590,9 +590,10 @@ const Appendix1 = ({
                 </td>
                 <td>
                   {form.status
-                    ? !Boolean(form.emitter?.isPrivateIndividual)
-                      ? STATUS_LABELS[form.status]
-                      : STATUS_LABELS["SEALED_PRIVATE_INDIVIDUAL"]
+                    ? form.emitter?.isPrivateIndividual &&
+                      form.status === FormStatus.Sealed
+                      ? STATUS_LABELS["SEALED_PRIVATE_INDIVIDUAL"]
+                      : STATUS_LABELS[form.status]
                     : "-"}
                 </td>
                 <td>
@@ -689,13 +690,13 @@ export default function BSDDetailContent({
             <IconBSDD className="tw-mr-2" />
 
             <span className={styles.detailStatus}>
-              {!isAppendix1Producer
-                ? [STATUS_LABELS[form.status]]
-                : Boolean(form.emitter?.isPrivateIndividual)
-                ? [STATUS_LABELS["SEALED_PRIVATE_INDIVIDUAL"]]
-                : [STATUS_LABELS[form.status]]}
+              {isAppendix1Producer &&
+              form.emitter?.isPrivateIndividual &&
+              form.status === FormStatus.Sealed
+                ? STATUS_LABELS["SEALED_PRIVATE_INDIVIDUAL"]
+                : STATUS_LABELS[form.status]}
             </span>
-            {form.status !== "DRAFT" && <span>{form.readableId}</span>}
+            {form.status !== FormStatus.Draft && <span>{form.readableId}</span>}
 
             {!!form.customId && (
               <span className="tw-ml-auto">Numéro libre: {form.customId}</span>
@@ -704,7 +705,7 @@ export default function BSDDetailContent({
 
           <div className={styles.detailContent}>
             <div className={`${styles.detailQRCodeIcon}`}>
-              {form.status !== "DRAFT" && (
+              {form.status !== FormStatus.Draft && (
                 <div className={styles.detailQRCode}>
                   <QRCodeIcon value={form.readableId} size={96} />
                   <span>Ce QR code contient le numéro du bordereau </span>
