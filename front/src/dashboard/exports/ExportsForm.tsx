1c593c603915386eaf07dfb25b6bafada57a81ea
GaelFerrand <45355989+GaelFerrand@users.noreply.github.com>
2024-05-16 17:47:57 +0200

    [TRA-10101] Retirer la possibilité de filtrer le registre par code déchet via le sélecteur  (#3312)
    * fix: removed waste code option in registry
    
    * fix: removed code alltogether
    
    * fix: removed useless import

--- front/src/dashboard/exports/ExportsForm.tsx
+++ front/src/dashboard/exports/ExportsForm.tsx
@@ -10,7 +10,7 @@ import {
 } from "@td/codegen-ui";
 import WasteTreeModal from "../../search/WasteTreeModal";
 
-import { ALL_WASTES, ALL_WASTES_TREE } from "@td/constants";
+import { ALL_WASTES_TREE } from "@td/constants";
 import { useLazyQuery, gql } from "@apollo/client";
 import { NotificationError } from "../../Apps/common/Components/Error/Error";
 import RedErrorMessage from "../../common/components/RedErrorMessage";
@@ -228,10 +228,6 @@ export default function ExportsForm({ companies }: IProps) {
       validate={validate}
     >
       {({ values, setFieldValue }) => {
-        const wasteCodeDetail = ALL_WASTES.find(
-          waste => waste.code === values.wasteCode
-        );
-
         const exportTypes = getPossibleExportTypes(values.companies);
         if (!exportTypes.includes(values.exportType)) {
           setFieldValue("exportType", exportTypes[0]);
@@ -348,26 +344,6 @@ export default function ExportsForm({ companies }: IProps) {
                   </select>
                 )}
               </Field>
-              <label className="tw-col-span-1 tw-text-right tw-flex tw-items-start tw-justify-end tw-font-bold">
-                Code déchet (optionnel)
-              </label>
-              <div className="tw-col-span-2 tw-max-w-md">
-                <div className="tw-container tw-flex tw-flex-row">
-                  <Field
-                    name="wasteCode"
-                    className={`${styles["max-w-xxs"]} tw-mr-4 td-input`}
-                  />
-                  <button
-                    type="button"
-                    className={`btn btn--outline-primary btn--small-text ${styles["max-w-xxs"]} tw-mr-4`}
-                    onClick={() => setOpenWasteTreeModal(true)}
-                  >
-                    Liste des codes déchets
-                  </button>
-                </div>
-                {wasteCodeDetail && <div>{wasteCodeDetail.description}</div>}
-                <RedErrorMessage name="wasteCode"></RedErrorMessage>
-              </div>
 
               <WasteTreeModal
                 wasteTree={ALL_WASTES_TREE}
