5b4284f0746de9482c177e51f6fe77777d6329fb
Orion C <orioncharlier@gmail.com>
2024-06-21 17:22:36 +0200

    Use randomUUID()

--- libs/back/logger/src/correlationId.ts
+++ libs/back/logger/src/correlationId.ts
@@ -1,12 +1,11 @@
 import { AsyncLocalStorage } from "node:async_hooks";
-import { randomBytes } from "node:crypto";
+import { randomUUID } from "node:crypto";
 
 export type CorrelationId = string;
 const correlationIdStorage = new AsyncLocalStorage<CorrelationId>();
 
 export function runWithCorrelationId<R>(callback: () => R): R {
-  const uuid = randomBytes(16).toString("hex");
-  return correlationIdStorage.run(uuid, callback);
+  return correlationIdStorage.run(randomUUID(), callback);
 }
 
 export function getCorrelationId(): CorrelationId | undefined {
