eb40667d7d8c3bb4758ef363ee20474223791726
Julien Seren-Rosso <julien.serenrosso@beta.gouv.fr>
2024-06-25 10:09:56 +0200

    Adds reception and acceptation V2 modals

--- front/src/dashboard/components/BSDList/BSDD/WorkflowAction/FormWasteSummary.tsx
+++ front/src/dashboard/components/BSDList/BSDD/WorkflowAction/FormWasteSummary.tsx
@@ -1,10 +1,14 @@
 import * as React from "react";
-import { Form, QuantityType } from "@td/codegen-ui";
+import { Consistence, Form, Maybe, QuantityType } from "@td/codegen-ui";
 import {
   DataList,
   DataListDescription,
   DataListItem,
-  DataListTerm
+  DataListTerm,
+  DsfrDataList,
+  DsfrDataListItem,
+  DsfrDataListTerm,
+  DsfrDataListDescription
 } from "../../../../../common/components";
 
 interface FormWasteSummaryProps {
@@ -50,5 +54,70 @@ export function FormWasteSummary({ form }: FormWasteSummaryProps) {
         </DataListDescription>
       </DataListItem>
     </DataList>
+  );
+}
+
+const getConsistenceLabel = (consistence: Maybe<Consistence> | undefined) => {
+  switch (consistence) {
+    case Consistence.Liquid:
+      return "Liquide";
+    case Consistence.Solid:
+      return "Solide";
+    case Consistence.Doughy:
+      return "Pâteux";
+    case Consistence.Gaseous:
+      return "Gaseux";
+
+    default:
+      return "Non soumis";
+  }
+};
+
+export function DsfrFormWasteSummary({ form }: FormWasteSummaryProps) {
+  return (
+    <DsfrDataList>
+      <DsfrDataListItem>
+        <DsfrDataListTerm>BSDD n°</DsfrDataListTerm>
+        <DsfrDataListDescription>{form.readableId}</DsfrDataListDescription>
+      </DsfrDataListItem>
+      <DsfrDataListItem>
+        <DsfrDataListTerm>Code déchet</DsfrDataListTerm>
+        <DsfrDataListDescription>
+          {form.wasteDetails?.code}
+        </DsfrDataListDescription>
+      </DsfrDataListItem>
+      <DsfrDataListItem>
+        <DsfrDataListTerm>Nom usuel</DsfrDataListTerm>
+        <DsfrDataListDescription>
+          {form.wasteDetails?.name}
+        </DsfrDataListDescription>
+      </DsfrDataListItem>
+      <DsfrDataListItem>
+        <DsfrDataListTerm>Consistance</DsfrDataListTerm>
+        <DsfrDataListDescription>
+          {getConsistenceLabel(form.wasteDetails?.consistence)}
+        </DsfrDataListDescription>
+      </DsfrDataListItem>
+      <DsfrDataListItem>
+        <DsfrDataListTerm>Conditionnement</DsfrDataListTerm>
+        <DsfrDataListDescription>
+          {form.wasteDetails?.packagingInfos
+            ?.map(packaging => `${packaging.quantity} ${packaging.type}`)
+            .join(", ")}
+        </DsfrDataListDescription>
+      </DsfrDataListItem>
+      <DsfrDataListItem>
+        <DsfrDataListTerm>Poids estimé / réel</DsfrDataListTerm>
+        <DsfrDataListDescription>
+          {form.quantityReceived
+            ? `${form.quantityReceived}t`
+            : `${form.wasteDetails?.quantity}t${
+                form.wasteDetails?.quantityType === QuantityType.Estimated
+                  ? " (estimé)"
+                  : ""
+              }`}
+        </DsfrDataListDescription>
+      </DsfrDataListItem>
+    </DsfrDataList>
   );
 }
---
+++ front/src/dashboard/components/BSDList/BSDD/WorkflowAction/SignFormModal.tsx
@@ -0,0 +1,51 @@
+import { useQuery } from "@apollo/client";
+
+import { Loader } from "../../../../../Apps/common/Components";
+import {
+  DsfrModal,
+  ModalSizes
+} from "../../../../../Apps/common/Components/Modal/DsfrModal";
+import { GET_FORM } from "../../../../../form/bsdd/utils/queries";
+import { Query, QueryFormArgs } from "@td/codegen-ui";
+import React from "react";
+import { FormSummary } from "./FormSummary";
+
+type Props = {
+  title: string;
+  formId: string;
+  children: (props: { form; onClose }) => React.ReactNode;
+  onClose: () => void;
+  size: string;
+};
+
+export function SignFormModal({
+  title,
+  formId,
+  children,
+  onClose,
+  size
+}: Readonly<Props>) {
+  const { data } = useQuery<Pick<Query, "form">, QueryFormArgs>(GET_FORM, {
+    variables: {
+      id: formId
+    }
+  });
+
+  if (data == null) {
+    return <Loader />;
+  }
+
+  const { form } = data;
+
+  return (
+    <DsfrModal
+      title={title}
+      onClose={onClose}
+      size={size as ModalSizes}
+      padding={true}
+    >
+      <FormSummary form={form} />
+      {children({ form, onClose })}
+    </DsfrModal>
+  );
+}
