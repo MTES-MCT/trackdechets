96ac993c6fdbfe0bfba861272b9c6c614165e9b9
Orion C <orioncharlier@gmail.com>
2024-06-25 17:07:55 +0200

    Use originalUrl

--- back/src/common/middlewares/loggingMiddleware.ts
+++ back/src/common/middlewares/loggingMiddleware.ts
@@ -1,6 +1,5 @@
 import { logger } from "@td/logger";
 import { AsyncResource } from "node:async_hooks";
-import { join } from "node:path";
 import { Request, Response, NextFunction } from "express";
 
 /**
@@ -77,27 +76,20 @@ function logExpressRequest(
     graphQLPath?: string;
   }
 ) {
-  const path =
-    request.path !== "/"
-      ? join(request.baseUrl, request.path)
-      : request.baseUrl; // In case the gql endpoint is mounted into a subpath
+  const path = request.originalUrl.split("?")[0];
   const message = `${request.method} ${path}`;
 
   let requestMetadata: Record<string, any> = {
     ip: request.ip,
     http_params: request.params,
     http_query: request.query,
-    http_path: join(request.baseUrl, path),
+    http_path: path,
     http_method: request.method,
     request_timing: requestTiming
   };
 
   // GraphQL specific fields
-  if (
-    graphQLPath &&
-    path.startsWith(graphQLPath) &&
-    request.method === "POST"
-  ) {
+  if (graphQLPath && path === graphQLPath && request.method === "POST") {
     requestMetadata.graphql_operation_name = request.body?.operationName;
     requestMetadata.graphql_variables = request.body?.variables;
     requestMetadata.graphql_query = request.body?.query;
