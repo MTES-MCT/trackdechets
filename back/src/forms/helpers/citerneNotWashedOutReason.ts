37f4d2c3f36c04ddf3570f88445c5757bfb39743
GaelFerrand <gael.ferrand@gmail.com>
2024-08-29 15:00:04 +0200

    feat: adding citerne data to the PDF

---
+++ back/src/forms/helpers/citerneNotWashedOutReason.ts
@@ -0,0 +1,18 @@
+import { CiterneNotWashedOutReason } from "@prisma/client";
+
+const LABELS = {
+  EXEMPTED: "Exemptions de rinçage (citerne dédiée)",
+  INCOMPATIBLE: "Incompatibilité avec l'opération de rinçage à l'eau",
+  UNAVAILABLE: "Indisponibilité de l'installation de rinçage",
+  NOT_BY_DRIVER: "Rinçage non réalisé par le chauffeur"
+};
+
+export const getReasonLabel = (
+  reason: CiterneNotWashedOutReason | null | undefined
+) => {
+  if (!reason) {
+    return null;
+  }
+
+  return LABELS[reason];
+};
