7bbbd553396e75657db8f42b0ba9f803d629d7d9
silto <silte00@gmail.com>
2024-08-29 16:15:21 +0200

    feat(BSVHU): add split paths for BSVHU valdiation rules feat(BSPAOH): add split paths for BSPAOH validation rules

--- back/src/types.ts
+++ back/src/types.ts
@@ -25,6 +25,57 @@ export type Nullable<T> = {
   [P in keyof T]: T[P] | null;
 };
 
+type Cons<H, T> = T extends readonly any[]
+  ? ((h: H, ...t: T) => void) extends (...r: infer R) => void
+    ? R
+    : never
+  : never;
+type Prev = [
+  never,
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  ...0[]
+];
+
+export type Paths<T, D extends number = 3> = [D] extends [never]
+  ? never
+  : T extends object
+  ? {
+      [K in keyof T]-?:
+        | [K]
+        | (Paths<T[K], Prev[D]> extends infer P
+            ? P extends []
+              ? never
+              : Cons<K, P>
+            : never);
+    }[keyof T]
+  : [];
+
+export type Leaves<T, D extends number = 3> = [D] extends [never]
+  ? never
+  : T extends object
+  ? { [K in keyof T]-?: Cons<K, Leaves<T[K], Prev[D]>> }[keyof T]
+  : [];
+
 declare module "express-session" {
   interface SessionData {
     warningMessage?: string;
