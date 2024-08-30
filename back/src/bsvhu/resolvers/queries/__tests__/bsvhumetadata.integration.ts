c369e4e05b98c6cecf67542babf3550d8d323c92
silto <silte00@gmail.com>
2024-08-31 00:46:04 +0200

    test(BSPAOH,BSVHU): update bspaoh metadata tests, add bsvhu metadata tests, fix compilation issue in frontend

---
+++ back/src/bsvhu/resolvers/queries/__tests__/bsvhumetadata.integration.ts
@@ -0,0 +1,155 @@
+import { UserRole, BsvhuStatus } from "@prisma/client";
+import { gql } from "graphql-tag";
+import { resetDatabase } from "../../../../../integration-tests/helper";
+import { Query } from "../../../../generated/graphql/types";
+import {
+  companyFactory,
+  userWithCompanyFactory
+} from "../../../../__tests__/factories";
+import makeClient from "../../../../__tests__/testClient";
+
+import { bsvhuFactory } from "../../../__tests__/factories.vhu";
+
+const GET_BSVHU = gql`
+  query GetBsvhu($id: ID!) {
+    bsvhu(id: $id) {
+      id
+      status
+      metadata {
+        fields {
+          sealed
+          requiredForNextSignature
+        }
+      }
+    }
+  }
+`;
+
+describe("Query.Bsvhu", () => {
+  afterEach(resetDatabase);
+
+  it("should return INITIAL bsvhu sealed fields", async () => {
+    const { user, company: emitterCompany } = await userWithCompanyFactory(
+      UserRole.ADMIN
+    );
+    const bsd = await bsvhuFactory({
+      opt: {
+        status: BsvhuStatus.INITIAL,
+        emitterCompanySiret: emitterCompany.siret
+      }
+    });
+
+    const { query } = makeClient(user);
+
+    const { data } = await query<Pick<Query, "bsvhu">>(GET_BSVHU, {
+      variables: { id: bsd.id }
+    });
+
+    expect(data.bsvhu.metadata?.fields?.sealed?.length).toBe(0);
+    expect(data.bsvhu.metadata?.fields?.requiredForNextSignature?.length).toBe(
+      20
+    );
+  });
+
+  it("should return EMISSION signed bsvhu sealed fields", async () => {
+    const { user, company: emitterCompany } = await userWithCompanyFactory(
+      UserRole.ADMIN
+    );
+    const bsd = await bsvhuFactory({
+      opt: {
+        status: BsvhuStatus.SIGNED_BY_PRODUCER,
+        emitterCompanySiret: emitterCompany.siret,
+        emitterEmissionSignatureDate: new Date()
+      }
+    });
+
+    const { query } = makeClient(user);
+
+    const { data } = await query<Pick<Query, "bsvhu">>(GET_BSVHU, {
+      variables: { id: bsd.id }
+    });
+
+    expect(data.bsvhu.metadata?.fields?.sealed?.length).toBe(0);
+    expect(data.bsvhu.metadata?.fields?.requiredForNextSignature?.length).toBe(
+      29
+    );
+  });
+
+  it("should return EMISSION signed bsvhu sealed fields with emitter fields sealed", async () => {
+    const emitterCompany = await companyFactory();
+    const { user, company: transporterCompany } = await userWithCompanyFactory(
+      UserRole.ADMIN
+    );
+    const bsd = await bsvhuFactory({
+      opt: {
+        status: BsvhuStatus.SIGNED_BY_PRODUCER,
+        emitterCompanySiret: emitterCompany.siret,
+        emitterEmissionSignatureDate: new Date(),
+        transporterCompanySiret: transporterCompany.siret
+      }
+    });
+
+    const { query } = makeClient(user);
+
+    const { data } = await query<Pick<Query, "bsvhu">>(GET_BSVHU, {
+      variables: { id: bsd.id }
+    });
+
+    expect(data.bsvhu.metadata?.fields?.sealed?.length).toBe(21);
+    expect(data.bsvhu.metadata?.fields?.requiredForNextSignature?.length).toBe(
+      29
+    );
+  });
+
+  it("should return TRANSPORTER signed bsvhu sealed fields", async () => {
+    const { user, company: transporterCompany } = await userWithCompanyFactory(
+      UserRole.ADMIN
+    );
+    const emitterCompany = await companyFactory();
+    const bsd = await bsvhuFactory({
+      opt: {
+        status: BsvhuStatus.SENT,
+        emitterCompanySiret: emitterCompany.siret,
+        emitterEmissionSignatureDate: new Date(),
+        transporterCompanySiret: transporterCompany.siret,
+        transporterTransportSignatureDate: new Date()
+      }
+    });
+
+    const { query } = makeClient(user);
+
+    const { data } = await query<Pick<Query, "bsvhu">>(GET_BSVHU, {
+      variables: { id: bsd.id }
+    });
+
+    expect(data.bsvhu.metadata?.fields?.sealed?.length).toBe(33);
+    expect(data.bsvhu.metadata?.fields?.requiredForNextSignature?.length).toBe(
+      33
+    );
+  });
+
+  it("should return OPERATION signed bsvhu sealed fields", async () => {
+    const { user, company } = await userWithCompanyFactory(UserRole.ADMIN);
+
+    const bsd = await bsvhuFactory({
+      opt: {
+        status: BsvhuStatus.PROCESSED,
+        emitterCompanySiret: company.siret,
+        emitterEmissionSignatureDate: new Date(),
+        destinationOperationSignatureDate: new Date(),
+        transporterTransportSignatureDate: new Date()
+      }
+    });
+
+    const { query } = makeClient(user);
+
+    const { data } = await query<Pick<Query, "bsvhu">>(GET_BSVHU, {
+      variables: { id: bsd.id }
+    });
+
+    expect(data.bsvhu.metadata?.fields?.sealed?.length).toBe(57);
+    expect(data.bsvhu.metadata?.fields?.requiredForNextSignature?.length).toBe(
+      33
+    );
+  });
+});
