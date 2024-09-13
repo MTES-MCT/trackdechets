0f02a65e607717c2a4c572a44c786ddf61e0a8be
GaelFerrand <gael.ferrand@gmail.com>
2024-09-13 16:26:49 +0200

    test: added test for revocation

--- back/src/rndtsDeclarationDelegation/resolvers/mutations/__tests__/revokeRndtsDeclarationDelegation.integration.ts
+++ back/src/rndtsDeclarationDelegation/resolvers/mutations/__tests__/revokeRndtsDeclarationDelegation.integration.ts
@@ -5,6 +5,7 @@ import makeClient from "../../../../__tests__/testClient";
 import { Mutation } from "../../../../generated/graphql/types";
 import { prisma } from "@td/prisma";
 import { rndtsDeclarationDelegationFactory } from "../../../__tests__/factories";
+import { userFactory, userInCompany } from "../../../../__tests__/factories";
 
 const REVOKE_RNDTS_DECLARATION_DELEGATION = gql`
   mutation revokeRndtsDeclarationDelegation($delegationId: ID!) {
@@ -54,6 +55,108 @@ describe("mutation revokeRndtsDeclarationDelegation", () => {
       expect(data.revokeRndtsDeclarationDelegation.isRevoked).toBeTruthy();
       expect(data.revokeRndtsDeclarationDelegation.status).toBe("CLOSED");
       expect(updatedDelegation?.isRevoked).toBeTruthy();
+    });
+
+    it("delegator can revoke delegation", async () => {
+      // Given
+      const { delegation, delegatorUser } =
+        await rndtsDeclarationDelegationFactory();
+
+      // When
+      const {
+        errors,
+        data,
+        delegation: updatedDelegation
+      } = await revokeDelegation(delegatorUser, delegation.id);
+
+      // Then
+      expect(errors).toBeUndefined();
+      expect(data.revokeRndtsDeclarationDelegation.isRevoked).toBeTruthy();
+      expect(data.revokeRndtsDeclarationDelegation.status).toBe("CLOSED");
+      expect(updatedDelegation?.isRevoked).toBeTruthy();
+    });
+
+    it("delegate can revoke delegation", async () => {
+      // Given
+      const { delegation, delegateUser } =
+        await rndtsDeclarationDelegationFactory();
+
+      // When
+      const {
+        errors,
+        data,
+        delegation: updatedDelegation
+      } = await revokeDelegation(delegateUser, delegation.id);
+
+      // Then
+      expect(errors).toBeUndefined();
+      expect(data.revokeRndtsDeclarationDelegation.isRevoked).toBeTruthy();
+      expect(data.revokeRndtsDeclarationDelegation.status).toBe("CLOSED");
+      expect(updatedDelegation?.isRevoked).toBeTruthy();
+    });
+  });
+
+  describe("authentication & roles", () => {
+    it("user must be authenticated", async () => {
+      // Given
+      const { delegation } = await rndtsDeclarationDelegationFactory();
+
+      // When
+      const { errors } = await revokeDelegation(null, delegation.id);
+
+      // Then
+      expect(errors).not.toBeUndefined();
+      expect(errors[0].message).toBe("Vous n'êtes pas connecté.");
+    });
+
+    it("user must belong to one of the companies", async () => {
+      // Given
+      const { delegation } = await rndtsDeclarationDelegationFactory();
+      const user = await userFactory();
+
+      // When
+      const { errors } = await revokeDelegation(user, delegation.id);
+
+      // Then
+      expect(errors).not.toBeUndefined();
+      expect(errors[0].message).toBe(
+        "Vous devez faire partie de l'entreprise délégante ou délégataire d'une délégation pour pouvoir la révoquer."
+      );
+    });
+
+    it("user must be admin", async () => {
+      // Given
+      const { delegation, delegateCompany } =
+        await rndtsDeclarationDelegationFactory();
+      const user = await userInCompany("MEMBER", delegateCompany.id);
+
+      // When
+      const { errors } = await revokeDelegation(user, delegation.id);
+
+      // Then
+      expect(errors).not.toBeUndefined();
+      expect(errors[0].message).toBe(
+        "Vous devez être admin pour pouvoir révoquer une délégation."
+      );
+    });
+  });
+
+  describe("async validation", () => {
+    it("should throw if delegation does not exist", async () => {
+      // Given
+      const user = await userFactory();
+
+      // When
+      const { errors } = await revokeDelegation(
+        user,
+        "cxxxxxxxxxxxxxxxxxxxxxxxx"
+      );
+
+      // Then
+      expect(errors).not.toBeUndefined();
+      expect(errors[0].message).toBe(
+        "La demande de délégation cxxxxxxxxxxxxxxxxxxxxxxxx n'existe pas."
+      );
     });
   });
 });
