95d6316e964c4b7fb6e2c88f54e6e9d8470e808b
silto <silte00@gmail.com>
2024-10-15 17:16:15 +0200

    fix(Shared): remove prisma ref in shared constants

--- libs/shared/constants/src/authorizedNotifications.ts
+++ libs/shared/constants/src/authorizedNotifications.ts
@@ -1,30 +1,28 @@
-import { UserRole, UserNotification } from "@prisma/client";
-
 export const ALL_NOTIFICATIONS = [
-  UserNotification.MEMBERSHIP_REQUEST,
-  UserNotification.REVISION_REQUEST,
-  UserNotification.BSD_REFUSAL,
-  UserNotification.SIGNATURE_CODE_RENEWAL,
-  UserNotification.BSDA_FINAL_DESTINATION_UPDATE
+  "MEMBERSHIP_REQUEST",
+  "REVISION_REQUEST",
+  "BSD_REFUSAL",
+  "SIGNATURE_CODE_RENEWAL",
+  "BSDA_FINAL_DESTINATION_UPDATE"
 ];
 
 export const authorizedNotifications = {
-  [UserRole.ADMIN]: ALL_NOTIFICATIONS,
-  [UserRole.MEMBER]: [
-    UserNotification.REVISION_REQUEST,
-    UserNotification.BSD_REFUSAL,
-    UserNotification.SIGNATURE_CODE_RENEWAL,
-    UserNotification.BSDA_FINAL_DESTINATION_UPDATE
+  ADMIN: ALL_NOTIFICATIONS,
+  MEMBER: [
+    "REVISION_REQUEST",
+    "BSD_REFUSAL",
+    "SIGNATURE_CODE_RENEWAL",
+    "BSDA_FINAL_DESTINATION_UPDATE"
   ],
-  [UserRole.READER]: [
-    UserNotification.REVISION_REQUEST,
-    UserNotification.BSD_REFUSAL,
-    UserNotification.SIGNATURE_CODE_RENEWAL,
-    UserNotification.BSDA_FINAL_DESTINATION_UPDATE
+  READER: [
+    "REVISION_REQUEST",
+    "BSD_REFUSAL",
+    "SIGNATURE_CODE_RENEWAL",
+    "BSDA_FINAL_DESTINATION_UPDATE"
   ],
-  [UserRole.DRIVER]: [
-    UserNotification.BSD_REFUSAL,
-    UserNotification.SIGNATURE_CODE_RENEWAL,
-    UserNotification.BSDA_FINAL_DESTINATION_UPDATE
+  DRIVER: [
+    "BSD_REFUSAL",
+    "SIGNATURE_CODE_RENEWAL",
+    "BSDA_FINAL_DESTINATION_UPDATE"
   ]
 };
