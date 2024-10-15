a1c8c0eb60f99bd2f58fb4e39edbb111f5dc1865
Julien Seren-Rosso <julien.serenrosso@beta.gouv.fr>
2024-10-15 13:10:44 +0200

    Adds page titles (#3639)

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ApolloProvider } from "@apollo/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import client from "./graphql-client";
 import LayoutContainer from "./Apps/common/Components/layout/LayoutContainer";
 import setYupLocale from "./common/setYupLocale";
@@ -8,27 +8,27 @@ import BrowserDetect from "./BrowserDetect";
 import ErrorBoundary from "./ErrorBoundary";
 import { FeatureFlagsProvider } from "./common/contexts/FeatureFlagsContext";
 import { PermissionsProvider } from "./common/contexts/PermissionsContext";
-import A11ySkipLinks from "./Apps/common/Components/A11ySkipLinks/A11ySkipLinks";
 
 // Defines app-wide french error messages for yup
 // See https://github.com/jquense/yup#using-a-custom-locale-dictionary
 setYupLocale();
+
+const router = createBrowserRouter([
+  { path: "*", element: <LayoutContainer /> }
+]);
 
 export default function App() {
   return (
     <BrowserDetect>
       <ErrorBoundary>
         <ApolloProvider client={client}>
-          <Router>
-            <PermissionsProvider defaultPermissions={[]}>
-              <FeatureFlagsProvider defaultFeatureFlags={{}}>
-                <div className="App">
-                  <A11ySkipLinks />
-                  <LayoutContainer />
-                </div>
-              </FeatureFlagsProvider>
-            </PermissionsProvider>
-          </Router>
+          <PermissionsProvider defaultPermissions={[]}>
+            <FeatureFlagsProvider defaultFeatureFlags={{}}>
+              <div className="App">
+                <RouterProvider router={router} />
+              </div>
+            </FeatureFlagsProvider>
+          </PermissionsProvider>
         </ApolloProvider>
       </ErrorBoundary>
     </BrowserDetect>
