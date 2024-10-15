5eb470c7b4b64a2ddd5eec30cc6fb956dbd6d75b
Laurent Paoletti <lp@providenz.fr>
2024-10-15 09:09:17 +0200

    Cleanup worker certification data when hasNoSubSectionThree is false 8c8084c

--- back/src/companies/resolvers/mutations/updateWorkerCertification.ts
+++ back/src/companies/resolvers/mutations/updateWorkerCertification.ts
@@ -1,12 +1,26 @@
 import { applyAuthStrategies, AuthType } from "../../../auth";
 import { removeEmptyKeys } from "../../../common/converter";
 import { checkIsAuthenticated } from "../../../common/permissions";
-import { MutationUpdateWorkerCertificationArgs } from "../../../generated/graphql/types";
+import {
+  MutationUpdateWorkerCertificationArgs,
+  UpdateWorkerCertificationInput
+} from "../../../generated/graphql/types";
 import { prisma } from "@td/prisma";
 import { GraphQLContext } from "../../../types";
 import { getWorkerCertificationOrNotFound } from "../../database";
 import { checkCanReadUpdateDeleteWorkerCertification } from "../../permissions";
 import { workerCertificationSchema } from "./createWorkerCertification";
+
+const cleanupCertificate = (
+  input: Omit<UpdateWorkerCertificationInput, "id">
+) => {
+  return {
+    ...removeEmptyKeys(input),
+    certificationNumber: "",
+    organisation: "",
+    validityLimit: null
+  };
+};
 
 export async function updateWorkerCertification(
   _,
@@ -16,13 +30,15 @@ export async function updateWorkerCertification(
   applyAuthStrategies(context, [AuthType.Session]);
   const user = checkIsAuthenticated(context);
   const { id, ...data } = input;
-
+  const hasNoSubSectionThree = input.hasSubSectionThree === false;
   const certification = await getWorkerCertificationOrNotFound({ id });
   await checkCanReadUpdateDeleteWorkerCertification(user, certification);
   await workerCertificationSchema.validate(input);
 
   return prisma.workerCertification.update({
-    data: removeEmptyKeys(data),
+    data: hasNoSubSectionThree
+      ? cleanupCertificate(data)
+      : removeEmptyKeys(data),
     where: { id: certification.id }
   });
 }
