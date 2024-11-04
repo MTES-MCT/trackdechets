97fb3ea7c603e49cd78987aad642b09b9e137f0c
GaelFerrand <gael.ferrand@gmail.com>
2024-11-04 17:03:48 +0100

    feat: added migration script

---
+++ back/src/scripts/bin/fixEmptyReturnADR.ts
@@ -0,0 +1,34 @@
+import { prisma } from "@td/prisma";
+import { getFormRepository } from "../../forms/repository";
+
+// Ticket Favro: https://favro.com/widget/ab14a4f0460a99a9d64d4945/76e5e3cb6cf95d7444ba2c0c?card=tra-15272
+// Script pour passer tous les bordereaux à emptyReturnADR: "EMPTY_NOT_WASHED", à emptyReturnADR: "EMPTY_RETURN_NOT_WASHED"
+// (la valeur EMPTY_NOT_WASHED étant supprimée)
+//
+// !!! DOIT ÊTRE EXECUTE **AVANT** LA MIGRATION !!!
+(async function () {
+  const bsds = await prisma.form.findMany({
+    where: { emptyReturnADR: "EMPTY_NOT_WASHED" },
+    select: { id: true, readableId: true }
+  });
+
+  console.log(`Found ${bsds.length} BSDs to fix`);
+
+  // If number is not normal, give time to Ctrl + C the whole process
+  await new Promise(res => setTimeout(res, 10000));
+
+  console.log("Updating...");
+
+  const { update } = await getFormRepository({
+    id: "support_tech"
+  } as Express.User);
+
+  // Going through BSDs
+  for (const bsd of bsds) {
+    console.log(`Fixing BSD ${bsd.readableId}`);
+
+    await update({ id: bsd.id }, { emptyReturnADR: "EMPTY_RETURN_NOT_WASHED" });
+  }
+
+  console.log("Done!");
+})().then(() => process.exit());
