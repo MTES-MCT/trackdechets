0f2cc3752044540935bab1b7acb62506f797a9d3
silto <silte00@gmail.com>
2024-11-29 11:59:24 +0100

    fix(BSVHU): show bsvhu on emitter's dashboard if he signs up after it was created + update changelog

--- back/src/bsvhu/elastic.ts
+++ back/src/bsvhu/elastic.ts
@@ -131,7 +131,13 @@ export function getWhere(bsvhu: BsvhuForElastic): Pick<BsdElastic, WhereKeys> {
         for (const fieldName of siretsFilters.keys()) {
           setTab(siretsFilters, fieldName, "isDraftFor");
         }
-      } else if (bsvhu.emitterNoSiret || bsvhu.emitterNotOnTD) {
+      } else if (bsvhu.emitterNotOnTD) {
+        // even though the emitter is not on TD, we still add the SIRET to the index
+        // so if the account is created after the BSVHU, it will still appear in the
+        // emitter's dashboard.
+        setTab(siretsFilters, "emitterCompanySiret", "isForActionFor");
+        setTab(siretsFilters, "transporterCompanySiret", "isToCollectFor");
+      } else if (bsvhu.emitterNoSiret) {
         setTab(siretsFilters, "transporterCompanySiret", "isToCollectFor");
       } else {
         setTab(siretsFilters, "emitterCompanySiret", "isForActionFor");
