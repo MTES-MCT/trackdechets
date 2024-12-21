c26b2733e35ee10a293668a553fce0d4a883c724
Riron <orioncharlier@gmail.com>
2024-12-21 15:27:38 +0100

    Script compatibility with bash 3.2

--- scripts/restore-db.sh
+++ scripts/restore-db.sh
@@ -6,13 +6,13 @@
 BASEDIR=$(realpath "$0" | sed 's|\(.*\)/.*|\1|')
 psql_container_id=$(docker ps -qf name=^/trackdechets.postgres)
 
-read -erp $'\e[1m! Before running this script, make sure you closed all open connections to the DB (app, queue, notifier, SQL soft...)\e[m'
+printf "\e[1m! Before running this script, make sure you closed all open connections to the DB (app, queue, notifier, SQL soft...)\e[m\n"
 
 read -erp $'\e[1m? Do you wish to download the latest backup of your chosen database \e[m (Y/n) ' -e downloadBackup
 downloadBackup=${downloadBackup:-Y}
 
 if [ "$downloadBackup" != "${downloadBackup#[Yy]}" ]; then
-    echo -e "\e[90m"
+    printf "\e[90m"
 
     backupName="db_backup.pgsql"
     backupPath="$BASEDIR/$backupName"
@@ -26,38 +26,42 @@ if [ "$downloadBackup" != "${downloadBackup#[Yy]}" ]; then
       mv "$name" $backupName
     done
     rm $backupTarName
-    echo -e "\e[m"
+    printf "\e[m"
 else
     while read -erp $'\e[1m? Enter local backup path (pgsql file):\e[m ' backupPath; do
         if [ -f "$backupPath" ]; then
             break
         else
-            echo -e "\e[91m$backupPath is not a valid path.\e[m"
-        fi 
+            printf "\e[91m$backupPath is not a valid path.\e[m\n"
+        fi
     done
-fi 
+fi
 
-echo -e "\e[1m→ Using backup file \e[36m$backupPath\e[m"
+printf "\e[1m→ Using backup file \e[36m$backupPath\e[m\n"
 
-read -rp $'\e[1m? Postgres User:\e[m ' -i "trackdechets" -e psqlUser
+default_user="trackdechets"
+printf "\e[1m? Postgres User\e[m [$default_user]: "
+read psqlUser
+psqlUser="${psqlUser:-$default_user}"
+# read -rp $'\e[1m? Postgres User:\e[m ' -i "trackdechets" -e psqlUser
 
 echo "Copying backup file to postgres"
 docker cp "$backupPath" "$psql_container_id":/tmp/dump.sql
 
-echo -e "\e[1m→ Recreating DB \e[36mprisma\e[m"
+printf "\e[1m→ Recreating DB \e[36mprisma\e[m\n"
 docker exec -t "$psql_container_id" bash -c "psql -U $psqlUser -c \"DROP DATABASE IF EXISTS prisma;\"";
 docker exec -t "$psql_container_id" bash -c "psql -U $psqlUser -c \"CREATE DATABASE prisma;\"";
 docker exec -t "$psql_container_id" bash -c "psql -U $psqlUser -d prisma -c 'CREATE SCHEMA default\$default;'";
 
-echo -e "\e[1m→ Restoring dump"
+printf "\e[1m→ Restoring dump\n"
 docker exec -t "$psql_container_id" bash -c "pg_restore -U $psqlUser -d prisma --clean /tmp/dump.sql 2>/dev/null";
 
 PWD=$(pwd)
 if [ "$BASEDIR"  == "$PWD" ]; then
   APP_DIR=$(dirname "$PWD")
-  echo -e "\e[1m→ Changing directory to $APP_DIR, as migrate needs to access envs\e[m"
+  printf "\e[1m→ Changing directory to $APP_DIR, as migrate needs to access envs\e[m\n"
   cd "$APP_DIR" || exit
 fi
 
-echo -e "\e[1m→ Running SQL migrations"
+printf "\e[1m→ Running SQL migrations\n"
 npx prisma migrate dev
