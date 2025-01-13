0d6dc056b9a0be8b21fa991801d05bfd0f334935
Julien Seren-Rosso <julien.serenrosso@beta.gouv.fr>
2025-01-13 09:25:43 +0100

    Updates banner

--- front/src/Apps/common/Components/layout/LayoutContainer.tsx
+++ front/src/Apps/common/Components/layout/LayoutContainer.tsx
@@ -70,7 +70,7 @@ const GET_ME = gql`
 `;
 
 const BANNER_MESSAGES = [
-  `Merci d'anticiper dès à présent vos congés en vous assurant que vos collaborateurs possèdent un accès aux établissements nécessaires sur Trackdéchets (Mes établissements > Membres).`
+  `Abonnez-vous à notre lettre d'information mensuelle pour suivre les nouveautés de la plateforme, la programmation des formations, des conseils pratiques, ainsi que les évolutions réglementaires liées à la traçabilité des déchets.`
 ];
 
 export default function LayoutContainer() {
@@ -122,10 +122,10 @@ export default function LayoutContainer() {
                 <SurveyBanner
                   messages={BANNER_MESSAGES}
                   button={{
-                    title: "Consulter la FAQ",
-                    href: "https://faq.trackdechets.fr/inscription-et-gestion-de-compte/gerer-son-compte/inviter-des-personnes-a-rejoindre-mon-etablissement#les-responsabilites-des-administrateurs-trackdechets"
+                    title: "Je m'abonne",
+                    href: "https://0806de2d.sibforms.com/serve/MUIEAG29k1cikyqt55ql5CSQp_3hunRICQ8Eu8IvTZMpZl1EuQSEYeErCYUb31W6nx1mUfBKGfamqI9xMrql4caFpN2IUJQ_NR-00sPbnSv5Kw21AYm8tMHap8_7ah9NCHlcPqpNKrp7CPjO2zYsiAaBFX8r3PHDY72zP55LieF3N9gc3sUfOG16ioQgATXDPF0GeDpTuU46gBWT"
                   }}
-                  persistedSurveyName="td-20241217"
+                  persistedSurveyName="td-20240114"
                 />
               }
               defaultOrgId={defaultOrgId}
