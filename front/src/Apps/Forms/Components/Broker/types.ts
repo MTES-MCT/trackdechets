fc47e7dea2dcc1e3db49ad129dd3e02cda4be5a2
Benoit Guigal <benoit.guigal@protonmail.com>
2025-01-28 12:04:25 +0100

    fix typo

--- front/src/Apps/Forms/Components/Broker/types.ts
+++ front/src/Apps/Forms/Components/Broker/types.ts
@@ -2,8 +2,8 @@
 // On choisit arbitrairement un type de bordereau comme réferentiel
 // commun et on s'assurera de convertir les données dans un sens et
 // dans l'autre lorsque ce composant est utilisé sur un autre type de
+// bordereau (ex: BSDD)
 
 import { BsdaBrokerInput } from "@td/codegen-ui";
 
-// bordereau (ex: BSDD)
 export type CommonBrokerInput = BsdaBrokerInput;
