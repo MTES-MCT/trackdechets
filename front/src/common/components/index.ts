f019b0f481559ed294e94647848c0db990552286
JulianaJM <juliana.jeanmarie972@gmail.com>
2025-02-12 11:37:12 +0100

    [tra-15844]Corriger l'affichage de la timeline dans les modales de signature PAOH

--- front/src/common/components/index.ts
+++ front/src/common/components/index.ts
@@ -20,7 +20,6 @@ export * from "./Slideshow";
 export * from "./List";
 export * from "./Table";
 export * from "../../Apps/Dashboard/Components/Journey/Journey";
-export * from "../../Apps/common/Components/Journey/DsfrJourney";
 export * from "./DataList";
 export * from "../../Apps/common/Components/DataList/DsfrDataList";
 export * from "./TransportModeSelect";
