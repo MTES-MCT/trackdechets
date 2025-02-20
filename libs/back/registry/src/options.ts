576fabad8de10959d3961c35dafd39828f923ea6
Riron <orioncharlier@gmail.com>
2025-02-20 10:35:41 +0100

    Format

--- libs/back/registry/src/options.ts
+++ libs/back/registry/src/options.ts
@@ -1,40 +1,40 @@
 import type {
-    IncomingWasteV2,
-    OutgoingWasteV2,
-    SsdWasteV2
+  IncomingWasteV2,
+  OutgoingWasteV2,
+  SsdWasteV2
 } from "@td/codegen-back";
 import { SafeParseReturnType } from "zod";
 import { INCOMING_TEXS_HEADERS } from "./incomingTexs/constants";
 import {
-    getIncomingTexsImportSiretsAssociations,
-    saveIncomingTexsLine
+  getIncomingTexsImportSiretsAssociations,
+  saveIncomingTexsLine
 } from "./incomingTexs/database";
 import { toIncomingWaste as IncomingTexsToIncomingWaste } from "./incomingTexs/registry";
 import { safeParseAsyncIncomingTexs } from "./incomingTexs/validation";
 import { INCOMING_WASTE_HEADERS } from "./incomingWaste/constants";
 import {
-    getIncomingWasteImportSiretsAssociations,
-    saveIncomingWasteLine
+  getIncomingWasteImportSiretsAssociations,
+  saveIncomingWasteLine
 } from "./incomingWaste/database";
 import { toIncomingWaste as IncomingWasteToIncomingWaste } from "./incomingWaste/registry";
 import { safeParseAsyncIncomingWaste } from "./incomingWaste/validation";
 import { MANAGED_HEADERS } from "./managed/constants";
 import {
-    getManagedImportSiretsAssociations,
-    saveManagedLine
+  getManagedImportSiretsAssociations,
+  saveManagedLine
 } from "./managed/database";
 import { safeParseAsyncManaged } from "./managed/validation";
 import { OUTGOING_TEXS_HEADERS } from "./outgoingTexs/constants";
 import {
-    getOutgoingTexsImportSiretsAssociations,
-    saveOutgoingTexsLine
+  getOutgoingTexsImportSiretsAssociations,
+  saveOutgoingTexsLine
 } from "./outgoingTexs/database";
 import { toOutgoingWaste as OutgoingTexsToOutgoingWaste } from "./outgoingTexs/registry";
 import { safeParseAsyncOutgoingTexs } from "./outgoingTexs/validation";
 import { OUTGOING_WASTE_HEADERS } from "./outgoingWaste/constants";
 import {
-    getOutgoingWasteImportSiretsAssociations,
-    saveOutgoingWasteLine
+  getOutgoingWasteImportSiretsAssociations,
+  saveOutgoingWasteLine
 } from "./outgoingWaste/database";
 import { toOutgoingWaste as OutgoingWasteToOutgoingWaste } from "./outgoingWaste/registry";
 import { safeParseAsyncOutgoingWaste } from "./outgoingWaste/validation";
@@ -44,8 +44,8 @@ import { toSsdWaste as SsdToSsdWaste } from "./ssd/registry";
 import { safeParseAsyncSsd } from "./ssd/validation";
 import { TRANSPORTED_HEADERS } from "./transported/constants";
 import {
-    getTransportedImportSiretsAssociations,
-    saveTransportedLine
+  getTransportedImportSiretsAssociations,
+  saveTransportedLine
 } from "./transported/database";
 import { safeParseAsyncTransported } from "./transported/validation";
 
