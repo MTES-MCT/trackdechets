bed5f79680342cf3cc23ee08bdd32fea6fc81bec
Julien Seren-Rosso <julien.serenrosso@beta.gouv.fr>
2025-02-27 10:22:27 +0100

    Fixes company switcher (#4000)

--- front/src/login/auth.service.ts
+++ front/src/login/auth.service.ts
@@ -1,10 +1,8 @@
 import client from "../graphql-client";
-import { SIRET_STORAGE_KEY } from "../Apps/common/Components/CompanySwitcher/CompanySwitcher";
 
 export const localAuthService = {
   locallySignOut() {
     client.stop();
     client.clearStore();
-    window.localStorage.removeItem(SIRET_STORAGE_KEY);
   }
 };
