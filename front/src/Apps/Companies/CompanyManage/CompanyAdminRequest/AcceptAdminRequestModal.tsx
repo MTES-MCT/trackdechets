6989b9329067001fc095c232795a12772d52541e
GaelFerrand <gael.ferrand@gmail.com>
2025-03-13 15:34:24 +0100

    feat: displaying error messages in modal if request has already been accepted / refused

--- front/src/Apps/Companies/CompanyManage/CompanyAdminRequest/AcceptAdminRequestModal.tsx
+++ front/src/Apps/Companies/CompanyManage/CompanyAdminRequest/AcceptAdminRequestModal.tsx
@@ -5,7 +5,8 @@ import {
   Query,
   Mutation,
   MutationAcceptAdminRequestArgs,
-  MutationRefuseAdminRequestArgs
+  MutationRefuseAdminRequestArgs,
+  AdminRequestStatus
 } from "@td/codegen-ui";
 import { useMutation, useQuery } from "@apollo/client";
 import {
@@ -77,75 +78,89 @@ export const AcceptAdminRequestModal = ({
 
       {loadingGet && <InlineLoader />}
 
-      {!loadingGet && adminRequest && (
-        <>
-          <p className="fr-mb-4w">
-            Acceptez-vous que l'utilisateur <b>{adminRequest.user.name}</b>{" "}
-            devienne <b>ADMINISTRATEUR</b> de l'entreprise{" "}
-            <b>
-              {adminRequest.company.name} - {adminRequest.company.orgId}
-            </b>{" "}
-            ?
-          </p>
+      {!loadingGet &&
+        adminRequest &&
+        adminRequest.status === AdminRequestStatus.Pending && (
+          <>
+            <p className="fr-mb-4w">
+              Acceptez-vous que l'utilisateur <b>{adminRequest.user.name}</b>{" "}
+              devienne <b>ADMINISTRATEUR</b> de l'entreprise{" "}
+              <b>
+                {adminRequest.company.name} - {adminRequest.company.orgId}
+              </b>{" "}
+              ?
+            </p>
 
-          {errorAccept && (
-            <Alert
-              title="Erreur"
-              description={errorAccept.message}
-              severity="error"
-            />
-          )}
-          {errorRefuse && (
-            <Alert
-              title="Erreur"
-              description={errorRefuse.message}
-              severity="error"
-            />
-          )}
+            {errorAccept && (
+              <Alert
+                title="Erreur"
+                description={errorAccept.message}
+                severity="error"
+              />
+            )}
+            {errorRefuse && (
+              <Alert
+                title="Erreur"
+                description={errorRefuse.message}
+                severity="error"
+              />
+            )}
 
-          <div className="dsfr-modal-actions">
-            <Button
-              disabled={loadingAccept || loadingRefuse}
-              priority="primary"
-              onClick={() => {
-                resetRefuse();
-                resetAccept();
+            <div className="dsfr-modal-actions">
+              <Button
+                disabled={loadingAccept || loadingRefuse}
+                priority="primary"
+                onClick={() => {
+                  resetRefuse();
+                  resetAccept();
 
-                acceptAdminRequest({
-                  variables: { input: { adminRequestId } },
-                  onCompleted: () => {
-                    toast.success("Demande accordée");
-                    onClose();
-                  }
-                });
-              }}
-              type="button"
-            >
-              Accepter
-            </Button>
+                  acceptAdminRequest({
+                    variables: { input: { adminRequestId } },
+                    onCompleted: () => {
+                      toast.success("Demande accordée");
+                      onClose();
+                    }
+                  });
+                }}
+                type="button"
+              >
+                Accepter
+              </Button>
 
-            <Button
-              priority="secondary"
-              type="submit"
-              disabled={loadingAccept || loadingRefuse}
-              onClick={() => {
-                resetRefuse();
-                resetAccept();
+              <Button
+                priority="secondary"
+                type="submit"
+                disabled={loadingAccept || loadingRefuse}
+                onClick={() => {
+                  resetRefuse();
+                  resetAccept();
 
-                refuseAdminRequest({
-                  variables: { adminRequestId },
-                  onCompleted: () => {
-                    toast.success("Demande refusée");
-                    onClose();
-                  }
-                });
-              }}
-            >
-              Refuser
-            </Button>
-          </div>
-        </>
-      )}
+                  refuseAdminRequest({
+                    variables: { adminRequestId },
+                    onCompleted: () => {
+                      toast.success("Demande refusée");
+                      onClose();
+                    }
+                  });
+                }}
+              >
+                Refuser
+              </Button>
+            </div>
+          </>
+        )}
+
+      {!loadingGet &&
+        adminRequest &&
+        adminRequest.status === AdminRequestStatus.Accepted && (
+          <p>Cette demande a déjà été acceptée.</p>
+        )}
+
+      {!loadingGet &&
+        adminRequest &&
+        adminRequest.status === AdminRequestStatus.Refused && (
+          <p>Cette demande a déjà été refusée.</p>
+        )}
     </TdModal>
   );
 };
