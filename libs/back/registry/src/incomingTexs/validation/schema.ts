65ef9c8222bab0e49aefccf0bccfb4f91ef6937c
silto <silte00@gmail.com>
2025-04-03 23:55:56 +0200

    fix(RegistryV2): fix xlsx parsing by adding a patch to exceljs + various small fixes

--- libs/back/registry/src/incomingTexs/validation/schema.ts
+++ libs/back/registry/src/incomingTexs/validation/schema.ts
@@ -64,7 +64,8 @@ const inputIncomingTexsSchema = z.object({
   sisIdentifier: z
     .string()
     .trim()
-    .max(13, "Un identifiant SIS ne doit pas excéder 13 caractères")
+    .min(10, "Un identifiant SIS doit faire au moins 10 caractères")
+    .max(15, "Un identifiant SIS ne doit pas excéder 15 caractères")
     .nullish(),
   initialEmitterCompanyType: actorTypeSchema.nullish(),
   initialEmitterCompanyOrgId: actorOrgIdSchema.nullish(),
