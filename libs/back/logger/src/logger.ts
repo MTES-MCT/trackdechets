80f3f4bb2bbd2d2f64add3e788dfc1b82e7769fa
silto <silte00@gmail.com>
2025-04-28 13:47:06 +0200

    Redact Winston logs (#4149)

--- libs/back/logger/src/logger.ts
+++ libs/back/logger/src/logger.ts
@@ -5,7 +5,30 @@ import { addMetadata } from "./format";
 const LOG_PATH = `${appRoot}/logs/app.log`;
 // Avoid using undefined console.log() in jest context
 const LOG_TO_CONSOLE =
-  process.env.FORCE_LOGGER_CONSOLE && process.env.JEST_WORKER_ID === undefined;
+  process.env.FORCE_LOGGER_CONSOLE === "true" &&
+  process.env.JEST_WORKER_ID === undefined;
+const sensitiveFields = ["password", "token", "secret", "key"];
+const sanitizeObject = (obj: any) => {
+  if (!obj || typeof obj !== "object") return obj;
+
+  for (const key in obj) {
+    if (sensitiveFields.some(field => key.toLowerCase().includes(field))) {
+      obj[key] = "[REDACTED]";
+    } else if (typeof obj[key] === "object") {
+      if (obj[key] !== null && !Array.isArray(obj[key])) {
+        obj[key] = sanitizeObject({
+          ...obj[key]
+        });
+      }
+    }
+  }
+  return obj;
+};
+const sanitizeVariables = (variables: Record<string, any>) => {
+  const sanitized = { ...variables };
+
+  return sanitizeObject(sanitized);
+};
 
 export const logger = createLogger({
   level: "info",
@@ -13,7 +36,16 @@ export const logger = createLogger({
   format: format.combine(
     addMetadata(),
     format.errors({ stack: true }),
-    format.json()
+    format.json(),
+    format(info => {
+      if (!info.graphql_variables) {
+        return info;
+      }
+      return {
+        ...info,
+        graphql_variables: sanitizeVariables(info.graphql_variables)
+      };
+    })()
   ),
   transports: [
     LOG_TO_CONSOLE
