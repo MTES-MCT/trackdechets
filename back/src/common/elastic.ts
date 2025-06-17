e9c85e93ec4e2e6d5a643766e89e208ef5cfebb3
GaelFerrand <gael.ferrand@gmail.com>
2025-06-17 15:10:11 +0200

    feat: bump ES schema version

--- back/src/common/elastic.ts
+++ back/src/common/elastic.ts
@@ -357,7 +357,7 @@ export const index: BsdIndexationConfig = {
   // increment when mapping has changed to trigger re-indexation on release
   // only use vX.Y.Z that matches regexp "v\d\.\d\.\d"
   // no special characters that are not supported by ES index names (like ":")
-  mappings_version: "v1.1.3",
+  mappings_version: "v1.1.4",
   mappings: {
     properties
   },
