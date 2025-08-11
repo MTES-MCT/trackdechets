52cc3d1adc2ac0919fc46c9829db6233f5c7b5a6
Riron <orioncharlier@gmail.com>
2025-08-11 15:36:58 +0200

    Fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,9 +5,8 @@ import { defineConfig } from "prisma/config";
 const prismaFolder = path.join("libs", "back", "prisma", "src");
 
 export default defineConfig({
-  schema: path.join(prismaFolder, "schema.prisma"),
+  schema: prismaFolder,
   migrations: {
-    path: path.join(prismaFolder, "migrations"),
     seed: "tsx --tsconfig back/tsconfig.lib.json back/prisma/seed.ts"
   }
 });
