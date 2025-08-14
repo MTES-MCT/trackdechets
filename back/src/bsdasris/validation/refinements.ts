2db363655259320d0f54b3e574e89a499cd80c73
silto <silte00@gmail.com>
2025-08-14 03:01:59 +0200

    feat(DASRI): make it possible for wastecenters (2710) to receive DASRI with D13/R12 codes

--- back/src/bsdasris/validation/refinements.ts
+++ back/src/bsdasris/validation/refinements.ts
@@ -25,7 +25,7 @@ import { isDefined } from "../../common/helpers";
 
 import { DASRI_GROUPING_OPERATIONS_CODES } from "@td/constants";
 import { prisma } from "@td/prisma";
-import { isCollector } from "../../companies/validation";
+import { isCollector, isWasteCenter } from "../../companies/validation";
 
 export const validateDestinationOperationCode: (
   validationContext: BsdasriValidationContext
@@ -116,7 +116,10 @@ export const validateRecipientIsCollectorForGroupingCodes: (
         }
       });
 
-      if (!destinationCompany || !isCollector(destinationCompany)) {
+      if (
+        !destinationCompany ||
+        (!isCollector(destinationCompany) && !isWasteCenter(destinationCompany))
+      ) {
         addIssue({
           code: z.ZodIssueCode.custom,
           fatal: true,
