c6952f54be054c017e5893663e8875a02e953444
Julien Seren-Rosso <julien.serenrosso@beta.gouv.fr>
2025-09-19 15:23:37 +0200

    [TRA 16284] Updates banner (#4441)

--- front/src/Apps/common/Components/layout/LayoutContainer.tsx
+++ front/src/Apps/common/Components/layout/LayoutContainer.tsx
@@ -31,7 +31,7 @@ import WasteTree from "../search/WasteTree";
 const Company = lazy(() => import("../../../../Pages/Company/Company"));
 
 const BANNER_MESSAGES = [
-  `Le profil "Entreprise de travaux amiante" a été retiré pour les établissements n'ayant pas complété les compétences requises (SS3 et/ou SS4). Il peut être ajouté à nouveau à tout moment depuis l'onglet Mes établissements.`
+  `Abonnez-vous à notre lettre d'information mensuelle pour suivre les nouveautés de la plateforme, la programmation des formations, des conseils pratiques, ainsi que les évolutions réglementaires liées à la traçabilité des déchets.`
 ];
 
 const IS_AUTHENTICATED = gql`
@@ -68,10 +68,10 @@ export default function LayoutContainer() {
                 <SurveyBanner
                   messages={BANNER_MESSAGES}
                   button={{
-                    title: "En savoir plus",
-                    href: "https://faq.trackdechets.fr/amiante/entreprises-de-travaux-maitres-doeuvre"
+                    title: "Je m'abonne",
+                    href: "https://0806de2d.sibforms.com/serve/MUIEAG29k1cikyqt55ql5CSQp_3hunRICQ8Eu8IvTZMpZl1EuQSEYeErCYUb31W6nx1mUfBKGfamqI9xMrql4caFpN2IUJQ_NR-00sPbnSv5Kw21AYm8tMHap8_7ah9NCHlcPqpNKrp7CPjO2zYsiAaBFX8r3PHDY72zP55LieF3N9gc3sUfOG16ioQgATXDPF0GeDpTuU46gBWT "
                   }}
-                  persistedSurveyName="td-20250826"
+                  persistedSurveyName="td-20250923"
                 />
               }
             />
