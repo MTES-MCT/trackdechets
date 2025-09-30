604eefbbfdb770081518b43ce49651e18897ac0b
Riron <orioncharlier@gmail.com>
2025-09-30 22:01:02 +0200

    Fix warning

--- libs/back/prisma/src/index.ts
+++ libs/back/prisma/src/index.ts
@@ -1,8 +1,7 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-import { collectMetrics } from "./metrics";
 
-const { DATABASE_URL, NODE_ENV } = process.env;
+const { DATABASE_URL } = process.env;
 
 if (!DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined");
@@ -17,7 +16,3 @@ export const prisma = new PrismaClient({
   adapter,
   log: process.env.NODE_ENV !== "test" ? ["info", "warn", "error"] : []
 });
-
-if (NODE_ENV === "production") {
-  collectMetrics(prisma);
-}
