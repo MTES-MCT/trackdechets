39e2723c7f0f761a1d144197c1963e3ab09dc079
Riron <orioncharlier@gmail.com>
2025-09-30 15:31:09 +0200

    Update to prisma 6.16

--- libs/back/prisma/src/index.ts
+++ libs/back/prisma/src/index.ts
@@ -1,6 +1,5 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-import { unescape } from "node:querystring";
-import { URL } from "node:url";
 import { collectMetrics } from "./metrics";
 
 const { DATABASE_URL, NODE_ENV } = process.env;
@@ -9,24 +8,16 @@ if (!DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined");
 }
 
+const adapter = new PrismaPg(
+  { connectionString: DATABASE_URL },
+  { schema: "default$default" }
+);
+
 export const prisma = new PrismaClient({
-  datasources: {
-    db: { url: getDbUrlWithSchema(DATABASE_URL) }
-  },
+  adapter,
   log: process.env.NODE_ENV !== "test" ? ["info", "warn", "error"] : []
 });
 
 if (NODE_ENV === "production") {
   collectMetrics(prisma);
-}
-
-function getDbUrlWithSchema(rawDatabaseUrl: string) {
-  try {
-    const dbUrl = new URL(rawDatabaseUrl);
-    dbUrl.searchParams.set("schema", "default$default");
-
-    return unescape(dbUrl.href); // unescape needed because of the `$`
-  } catch (_) {
-    return "";
-  }
 }
