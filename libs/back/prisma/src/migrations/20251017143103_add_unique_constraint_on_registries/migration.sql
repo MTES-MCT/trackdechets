5eafc2610a5b66bed7a931a05c0233952b17a1d9
Riron <orioncharlier@gmail.com>
2025-10-17 16:56:36 +0200

    Use partial index

--- libs/back/prisma/src/migrations/20251017143103_add_unique_constraint_on_registries/migration.sql
+++ libs/back/prisma/src/migrations/20251017143103_add_unique_constraint_on_registries/migration.sql
@@ -1,20 +1,43 @@
+-- Partial indexes to enforce unique constraints on registries tables
+-- This is not supported by Prisma, so this doesnt appear in the registry.prisma file
 -- CreateIndex
-CREATE UNIQUE INDEX IF NOT EXISTS "_RegistryIncomingTexsPublicIdReportForCompanySiretIsLatestUIdx" ON "RegistryIncomingTexs"("publicId", "reportForCompanySiret", "isLatest");
+CREATE UNIQUE INDEX IF NOT EXISTS
+"_RegistryIncomingTexsPublicIdReportForCompanySiretIsLatestUIdx" ON
+"RegistryIncomingTexs"("publicId", "reportForCompanySiret")
+WHERE "isLatest"=TRUE;
 
 -- CreateIndex
-CREATE UNIQUE INDEX IF NOT EXISTS "_RegistryIncomingWastePublicIdReportForCompanySiretIsLatestUIdx" ON "RegistryIncomingWaste"("publicId", "reportForCompanySiret", "isLatest");
+CREATE UNIQUE INDEX IF NOT EXISTS
+"_RegistryIncomingWastePublicIdReportForCompanySiretIsLatestUIdx" ON
+"RegistryIncomingWaste"("publicId", "reportForCompanySiret")
+WHERE "isLatest"=TRUE;
 
 -- CreateIndex
-CREATE UNIQUE INDEX IF NOT EXISTS "_RegistryManagedPublicIdReportForCompanySiretIsLatestUIdx" ON "RegistryManaged"("publicId", "reportForCompanySiret", "isLatest");
+CREATE UNIQUE INDEX IF NOT EXISTS
+"_RegistryManagedPublicIdReportForCompanySiretIsLatestUIdx" ON "RegistryManaged"
+("publicId", "reportForCompanySiret")
+WHERE "isLatest"=TRUE;
 
 -- CreateIndex
-CREATE UNIQUE INDEX IF NOT EXISTS "_RegistryOutgoingTexsPublicIdReportForCompanySiretIsLatestUIdx" ON "RegistryOutgoingTexs"("publicId", "reportForCompanySiret", "isLatest");
+CREATE UNIQUE INDEX IF NOT EXISTS
+"_RegistryOutgoingTexsPublicIdReportForCompanySiretIsLatestUIdx" ON
+"RegistryOutgoingTexs"("publicId", "reportForCompanySiret")
+WHERE "isLatest"=TRUE;
 
 -- CreateIndex
-CREATE UNIQUE INDEX IF NOT EXISTS "_RegistryOutgoingWastePublicIdReportForCompanySiretIsLatestUIdx" ON "RegistryOutgoingWaste"("publicId", "reportForCompanySiret", "isLatest");
+CREATE UNIQUE INDEX IF NOT EXISTS
+"_RegistryOutgoingWastePublicIdReportForCompanySiretIsLatestUIdx" ON
+"RegistryOutgoingWaste"("publicId", "reportForCompanySiret")
+WHERE "isLatest"=TRUE;
 
 -- CreateIndex
-CREATE UNIQUE INDEX IF NOT EXISTS "_RegistrySsdPublicIdReportForSiretIsLatestUIdx" ON "RegistrySsd"("publicId", "reportForCompanySiret", "isLatest");
+CREATE UNIQUE INDEX IF NOT EXISTS
+"_RegistrySsdPublicIdReportForSiretIsLatestUIdx" ON "RegistrySsd"("publicId",
+"reportForCompanySiret")
+WHERE "isLatest"=TRUE;
 
 -- CreateIndex
-CREATE UNIQUE INDEX IF NOT EXISTS "_RegistryTransportedPublicIdReportForCompanySiretIsLatestUIdx" ON "RegistryTransported"("publicId", "reportForCompanySiret", "isLatest");
+CREATE UNIQUE INDEX IF NOT EXISTS
+"_RegistryTransportedPublicIdReportForCompanySiretIsLatestUIdx" ON
+"RegistryTransported"("publicId", "reportForCompanySiret")
+WHERE "isLatest"=TRUE; 
