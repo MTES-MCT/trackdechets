8fa4b38110e1b5d840dfe9ee29f89b91a684bb5c
GaelFerrand <45355989+GaelFerrand@users.noreply.github.com>
2025-11-07 09:29:33 +0100

    [TRA-16287] Ajout de nouveaux éco-organismes VHU (#4502)

--- libs/back/object-creator/src/objects.ts
+++ libs/back/object-creator/src/objects.ts
@@ -266,6 +266,66 @@ const objects = [
       handleBsda: false,
       handleBsvhu: true
     }
+  } as CreationObject<"ecoOrganisme">,
+  {
+    type: "ecoOrganisme",
+    object: {
+      siret: "32836885700020",
+      name: "AIXAM MEGA",
+      address: "56 ROUTE DE PUGNY 73100 AIX-LES-BAINS",
+      handleBsdd: false,
+      handleBsdasri: false,
+      handleBsda: false,
+      handleBsvhu: true
+    }
+  } as CreationObject<"ecoOrganisme">,
+  {
+    type: "ecoOrganisme",
+    object: {
+      siret: "39391874300062",
+      name: "HARLEY-DAVIDSON FRANCE",
+      address: "EUROPARC 12 RUE EUGENE DUPUIS 94000 CRETEIL",
+      handleBsdd: false,
+      handleBsdasri: false,
+      handleBsda: false,
+      handleBsvhu: true
+    }
+  } as CreationObject<"ecoOrganisme">,
+  {
+    type: "ecoOrganisme",
+    object: {
+      siret: "38391529500083",
+      name: "KIA FRANCE",
+      address: "2 RUE DES MARTINETS 92500 RUEIL-MALMAISON",
+      handleBsdd: false,
+      handleBsdasri: false,
+      handleBsda: false,
+      handleBsvhu: true
+    }
+  } as CreationObject<"ecoOrganisme">,
+  {
+    type: "ecoOrganisme",
+    object: {
+      siret: "44451198400031",
+      name: "MIDI FRANCE",
+      address: "6 RUE JEAN-PIERRE TIMBAUD 78180 MONTIGNY-LE-BRETONNEUX",
+      handleBsdd: false,
+      handleBsdasri: false,
+      handleBsda: false,
+      handleBsvhu: true
+    }
+  } as CreationObject<"ecoOrganisme">,
+  {
+    type: "ecoOrganisme",
+    object: {
+      siret: "31022749100159",
+      name: "PIAGGIO FRANCE",
+      address: "21 RUE GEORGES BOISSEAU 92110 CLICHY",
+      handleBsdd: false,
+      handleBsdasri: false,
+      handleBsda: false,
+      handleBsvhu: true
+    }
   } as CreationObject<"ecoOrganisme">
 ];
 
