07d0ec2a0a0627f93dd18d40655ff8b9e1e74fc9
silto <silte00@gmail.com>
2025-12-03 02:07:20 +0100

    feat(All): block npm pre/post install scripts and add allow-scripts to whitelist necessary scripts. remove unused scripts.

--- scripts/build/scalingo.sh
+++ scripts/build/scalingo.sh
@@ -8,7 +8,7 @@ ROOT_DIR="$(dirname "$(dirname "$SCRIPT_DIR")")"
 echo "Building apps for $APP"
 
 if echo "$APP" | grep -o 'api'; then
-    npx patch-package
+    npx allow-scripts
     npx nx run-many -t build --projects=api,cron,tag:backend:queues
     echo "Running migrate for $APP"
     npx prisma migrate deploy
@@ -16,18 +16,22 @@ if echo "$APP" | grep -o 'api'; then
     cp "$ROOT_DIR/.slugignore.back" "$ROOT_DIR/.slugignore"
 
 elif echo "$APP" | grep -q 'notifier'; then
+    npx allow-scripts
     npx nx run notifier:build
     cp "$ROOT_DIR/.slugignore.back" "$ROOT_DIR/.slugignore"
 
 elif echo "$APP" | grep -q 'front'; then
+    npx allow-scripts
     npx nx run ui:build
     cp "$ROOT_DIR/.slugignore.front" "$ROOT_DIR/.slugignore"
 
 elif echo "$APP" | grep -q 'storybook'; then
+    npx allow-scripts
     npx nx run front:build-storybook
     cp "$ROOT_DIR/.slugignore.storybook" "$ROOT_DIR/.slugignore"
 
 elif echo "$APP" | grep -q 'doc'; then
+    npx allow-scripts
     npx nx run doc:build
     cp "$ROOT_DIR/.slugignore.documentation" "$ROOT_DIR/.slugignore"
 fi
