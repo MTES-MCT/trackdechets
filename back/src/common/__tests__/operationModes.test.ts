927aac7e5494668620ff67869c8f32da7ab7de06
Riron <orioncharlier@gmail.com>
2025-12-10 10:11:26 +0100

    Fix

--- back/src/common/__tests__/operationModes.test.ts
+++ back/src/common/__tests__/operationModes.test.ts
@@ -1,25 +1,17 @@
-import { OperationMode } from "@td/prisma";
 import {
   CODES_AND_EXPECTED_OPERATION_MODES,
   getOperationModes
 } from "@td/constants";
 
-const addSpaceAfterFirstCharacter = (input: string): string => {
-  return input[0] + " " + input.slice(1);
-};
-
-const test = (code: string, expectedModes: OperationMode[]) => {
+const test = (code: string) => {
   // Without spaces
   const mode = getOperationModes(code);
-  expect(mode).toEqual(expectedModes);
-
-  // With spaces
-  const modeWithSpace = getOperationModes(addSpaceAfterFirstCharacter(code));
-  expect(modeWithSpace).toEqual(expectedModes);
+  const modeWithoutSpace = getOperationModes(code.replace(/ /g, ""));
+  expect(mode).toEqual(modeWithoutSpace);
 };
 
 describe("getOperationModes", () => {
   it.each(Object.keys(CODES_AND_EXPECTED_OPERATION_MODES))("Code %p", code => {
-    test(code, CODES_AND_EXPECTED_OPERATION_MODES[code]);
+    test(code);
   });
 });
