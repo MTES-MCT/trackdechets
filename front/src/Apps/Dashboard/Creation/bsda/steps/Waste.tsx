80860300ffa436b4fa873d3f136ce6c482a5b13d
Julien Seren-Rosso <julien.serenrosso@beta.gouv.fr>
2025-12-18 11:19:54 +0100

    Fixes weight step

--- front/src/Apps/Dashboard/Creation/bsda/steps/Waste.tsx
+++ front/src/Apps/Dashboard/Creation/bsda/steps/Waste.tsx
@@ -433,7 +433,7 @@ const WasteBsda = ({ errors }) => {
                   disabled={sealedFields.includes("weight.value")}
                   nativeInputProps={{
                     inputMode: "decimal",
-                    step: "0.1",
+                    step: "0.000001",
                     type: "number",
                     ...register("weight.value")
                   }}
