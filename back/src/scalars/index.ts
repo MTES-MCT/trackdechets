baa46e67fdcfa24382c3371ba1b13b4d8c48480d
Riron <orioncharlier@gmail.com>
2026-01-02 13:34:30 +0100

    Remove custom gql string resolver

--- back/src/scalars/index.ts
+++ back/src/scalars/index.ts
@@ -1,10 +1,8 @@
 import { URLResolver, JSONObjectResolver } from "graphql-scalars";
 import DateTimeResolver from "./DateTimeResolver";
-import StringResolver from "./StringResolver";
 
 export default {
   URL: URLResolver,
   DateTime: DateTimeResolver,
-  String: StringResolver,
   JSONObject: JSONObjectResolver
 };
