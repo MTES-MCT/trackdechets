1a2d1b0cac9c98218e3d58350705bf4b0028726e
Riron <orioncharlier@gmail.com>
2026-01-08 10:22:25 +0100

    Ajout de métriques sur le connection pool pg (#4616)

--- libs/back/prisma/src/index.ts
+++ libs/back/prisma/src/index.ts
@@ -1,12 +1,20 @@
+import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/client";
+import { initializePoolMetrics } from "./metric";
 
 const { DATABASE_URL } = process.env;
 
-const adapter = new PrismaPg(
-  { connectionString: DATABASE_URL },
-  { schema: "default$default" }
-);
+const pool = new Pool({
+  connectionString: DATABASE_URL
+});
+
+const cleanupMetrics = initializePoolMetrics(pool);
+process.on("SIGTERM", () => {
+  cleanupMetrics();
+});
+
+const adapter = new PrismaPg(pool, { schema: "default$default" });
 
 export const prisma = new PrismaClient({
   adapter,
