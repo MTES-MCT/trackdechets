4ff6ade53e1a7f42916a35fabfbdd173386930c8
Riron <orioncharlier@gmail.com>
2026-01-13 20:42:03 +0100

    Revert "[TRA-15968] L'API retourne la data brute et ne fait pas de traitement…"
    This reverts commit 79ec03ebc9c85850f13bb55565b5a6a826cbbca7.

--- back/src/scalars/index.ts
+++ back/src/scalars/index.ts
@@ -1,8 +1,10 @@
 import { URLResolver, JSONObjectResolver } from "graphql-scalars";
 import DateTimeResolver from "./DateTimeResolver";
+import StringResolver from "./StringResolver";
 
 export default {
   URL: URLResolver,
   DateTime: DateTimeResolver,
+  String: StringResolver,
   JSONObject: JSONObjectResolver
 };
