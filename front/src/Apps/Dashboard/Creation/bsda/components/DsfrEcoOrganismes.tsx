2a7f797770faab32407de0c4b3969d9fe4200b94
JulianaJM <37509748+JulianaJM@users.noreply.github.com>
2026-01-21 11:21:07 +0100

    [tra-17602]Revoir l'affichage de la sélection d'un éco-organisme (DSFR) lors de la création d'un BSDA (#4637)

--- front/src/Apps/Dashboard/Creation/bsda/components/DsfrEcoOrganismes.tsx
+++ front/src/Apps/Dashboard/Creation/bsda/components/DsfrEcoOrganismes.tsx
@@ -1,9 +1,11 @@
 import { gql, useQuery } from "@apollo/client";
-import { BsdaEcoOrganisme, EcoOrganisme, Query } from "@td/codegen-ui";
-import React, { useState } from "react";
+import { BsdaEcoOrganisme, Query } from "@td/codegen-ui";
+import React, { useState, useContext } from "react";
 import { useFormContext } from "react-hook-form";
-import CompanyResults from "../../../../../form/common/components/company/CompanyResults";
 import ToggleSwitch from "@codegouvfr/react-dsfr/ToggleSwitch";
+import Select from "@codegouvfr/react-dsfr/Select";
+import { SealedFieldsContext } from "../../../../Dashboard/Creation/context";
+import { capitalize } from "../../../../../common/helper";
 
 const GET_ECO_ORGANISMES = gql`
   {
@@ -24,12 +26,11 @@ export function getInitialEcoOrganisme(ecoOrganisme?: BsdaEcoOrganisme | null) {
   };
 }
 
-type EcoOrganismeWithOrgId = EcoOrganisme & {
-  orgId: string;
-};
-
 export default function DsfrBsdaEcoOrganismes() {
-  const { setValue, watch } = useFormContext();
+  const { setValue, watch, register, clearErrors, formState } =
+    useFormContext();
+
+  const sealedFields = useContext(SealedFieldsContext);
 
   const { loading, error, data } =
     useQuery<Pick<Query, "ecoOrganismes">>(GET_ECO_ORGANISMES);
@@ -37,6 +38,19 @@ export default function DsfrBsdaEcoOrganismes() {
   const ecoOrganisme = watch("ecoOrganisme");
 
   const [hasEcoOrganisme, setHasEcoOrganisme] = useState(ecoOrganisme !== null);
+
+  const onChangeEcoOrganisme = e => {
+    const ecoOrgSelected = data?.ecoOrganismes.find(
+      ecoOrg => ecoOrg.siret === e.currentTarget.value
+    );
+    if (ecoOrgSelected?.name) {
+      clearErrors("ecoOrganisme.siret");
+      setValue("ecoOrganisme.name", ecoOrgSelected.name);
+    } else {
+      setValue("ecoOrganisme.siret", "");
+      setValue("ecoOrganisme.name", "");
+    }
+  };
 
   function handleEcoOrganismeToggle() {
     if (hasEcoOrganisme) {
@@ -62,29 +76,35 @@ export default function DsfrBsdaEcoOrganismes() {
           {loading && <p>Chargement...</p>}
           {error && <p>Erreur lors du chargement des éco-organismes...</p>}
           {data && (
-            <div className="tw-overflow-auto my-6">
-              <CompanyResults<EcoOrganismeWithOrgId>
-                onSelect={eo =>
-                  setValue("ecoOrganisme", {
-                    name: eo.name,
-                    siret: eo.siret,
-                    orgId: eo.orgId
-                  })
-                }
-                results={data.ecoOrganismes.map(eo => ({
-                  ...eo,
-                  orgId: eo.siret
-                }))}
-                selectedItem={
-                  data.ecoOrganismes
-                    .map(eo => ({
-                      ...eo,
-                      orgId: eo.siret
-                    }))
-                    .find(eo => eo.siret === ecoOrganisme?.siret) || null
-                }
-              />
-            </div>
+            <Select
+              label="Nom de l'éco-organisme"
+              nativeSelectProps={{
+                ...register("ecoOrganisme.siret"),
+                onChange: onChangeEcoOrganisme
+              }}
+              state={formState.errors.ecoOrganisme?.["siret"] && "error"}
+              stateRelatedMessage={
+                formState.errors.ecoOrganisme?.["siret"]?.message
+              }
+              disabled={sealedFields.includes(`ecoOrganisme.siret`)}
+              className="fr-mt-2w"
+            >
+              <option value="">Sélectionner un éco-organisme</option>
+
+              {data?.ecoOrganismes
+                ?.map(ecoOrg => {
+                  return (
+                    <option
+                      key={ecoOrg.siret}
+                      value={ecoOrg.siret}
+                      defaultValue={ecoOrganisme?.siret || ""}
+                    >
+                      {`${ecoOrg.siret} - ${capitalize(ecoOrg.name)}`}
+                    </option>
+                  );
+                })
+                .filter(Boolean)}
+            </Select>
           )}
         </>
       )}
