850d1fdea99c3f8bd8d0bdc6759e93a0e4a7fe9b
Julien Seren-Rosso <julien.serenrosso@beta.gouv.fr>
2026-02-24 16:27:36 +0100

    feat: add bsff zod schema

---
+++ front/src/Apps/Dashboard/Creation/bsff/schema.ts
@@ -0,0 +1,177 @@
+import { z } from "zod";
+import { BSFF_WASTE_CODES } from "@td/constants";
+
+const BSFF_OPERATION_CODES = [
+  "R1",
+  "R2",
+  "R3",
+  "R5",
+  "R12",
+  "R13",
+  "D10",
+  "D13",
+  "D14",
+  "D15"
+] as const;
+
+const TransportMode = {
+  ROAD: "ROAD",
+  RAIL: "RAIL",
+  AIR: "AIR",
+  RIVER: "RIVER",
+  SEA: "SEA",
+  OTHER: "OTHER",
+  UNKNOWN: "UNKNOWN"
+} as const;
+
+const BsffPackagingType = {
+  BOUTEILLE: "BOUTEILLE",
+  CONTENEUR: "CONTENEUR",
+  CITERNE: "CITERNE",
+  AUTRE: "AUTRE"
+} as const;
+
+const BsffType = {
+  TRACER_FLUIDE: "TRACER_FLUIDE",
+  COLLECTE_PETITES_QUANTITES: "COLLECTE_PETITES_QUANTITES",
+  GROUPEMENT: "GROUPEMENT",
+  RECONDITIONNEMENT: "RECONDITIONNEMENT",
+  REEXPEDITION: "REEXPEDITION"
+} as const;
+
+const zodCompany = z
+  .object({
+    siret: z.string().nullish(),
+    orgId: z.string().nullish(),
+    vatNumber: z.string().nullish(),
+    name: z.string().nullish(),
+    contact: z.string().nullish(),
+    phone: z.string().nullish(),
+    mail: z.string().nullish(),
+    address: z.string().nullish(),
+    city: z.string().nullish()
+  })
+  .nullish();
+
+const zodSignature = z
+  .object({
+    author: z.string().nullish(),
+    date: z.coerce.date().nullish()
+  })
+  .nullish();
+
+export const ZodWasteCodeEnum = z
+  .enum(BSFF_WASTE_CODES, {
+    errorMap: (issue, ctx) => {
+      if (issue.code === z.ZodIssueCode.invalid_enum_value) {
+        return {
+          message:
+            `Le code déchet ne fait pas partie de la` +
+            ` liste reconnue : ${BSFF_WASTE_CODES.join(", ")}`
+        };
+      }
+      return { message: ctx.defaultError };
+    }
+  })
+  .nullish();
+
+export type ZodWasteCodeEnum = z.infer<typeof ZodWasteCodeEnum>;
+
+export const ZodOperationEnum = z
+  .enum(BSFF_OPERATION_CODES, {
+    errorMap: (issue, ctx) => {
+      if (issue.code === z.ZodIssueCode.invalid_enum_value) {
+        return {
+          message:
+            "Le code de l'opération de traitement ne fait pas" +
+            ` partie de la liste reconnue : ${BSFF_OPERATION_CODES.join(", ")}`
+        };
+      }
+      return { message: ctx.defaultError };
+    }
+  })
+  .nullish();
+
+export type ZodOperationEnum = z.infer<typeof ZodOperationEnum>;
+
+const rawBsffPackagingSchema = z.object({
+  type: z.nativeEnum(BsffPackagingType),
+  other: z.string().max(250).nullish(),
+  volume: z.number().nonnegative().nullish(),
+  weight: z.coerce.number().nonnegative().nullish(),
+  numero: z
+    .string({
+      required_error: "Conditionnements : le numéro d'identification est requis"
+    })
+    .max(250)
+    .min(1, "Conditionnements : le numéro d'identification est requis")
+});
+
+export const rawBsffSchema = z.object({
+  type: z
+    .nativeEnum(BsffType)
+    .nullish()
+    .transform(t => t ?? BsffType.COLLECTE_PETITES_QUANTITES),
+  emitter: z
+    .object({
+      company: zodCompany,
+      customInfo: z.string().max(250).nullish(),
+      emission: zodSignature
+    })
+    .nullish(),
+  waste: z
+    .object({
+      code: ZodWasteCodeEnum,
+      adr: z.string().max(750).nullish(),
+      weightValue: z.coerce.number().nonnegative().nullish(),
+      description: z.string().max(250).nullish()
+    })
+    .nullish(),
+  weightIsEstimate: z
+    .boolean()
+    .nullish()
+    .transform(v => Boolean(v)),
+  destination: z.object({
+    company: zodCompany,
+    customInfo: z.string().max(250).nullish(),
+    cap: z.string().max(250).nullish(),
+    reception: z.object({
+      date: z.coerce.date().nullish(),
+      signature: zodSignature
+    }),
+    plannedOperationCode: ZodOperationEnum
+  }),
+  detenteurCompanySirets: z.array(z.string().max(250)).optional(),
+  transporters: z
+    .array(
+      z
+        .object({
+          id: z.string().nullish(),
+          number: z.coerce.number().nonnegative().nullish(),
+          company: zodCompany,
+          customInfo: z.string().nullish(),
+          recepisse: z.object({
+            isExempted: z.boolean().nullish(),
+            number: z.string().nullish(),
+            department: z.string().nullish(),
+            validityLimit: z.coerce.date().nullish()
+          }),
+          transport: z.object({
+            mode: z.nativeEnum(TransportMode).nullish(),
+            plates: z.array(z.string()),
+            takenOverAt: z.coerce.date().nullish(),
+            signature: zodSignature
+          })
+        })
+        .nullish()
+    )
+    .max(5, "Vous ne pouvez pas ajouter plus de 5 transporteurs")
+    .optional(),
+  packagings: z.array(rawBsffPackagingSchema).nullish(),
+  ficheInterventions: z.string().max(250).array().nullish(),
+  forwarding: z.array(z.string().max(250)).nullish(),
+  repackaging: z.array(z.string().max(250)).nullish(),
+  grouping: z.array(z.string().max(250)).nullish()
+});
+
+export type ZodBsff = z.infer<typeof rawBsffSchema>;
