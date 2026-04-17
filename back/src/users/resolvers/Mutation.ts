a21c435a1abdbd852e3ccf9021bff2ff2c372c75
Nouhaila BOURHIM <nbourhim@norsys.fr>
2026-04-17 10:11:21 +0100

    [TRA-17921]-feat: Activer la double authentification

--- back/src/users/resolvers/Mutation.ts
+++ back/src/users/resolvers/Mutation.ts
@@ -1,6 +1,9 @@
 import type { MutationResolvers } from "@td/codegen-back";
 import signup from "./mutations/signup";
 import changePassword from "./mutations/changePassword";
+import generateTotpSetup from "./mutations/generateTotpSetup";
+import confirmTotpSetup from "./mutations/confirmTotpSetup";
+import disableTotp from "./mutations/disableTotp";
 import createPasswordResetRequest from "./mutations/createPasswordResetRequest";
 import resendActivationEmail from "./mutations/resendActivationEmail";
 import editProfile from "./mutations/editProfile";
@@ -44,7 +47,10 @@ const Mutation: MutationResolvers = {
   revokeAllAccessTokens,
   changeUserRole,
   subscribeToCompanyNotifications,
-  subscribeToNotifications
+  subscribeToNotifications,
+  generateTotpSetup,
+  confirmTotpSetup,
+  disableTotp
 };
 
 export default Mutation;
