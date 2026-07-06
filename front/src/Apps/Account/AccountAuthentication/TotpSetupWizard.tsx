b8a69e474f22ffaf2eba1df8e45c3a7746c4dc27
Nouhaila BOURHIM <nbourhim@norsys.fr>
2026-07-06 16:05:53 +0100

    feat(TRA-18501): MFA - Double authentification - Wording

--- front/src/Apps/Account/AccountAuthentication/TotpSetupWizard.tsx
+++ front/src/Apps/Account/AccountAuthentication/TotpSetupWizard.tsx
@@ -201,21 +201,52 @@ export default function TotpSetupWizard({ onSuccess, onClose }: Props) {
       {step === "explanation" && (
         <div>
           <p className="fr-mb-3w">
-            Un code unique vous sera demandé à chaque nouvelle session. Pour
-            l'obtenir, vous devez utiliser une application mobile.
+            Pour renforcer la sécurité de votre compte, un code à usage unique
+            vous sera demandé à chaque connexion. Pour générer ce code, vous
+            devrez utiliser une application d'authentification compatible TOTP,
+            installée sur votre téléphone.
           </p>
           <p className="fr-mb-3w">
-            Si vous n'avez pas encore d'application, nous vous conseillons
-            d'utiliser l'outil opensource{" "}
+            Si vous n'en utilisez pas déjà une, vous pouvez installer l'une des
+            applications suivantes :{" "}
             <a
               href="https://freeotp.github.io/"
               target="_blank"
               rel="noopener noreferrer"
               className="fr-link"
             >
-              FreeOTP Authenticator
+              FreeOTP
             </a>
-            .
+            <p>
+              <a
+                href="https://2fas.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="fr-link"
+              >
+                2FAS
+              </a>
+            </p>
+            <p>
+              <a
+                href="https://support.google.com/accounts/answer/1066447"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="fr-link"
+              >
+                Google Authenticator
+              </a>
+            </p>
+            <p>
+              <a
+                href="https://support.microsoft.com/en-us/authenticator/download-microsoft-authenticator"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="fr-link"
+              >
+                Microsoft Authenticator
+              </a>
+            </p>
           </p>
 
           {generateError && (
@@ -243,8 +274,9 @@ export default function TotpSetupWizard({ onSuccess, onClose }: Props) {
             </label>
             {showAppError && (
               <p className="fr-error-text">
-                Merci de bien vouloir prendre connaissance et cocher la case
-                pour continuer.
+                Il est nécessaire d'installer une application d'authentification
+                TOTP pour passer à l'étape suivante de mise en place de
+                l'authentification multifactorielle.
               </p>
             )}
           </div>
@@ -269,7 +301,7 @@ export default function TotpSetupWizard({ onSuccess, onClose }: Props) {
         <div>
           <h3 className="fr-h1">
             <p className="fr-text--lead">
-              Scannez ce QRcode avec votre application
+              Scannez ce QRCode avec votre application d'authentification.
             </p>
           </h3>
 
@@ -311,9 +343,9 @@ export default function TotpSetupWizard({ onSuccess, onClose }: Props) {
             </div>
           )}
 
-          <div className="fr-col-md-8">
+          <div className="fr-col-md-9">
             <label className="fr-label fr-text--bold fr-²">
-              Insérez le code généré par votre application
+              Insérez le code généré par votre application d'authentification
             </label>
             <Input
               label="Entrez le code à usage unique"
@@ -427,8 +459,9 @@ export default function TotpSetupWizard({ onSuccess, onClose }: Props) {
             </label>
             {showSavedError && (
               <p className="fr-error-text">
-                Merci de bien vouloir prendre connaissance et cocher la case
-                pour continuer.
+                Il est indispensable de sauvegarder vos clés de récupération
+                sans cela, en cas de perte de votre téléphone, vous ne pourrez
+                plus accéder à votre compte.
               </p>
             )}
           </div>
