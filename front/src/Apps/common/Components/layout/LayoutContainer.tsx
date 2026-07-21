30a94f720ce918ba7a441a2c83c9276034254091
Jihad OUKIK <joukik@norsys.fr>
2026-07-21 17:10:58 +0200

    feat/tra-18484: Mise à jour du texte présent dans le bandeau

--- front/src/Apps/common/Components/layout/LayoutContainer.tsx
+++ front/src/Apps/common/Components/layout/LayoutContainer.tsx
@@ -31,10 +31,8 @@ const Company = lazy(() => import("../../../../Pages/Company/Company"));
 
 const BANNER_MESSAGES = [
   <>
-    Abonnez-vous à notre lettre d'information mensuelle pour suivre les
-    nouveautés de la plateforme, la programmation des formations, des conseils
-    pratiques, ainsi que les évolutions réglementaires liées à la traçabilité
-    des déchets.
+    Renforcez la sécurité de votre compte Trackdéchets en activant dès
+    maintenant la double authentification depuis l'onglet Mon compte.
   </>
 ];
 
@@ -72,8 +70,8 @@ export default function LayoutContainer() {
                 <SurveyBanner
                   messages={BANNER_MESSAGES}
                   button={{
-                    title: "Je m'abonne",
-                    href: "https://0806de2d.sibforms.com/serve/MUIEAG29k1cikyqt55ql5CSQp_3hunRICQ8Eu8IvTZMpZl1EuQSEYeErCYUb31W6nx1mUfBKGfamqI9xMrql4caFpN2IUJQ_NR-00sPbnSv5Kw21AYm8tMHap8_7ah9NCHlcPqpNKrp7CPjO2zYsiAaBFX8r3PHDY72zP55LieF3N9gc3sUfOG16ioQgATXDPF0GeDpTuU46gBWT"
+                    title: "En savoir plus",
+                    href: "https://faq.trackdechets.fr/inscription-et-gestion-de-compte/gerer-son-compte/renouveler-son-mot-de-passe-1"
                   }}
                   persistedSurveyName="td-20260210"
                 />
