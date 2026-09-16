8f23ba5b687e927dcf7290575884d70f82bb85f3
Jihad OUKIK <joukik@norsys.fr>
2026-09-16 10:06:07 +0200

    YOGOSHA - 37649 - MEDIUM - Faille de sécurité - problème de possibilité de récupération d'email, téléphone, nom, prénom depuis l'API pour les établissement public

--- back/src/companies/resolvers/queries/__tests__/companyInfos.integration.ts
+++ back/src/companies/resolvers/queries/__tests__/companyInfos.integration.ts
@@ -1,8 +1,13 @@
 import { CompanyType } from "@td/prisma";
 import { resetDatabase } from "../../../../../integration-tests/helper";
 import { prisma } from "@td/prisma";
-import { companyFactory, siretify } from "../../../../__tests__/factories";
+import {
+  companyFactory,
+  siretify,
+  userFactory
+} from "../../../../__tests__/factories";
 import makeClient from "../../../../__tests__/testClient";
+import { AuthType } from "../../../../auth/auth";
 
 const mockSearchSirene = jest.fn();
 jest.mock("../../../sirene/searchCompany", () => ({
@@ -12,14 +17,43 @@ jest.mock("../../../sirene/searchCompany", () => ({
 
 describe("query { companyInfos(siret: <SIRET>) }", () => {
   let query: ReturnType<typeof makeClient>["query"];
-  beforeAll(() => {
-    const testClient = makeClient();
+  beforeAll(async () => {
+    const user = await userFactory();
+
+    const testClient = makeClient({
+      ...user,
+      auth: AuthType.Session
+    });
+
     query = testClient.query;
   });
 
   afterEach(async () => {
     await resetDatabase();
     mockSearchSirene.mockReset();
+  });
+  it("should forbid unauthenticated access", async () => {
+    const { query: unauthenticatedQuery } = makeClient();
+
+    const siret = siretify(8);
+
+    const gqlquery = `
+    query {
+      companyInfos(siret: "${siret}") {
+        siret
+        contact
+        contactEmail
+        contactPhone
+      }
+    }
+  `;
+
+    const { errors } = await unauthenticatedQuery<any>(gqlquery);
+
+    expect(errors).toBeDefined();
+    expect(errors[0].extensions?.code).toBe("UNAUTHENTICATED");
+
+    expect(mockSearchSirene).not.toHaveBeenCalled();
   });
 
   it("Random company not registered in Trackdéchets", async () => {
